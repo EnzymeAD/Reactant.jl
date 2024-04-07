a8bd6c1d15875d78d3f973bbbd873a69a4b38b46
William S. Moses <gh@wsmoses.com>
2024-04-07 18:22:56 -0400

    jll work

---
+++ deps/ReactantExtra/tblgen/mlir-jl-tblgen.cc
@@ -0,0 +1,67 @@
+// Copyright 2021 Google LLC
+// Copyright 2023 Valentin Churavy
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/FormatVariadic.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/TableGen/Error.h"
+#include "llvm/TableGen/Main.h"
+#include "llvm/TableGen/Record.h"
+#include "llvm/TableGen/TableGenBackend.h"
+
+using namespace llvm;
+
+using generator_function = bool(const llvm::RecordKeeper& recordKeeper,
+                                llvm::raw_ostream& os);
+
+struct GeneratorInfo {
+  const char* name;
+  generator_function* generator;
+};
+
+extern generator_function emitOpTableDefs;
+extern generator_function emitTestTableDefs;
+
+static std::array<GeneratorInfo, 1> generators {{
+  {"jl-op-defs", emitOpTableDefs},
+}};
+
+generator_function* generator;
+bool disableModuleWrap;
+
+int main(int argc, char **argv) {
+  llvm::InitLLVM y(argc, argv);
+  llvm::cl::opt<std::string> generatorOpt("generator", llvm::cl::desc("Generator to run"), cl::Required);
+  llvm::cl::opt<bool> disableModuleWrapOpt("disable-module-wrap", llvm::cl::desc("Disable module wrap"), cl::init(false));
+  cl::ParseCommandLineOptions(argc, argv);
+  for (const auto& spec : generators) {
+    if (generatorOpt == spec.name) {
+      generator = spec.generator;
+      break;
+    }
+  }
+  if (!generator) {
+    llvm::errs() << "Invalid generator type\n";
+    abort();
+  }
+  disableModuleWrap = disableModuleWrapOpt;
+
+  return TableGenMain(argv[0], [](raw_ostream& os, RecordKeeper &records) {
+    return generator(records, os);
+  });
+}
--- deps/build_local.jl
+++ deps/build_local.jl
@@ -1,44 +1,16 @@
 # Invoke with
 # `julia --project=deps deps/build_local.jl [path-to-enzyme]`
 
-# the pre-built Enzyme_jll might not be loadable on this platform
-Enzyme_jll = Base.UUID("7cc45869-7501-5eee-bdea-0790c847d4ef")
+# the pre-built ReactantExtra_jll might not be loadable on this platform
+ReactantExtra_jll = Base.UUID("7cc45869-7501-5eee-bdea-0790c847d4ef")
 
 using Pkg, Scratch, Preferences, Libdl
 
-BUILD_TYPE = "RelWithDebInfo" 
-
 # 1. Get a scratch directory
-scratch_dir = get_scratch!(Enzyme_jll, "build")
+scratch_dir = get_scratch!(ReactantExtra_jll, "build")
 isdir(scratch_dir) && rm(scratch_dir; recursive=true)
 
-source_dir = nothing
-branch = nothing
-if length(ARGS) == 2 
-    @assert ARGS[1] == "--branch"
-    branch = ARGS[2]
-    source_dir = nothing
-elseif length(ARGS) == 1
-    source_dir = ARGS[1]
-end
-
-if branch === nothing
-    branch = "main"
-end
-
-source_dir = (@__DIR__)*"/ReactionExtra"
-
-if source_dir === nothing
-    scratch_src_dir = get_scratch!(Enzyme_jll, "src")
-    cd(scratch_src_dir) do
-        if !isdir("Enzyme")
-            run(`git clone https://github.com/wsmoses/Enzyme`)
-        end
-        run(`git -C Enzyme fetch`)
-        run(`git -C Enzyme checkout origin/$(branch)`)
-    end
-    source_dir = joinpath(scratch_src_dir, "Enzyme", "enzyme")
-end
+source_dir = joinpath(@__DIR__, "ReactantExtra")
 
 # 2. Ensure that an appropriate LLVM_full_jll is installed
 Pkg.activate(; temp=true)
@@ -46,28 +18,21 @@ Pkg.activate(; temp=true)
 # Build!
 @info "Building" source_dir scratch_dir
 run(`mkdir -p $(scratch_dir)`)
-run(Cmd(`bazel build --check_visibility=false --experimental_repo_remote_exec -s ...`, dir=source_dir))
+run(Cmd(`bazel build --check_visibility=false ...`, dir=source_dir))
 
 # Discover built libraries
-built_libs = filter(readdir(joinpath(scratch_dir))) do file
+built_libs = filter(readdir(joinpath(source_dir, "bazel-bin"))) do file
     endswith(file, ".$(Libdl.dlext)") && startswith(file, "lib")
 end
 
-lib_path = joinpath(scratch_dir, "Enzyme", only(built_libs))
+lib_path = joinpath(source_dir, "bazel-bin", only(built_libs))
 isfile(lib_path) || error("Could not find library $lib_path in build directory")
 
-built_libs = filter(readdir(joinpath(scratch_dir, "BCLoad"))) do file
-    endswith(file, ".$(Libdl.dlext)") && startswith(file, "lib")
-end
-
-libBC_path = joinpath(scratch_dir, "BCLoad", only(built_libs))
-isfile(libBC_path) || error("Could not find library $libBC_path in build directory")
-
-# Tell Enzyme_jll to load our library instead of the default artifact one
+# Tell ReactReactantExtra_jllant_jll to load our library instead of the default artifact one
 set_preferences!(
     joinpath(dirname(@__DIR__), "LocalPreferences.toml"),
-    "Enzyme_jll",
-    "libEnzyme_path" => lib_path,
-    "libEnzymeBCLoad_path" => libBC_path;
+    "ReactantExtra_jll",
+    "libReactantExtra_path" => lib_path,
+    "libReactantDialects_path" => joinpath(source_dir, "bazel-bin"),
     force=true,
 )
