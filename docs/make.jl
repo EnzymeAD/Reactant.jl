89ce6d28fa342e544f7c4446cfd53d8f7184bb5d
William S. Moses <gh@wsmoses.com>
2024-05-14 23:25:06 -0400

    fixup docs/compat

---
+++ docs/make.jl
@@ -0,0 +1,50 @@
+pushfirst!(LOAD_PATH, joinpath(@__DIR__, "..")) # add Enzyme to environment stack
+
+using Reactant
+using Documenter
+
+DocMeta.setdocmeta!(Reactant, :DocTestSetup, :(using Reactant); recursive=true)
+
+# Generate examples
+
+using Literate
+
+const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
+const OUTPUT_DIR = joinpath(@__DIR__, "src/generated")
+
+examples = Pair{String,String}[]
+
+for (_, name) in examples
+    example_filepath = joinpath(EXAMPLES_DIR, string(name, ".jl"))
+    Literate.markdown(example_filepath, OUTPUT_DIR, documenter = true)
+end
+
+examples = [title => joinpath("generated", string(name, ".md")) for (title, name) in examples]
+
+makedocs(;
+    modules=[Reactant],
+    authors="William Moses <wsmoses@illinois.edu>, Valentin Churavy <vchuravy@mit.edu>",
+    repo="https://github.com/EnzymeAD/Reactant.jl/blob/{commit}{path}#{line}",
+    sitename="Reactant.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://enzymead.github.io/Reactant.jl/",
+        assets = [
+            asset("https://plausible.io/js/plausible.js",
+                    class=:js,
+                    attributes=Dict(Symbol("data-domain") => "enzyme.mit.edu", :defer => "")
+                )
+	    ],
+    ),
+    pages = [
+        "Home" => "index.md",
+    ],
+    doctest = true,
+    strict = true,
+)
+
+deploydocs(;
+    repo="github.com/EnzymeAD/Reactant.jl",
+    devbranch = "main",
+    push_preview = true,
+)
---
+++ docs/src/index.md
@@ -0,0 +1,13 @@
+```@meta
+CurrentModule = Reactant
+DocTestSetup = quote
+    using Reactant
+end
+```
+
+# Reactant
+
+Documentation for [Reactant.jl](https://github.com/EnzymeAD/Reactant.jl).
+
+This is truly something which we should write docs for.
+
