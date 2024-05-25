ca33253464855b5e12baa587b0c5235f866d000e
Sergio SÂSánchez Ramírez <Sergio Snchez Ramrez>
2024-05-26 01:02:36 +0200

    Test mutable struct types

--- test/struct.jl
+++ test/struct.jl
@@ -22,3 +22,24 @@ Base.cos(x::MockTensor) = MockTensor(cos(parent(x)), x.inds)
     @test y isa MockTensor{Float64,2,Reactant.ConcreteRArray{Float64,(4, 4),2}}
     @test isapprox(parent(y), cos.(parent(x)))
 end
+
+struct MutableMockTensor{T,N,A<:AbstractArray{T,N}}
+    data::A
+    inds::Vector{Symbol}
+end
+
+MutableMockTensor(data::A, inds) where {T,N,A<:AbstractArray{T,N}} = MutableMockTensor{T,N,A}(data, inds)
+Base.parent(t::MutableMockTensor) = t.data
+
+Base.cos(x::MutableMockTensor) = MutableMockTensor(cos(parent(x)), x.inds)
+
+@testset "Mutable Struct" begin
+    x = MutableMockTensor(rand(4, 4), [:i, :j])
+    x2 = MutableMockTensor(Reactant.ConcreteRArray(parent(x)), x.inds)
+
+    f = Reactant.compile(cos, (x2,))
+    y = f(x2)
+
+    @test y isa MutableMockTensor{Float64,2,Reactant.ConcreteRArray{Float64,(4, 4),2}}
+    @test isapprox(parent(y), cos.(parent(x)))
+end
