dad034ae3a203570ce88d5fc04d3051d01c06ab7
Sergio SÂSánchez Ramírez <Sergio Snchez Ramrez>
2024-05-26 00:59:00 +0200

    Test compilation on custom `struct` types

---
+++ test/struct.jl
@@ -0,0 +1,24 @@
+using Reactant
+using Test
+
+# from bsc-quantic/Tenet.jl
+struct MockTensor{T,N,A<:AbstractArray{T,N}}
+    data::A
+    inds::Vector{Symbol}
+end
+
+MockTensor(data::A, inds) where {T,N,A<:AbstractArray{T,N}} = MockTensor{T,N,A}(data, inds)
+Base.parent(t::MockTensor) = t.data
+
+Base.cos(x::MockTensor) = MockTensor(cos(parent(x)), x.inds)
+
+@testset "Struct" begin
+    x = MockTensor(rand(4, 4), [:i, :j])
+    x2 = MockTensor(Reactant.ConcreteRArray(parent(x)), x.inds)
+
+    f = Reactant.compile(cos, (x2,))
+    y = f(x2)
+
+    @test y isa MockTensor{Float64,2,Reactant.ConcreteRArray{Float64,(4, 4),2}}
+    @test isapprox(parent(y), cos.(parent(x)))
+end
