db8cc49a85f4ebe7f625e281b4e807e74494f934
Sergio Sánchez Ramírez <sergio.sancnchez.ramirez+git@bsc.es>
2024-06-27 16:19:17 +0200

    Test on mock closure

---
+++ test/closure.jl
@@ -0,0 +1,13 @@
+using Reactant
+
+muler(x) = y -> x * y
+
+@testset "closure" begin
+    x = Reactant.ConcreteRArray(ones(2, 2))
+    y = Reactant.ConcreteRArray(ones(2, 2))
+
+    f = muler(x)
+    g = Reactant.compile(f, (y,))
+
+    @test g(y) ≈ x * y
+end
