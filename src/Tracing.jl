9c5dee4b417a38bb08966fea8e8deb95e90fa717
Sergio Sánchez Ramírez <sergio.sancnchez.ramirez+git@bsc.es>
2024-07-19 17:38:32 +0200

    Refactor and test `traced_type`

--- src/Tracing.jl
+++ src/Tracing.jl
@@ -690,3 +690,204 @@ function _copyto!(dest::TracedRArray, bc::Broadcasted)
     dest.mlir_data = res.mlir_data
     return dest
 end
+
+for T in (
+    DataType,
+    Module,
+    Nothing,
+    Symbol,
+    AbstractChar,
+    AbstractFloat,
+    Integer,
+    AbstractString,
+    RArray,
+)
+    @eval function traced_type(::Type{T}, seen, mode) where {T<:$T}
+        return T
+    end
+end
+
+function traced_type(::Type{C}, seen, mode) where {T,C<:Complex{T}}
+    if !(C isa UnionAll)
+        return Complex{traced_type(T, seen, mode)}
+    else
+        return @invoke traced_type(C::Type{Any}, seen, mode)
+    end
+end
+
+function traced_type(::Type{T}, seen, mode) where {T<:Function}
+    # functions are directly returned
+    if sizeof(T) == 0
+        return T
+    end
+
+    # in closures, enclosured variables need to be traced
+    N = fieldcount(T)
+    traced_fieldtypes = ntuple(Val(N)) do i
+        return traced_type(fieldtype(T, i), seen, mode)
+    end
+
+    # closure are struct types with the types of enclosured vars as type parameters
+    return Core.apply_type(T.name.wrapper, traced_fieldtypes...)
+end
+
+function traced_type(::Type{T}, seen, mode) where {T<:Tuple}
+    if !Base.isconcretetype(T) || !is_concrete_tuple(T) || T isa UnionAll
+        throw(AssertionError("Type $T is not concrete type or concrete tuple"))
+    elseif is_concrete_tuple(T) && any(T2 isa Core.TypeofVararg for T2 in T.parameters)
+        # Tuple{((T2 isa Core.TypeofVararg ? Any : T2) for T2 in T.parameters)...}
+        throw(AssertionError("Type tuple of vararg $T is not supported"))
+    end
+    return Tuple{traced_type(T.parameters[i], seen, mode) for i in 1:length(T.parameters)}
+end
+
+function traced_type(::Type{T}, seen, mode) where {N,V,T<:NamedTuple{N,V}}
+    return NamedTuple{N,traced_type(V, seen, mode)}
+end
+
+function traced_type(::Type{T}, seen, mode) where {K,V,T<:AbstractDict{K,V}}
+    dictty = T.name.wrapper
+    return dictty{K,traced_type(V, seen, mode)}
+end
+
+function traced_type(::Type{T}, seen, mode) where {T}
+    if T === Any
+        return T
+    end
+
+    if T === Union{}
+        return T
+    end
+
+    if Enzyme.Compiler.isghostty(T) || Core.Compiler.isconstType(T)
+        return T
+    end
+
+    # unknown number of fields
+    if T isa UnionAll
+        aT = Base.argument_datatype(T)
+        if isnothing(aT)
+            throw("Unhandled type $T")
+        end
+        if isnothing(Base.datatype_fieldcount(aT))
+            throw("Unhandled type $T")
+        end
+    end
+
+    if T isa Union
+        return Union{traced_type(T.a, seen, mode),traced_type(T.b, seen, mode)}
+    end
+
+    # if abstract it must be by reference
+    if Base.isabstracttype(T)
+        throw("Unhandled abstract type $T")
+    end
+
+    if !(Base.isconcretetype(T) || T isa UnionAll)
+        throw(AssertionError("Type $T is not concrete type or concrete tuple"))
+    end
+
+    nextTy = getmap(Val(T), seen...)
+    if !isnothing(nextTy)
+        return nextTy
+    end
+
+    seen2 = (Val(T), Val(T), seen...)
+
+    changed = false
+    subTys = Type[]
+    for f in 1:fieldcount(T)
+        subT = fieldtype(T, f)
+        subTT = traced_type(subT, seen2, mode)
+        changed |= subT != subTT
+        push!(subTys, subTT)
+    end
+
+    if !changed
+        return T
+    end
+
+    subParms = []
+    for SST in T.parameters
+        if SST isa Type
+            TrT = traced_type(SST, seen, mode)
+            push!(subParms, TrT)
+        else
+            push!(subParms, SST)
+        end
+    end
+
+    TT2 = Core.apply_type(T.name.wrapper, subParms...)
+    seen3 = (Val(T), Val(TT2), seen...)
+    if fieldcount(T) == fieldcount(TT2)
+        legal = true
+        for f in 1:fieldcount(T)
+            subT = fieldtype(T, f)
+            subT2 = fieldtype(TT2, f)
+            subTT = traced_type(subT, seen3, mode)
+            if subT2 != subTT
+                legal = false
+                break
+            end
+        end
+        if legal
+            return TT2
+        end
+    end
+
+    name = Symbol[]
+    throw(error("Cannot convert type $T, best attempt $TT2 failed"))
+end
+
+function traced_type(::Type{T}, seen, ::Val{mode}) where {T<:ConcreteRArray,mode}
+    if mode == ConcreteToTraced
+        @inline base_typet(TV::TT) where {TT<:UnionAll} =
+            UnionAll(TV.var, base_typet(TV.body))
+        @inline base_typet(TV::TT) where {TT<:DataType} = TracedRArray{TV.parameters...}
+        return base_typet(T)
+    elseif mode == TracedToConcrete
+        return T
+    else
+        throw("Abstract RArray cannot be made concrete")
+    end
+end
+
+function traced_type(::Type{T}, seen::ST, ::Val{mode}) where {ST,T<:TracedRArray,mode}
+    if mode == ConcreteToTraced
+        throw("TracedRArray $T cannot be traced")
+    elseif mode == TracedToConcrete
+        @inline base_typec(TV::TT) where {TT<:UnionAll} =
+            UnionAll(TV.var, base_typec(TV.body))
+        @inline base_typec(TV::TT) where {TT<:DataType} = ConcreteRArray{TV.parameters...}
+        return base_typec(T)
+    elseif mode == TracedTrack || mode == TracedSetPath
+        return T
+    else
+        throw("Abstract RArray $T cannot be made concrete in mode $mode")
+    end
+end
+
+function traced_type(::Type{T}, seen, mode) where {T<:XLAArray}
+    throw("XLA $T array cannot be traced")
+end
+
+function traced_type(::Type{A}, seen::ST, ::Val{mode}) where {T,N,A<:Array{T,N},ST,mode}
+    if mode == ArrayToConcrete && T <: AbstractFloat
+        return ConcreteRArray{T,N}
+    else
+        return Array{traced_type(T, seen, Val(mode)),N}
+    end
+end
+
+for P in (Ptr, Core.LLVMPtr, Base.RefValue)
+    @eval function traced_type(::Type{P}, seen, mode) where {T,P<:$P{T}}
+        return $P{traced_type(T, seen, mode)}
+    end
+end
+
+function traced_type(::Type{Val{T}}, seen, mode) where {T}
+    if traced_type(typeof(T), seen, mode) == typeof(T)
+        return T
+    end
+    throw("Val type $T cannot be traced")
+end
