3c14d164e58e194f276f8bfc937c50600b17e8ad
Sergio Sánchez Ramírez <sergio.sancnchez.ramirez+git@bsc.es>
2024-07-19 12:13:24 +0200

    Finish refactor of tracing system

--- test/bcast.jl
+++ test/bcast.jl
@@ -45,7 +45,7 @@ function test()
                 a = ones(4)
                 b = ones(4)
                 d = Data(
-                    Reactant.TracedRArray{Float64,(4,),1}((), MLIR.IR.argument(fnbody, 1))
+                    Reactant.TracedRArray{Float64,1}((), MLIR.IR.argument(fnbody, 1), (4,))
                 )
 
                 return tmp(a, b, d)
