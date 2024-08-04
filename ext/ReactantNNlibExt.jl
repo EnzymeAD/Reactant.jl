d0e1dbde338e5eca22df5df0d87eeca01caa2eeb
Avik Pal <avikpal@mit.edu>
2024-08-04 14:54:15 -0700

    refactor: remove more elem_apply

--- ext/ReactantNNlibExt.jl
+++ ext/ReactantNNlibExt.jl
@@ -8,17 +8,13 @@ for (jlop, hloop) in (
     (:(NNlib.sigmoid_fast), :logistic),
     (:(NNlib.sigmoid), :logistic),
 )
-    @eval begin
-        function Reactant.elem_apply(
-            ::typeof($jlop), lhs::Reactant.TracedRArray{ElType,Shape,N}
-        ) where {ElType,Shape,N}
-            return Reactant.TracedRArray{ElType,Shape,N}(
-                (),
-                Reactant.MLIR.IR.result(
-                    Reactant.MLIR.Dialects.stablehlo.$hloop(lhs.mlir_data), 1
-                ),
-            )
-        end
+    @eval function $(jlop)(x::Reactant.TracedRArray{T,(),0}) where {T}
+        return Reactant.TracedRArray{T,(),0}(
+            (),
+            Reactant.MLIR.IR.result(
+                Reactant.MLIR.Dialects.stablehlo.$(hloop)(x.mlir_data), 1
+            ),
+        )
     end
 end
 
