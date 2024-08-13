a3fbb01780ac391d218eac4519df74fed1f12bc3
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-08-14 00:05:27 +0200

    Update `ReactantStatisticsExt` code

--- ext/ReactantStatisticsExt.jl
+++ ext/ReactantStatisticsExt.jl
@@ -3,14 +3,14 @@ module ReactantStatisticsExt
 using Reactant: TracedRArray
 using Statistics: Statistics
 
-function Statistics.mean(A::TracedRArray{T,Shape,N}; dims=:) where {T,Shape,N}
+function Statistics.mean(A::TracedRArray{T,N}; dims=:) where {T,N}
     denom = dims isa Colon ? length(A) : prod(Base.Fix1(size, A), dims)
     return mapreduce(identity, +, A; dims) / denom
 end
 
 function Statistics.var(
-    A::TracedRArray{T,Shape,N}; dims=:, mean=nothing, corrected=true
-) where {T,Shape,N}
+    A::TracedRArray{T,N}; dims=:, mean=nothing, corrected=true
+) where {T,N}
     mean === nothing && (mean = Statistics.mean(A; dims))
     denom = (dims isa Colon ? length(A) : prod(Base.Fix1(size, A), dims)) - corrected
     return mapreduce(abs2, +, A .- mean; dims) / denom
