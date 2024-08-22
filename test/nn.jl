b04636b0ecb2f223f283bd15700f6c44da9b7da7
Avik Pal <avikpal@mit.edu>
2024-08-22 08:21:02 -0700

    test(flux): remove non-reactant code

--- test/nn.jl
+++ test/nn.jl
@@ -18,8 +18,6 @@ model = Chain(
 using BenchmarkTools
 
 origout = model(noisy)
-@show origout[3]
-@btime model(noisy)
 
 cmodel = Reactant.make_tracer(IdDict(), model, (), Reactant.ArrayToConcrete)
 cnoisy = Reactant.ConcreteRArray(noisy)
@@ -41,32 +39,5 @@ f = Reactant.compile((a, b) -> a(b), (cmodel, cnoisy))
 # @show @code_typed f(cmodel,cnoisy)
 # @show @code_llvm f(cmodel,cnoisy)
 comp = f(cmodel, cnoisy)
-@show comp[3]
 # @btime f(cmodel, cnoisy)
 @test origout ≈ comp
-
-# To train the model, we use batches of 64 samples, and one-hot encoding:
-target = Flux.onehotbatch(truth, [true, false])                   # 2×1000 OneHotMatrix
-loader = Flux.DataLoader((noisy, target); batchsize=64, shuffle=true);
-# 16-element DataLoader with first element: (2×64 Matrix{Float32}, 2×64 OneHotMatrix)
-
-optim = Flux.setup(Flux.Adam(0.01), model)  # will store optimiser momentum, etc.
-
-# Training loop, using the whole data set 1000 times:
-losses = []
-for epoch in 1:1_000
-    for (x, y) in loader
-        loss, grads = Flux.withgradient(model) do m
-            # Evaluate model and loss inside gradient context:
-            y_hat = m(x)
-            return Flux.crossentropy(y_hat, y)
-        end
-        Flux.update!(optim, model, grads[1])
-        push!(losses, loss)  # logging, outside gradient context
-    end
-end
-
-optim # parameters, momenta and output have all changed
-out2 = model(noisy)  # first row is prob. of true, second row p(false)
-
-mean((out2[1, :] .> 0.5) .== truth)  # accuracy 94% so far!
