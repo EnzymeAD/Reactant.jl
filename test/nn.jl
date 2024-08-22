001aa0b06eb4c0539f5429624e21bbff3fd87966
Avik Pal <avikpal@mit.edu>
2024-08-22 08:21:02 -0700

    test(flux): remove non-reactant code

--- test/nn.jl
+++ test/nn.jl
@@ -42,32 +42,6 @@ comp = f(cmodel, cnoisy)
 # @btime f(cmodel, cnoisy)
 @test origout ≈ comp
 
-# To train the model, we use batches of 64 samples, and one-hot encoding:
-target = Flux.onehotbatch(truth, [true, false])                   # 2×1000 OneHotMatrix
-loader = Flux.DataLoader((noisy, target); batchsize=64, shuffle=true);
-# 16-element DataLoader with first element: (2×64 Matrix{Float32}, 2×64 OneHotMatrix)
-
-optim = Flux.setup(Flux.Adam(0.01), model)  # will store optimiser momentum, etc.
-
-# Training loop, using the whole data set 1000 times:
-losses = []
-for epoch in 1:1_000
-    for (x, y) in loader
-        loss, grads = Flux.withgradient(model) do m
-            # Evaluate model and loss inside gradient context:
-            y_hat = m(x)
-            return Flux.crossentropy(y_hat, y)
-        end
-        Flux.update!(optim, model, grads[1])
-        push!(losses, loss)  # logging, outside gradient context
-    end
-end
-
-optim # parameters, momenta and output have all changed
-out2 = model(noisy)  # first row is prob. of true, second row p(false)
-
-mean((out2[1, :] .> 0.5) .== truth)  # accuracy 94% so far!
-
 @testset "conv" begin
     conv = Conv(randn(Float32, 10, 10, 3, 1), randn(Float32, 1))
     conv_reactant = Conv(
