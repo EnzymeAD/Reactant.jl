d6e77267d39ec93652883dff7ac2aaa778debe1a
mofeing <15837247+mofeing@users.noreply.github.com>
2024-09-21 00:13:44 +0000

    Format code

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,9 @@ function Remotes.fileurl(::TestRemote, ::Any, filename, linerange)
     L1, L2 = first(linerange), last(linerange)
     return "https://github.com/JuliaBinaryWrappers/Reactant_jll.jl/$(filename)#L$(L1)-$(L2)"
 end
-Remotes.issueurl(::TestRemote, issue) = "https://github.com/EnzymeAD/Reactant.jl/blob/$(issue)"
+function Remotes.issueurl(::TestRemote, issue)
+    return "https://github.com/EnzymeAD/Reactant.jl/blob/$(issue)"
+end
 
 DocMeta.setdocmeta!(Reactant, :DocTestSetup, :(using Reactant); recursive=true)
 
@@ -25,51 +27,66 @@ examples = Pair{String,String}[]
 
 for (_, name) in examples
     example_filepath = joinpath(EXAMPLES_DIR, string(name, ".jl"))
-    Literate.markdown(example_filepath, OUTPUT_DIR, documenter = true)
+    Literate.markdown(example_filepath, OUTPUT_DIR; documenter=true)
 end
 
-run(Cmd(`rm -rf .git`, dir=Reactant_jll.artifact_dir))
-run(Cmd(`git init`, dir=Reactant_jll.artifact_dir))
-run(Cmd(`git config user.name ReactantDocs`, dir=Reactant_jll.artifact_dir))
-run(Cmd(`git config user.email ReactantDocs@wsmoses.com`, dir=Reactant_jll.artifact_dir))
-run(Cmd(`git remote add origin https://github.com/EnzymeAD/Reactant.jl`, dir=Reactant_jll.artifact_dir))
-run(Cmd(`git add -A`, dir=Reactant_jll.artifact_dir))
-run(Cmd(`git commit -m "Initial commit"`, dir=Reactant_jll.artifact_dir))
+run(Cmd(`rm -rf .git`; dir=Reactant_jll.artifact_dir))
+run(Cmd(`git init`; dir=Reactant_jll.artifact_dir))
+run(Cmd(`git config user.name ReactantDocs`; dir=Reactant_jll.artifact_dir))
+run(Cmd(`git config user.email ReactantDocs@wsmoses.com`; dir=Reactant_jll.artifact_dir))
+run(
+    Cmd(
+        `git remote add origin https://github.com/EnzymeAD/Reactant.jl`;
+        dir=Reactant_jll.artifact_dir,
+    ),
+)
+run(Cmd(`git add -A`; dir=Reactant_jll.artifact_dir))
+run(Cmd(`git commit -m "Initial commit"`; dir=Reactant_jll.artifact_dir))
 
-examples = [title => joinpath("generated", string(name, ".md")) for (title, name) in examples]
+examples = [
+    title => joinpath("generated", string(name, ".md")) for (title, name) in examples
+]
 
 gh = Documenter.Remotes.GitHub("EnzymeAD", "Reactant.jl")
 
 makedocs(;
-    modules=[Reactant, Reactant.XLA, Reactant.MLIR, Reactant.MLIR.API, Reactant.MLIR.IR, Reactant.MLIR.Dialects.chlo, Reactant.MLIR.Dialects.vhlo, Reactant.MLIR.Dialects.stablehlo, Reactant.MLIR.Dialects.enzyme, Reactant.MLIR.Dialects.arith, Reactant.MLIR.Dialects.func, Reactant.MLIR.Dialects.affine, Reactant.MLIR.Dialects.builtin],
+    modules=[
+        Reactant,
+        Reactant.XLA,
+        Reactant.MLIR,
+        Reactant.MLIR.API,
+        Reactant.MLIR.IR,
+        Reactant.MLIR.Dialects.chlo,
+        Reactant.MLIR.Dialects.vhlo,
+        Reactant.MLIR.Dialects.stablehlo,
+        Reactant.MLIR.Dialects.enzyme,
+        Reactant.MLIR.Dialects.arith,
+        Reactant.MLIR.Dialects.func,
+        Reactant.MLIR.Dialects.affine,
+        Reactant.MLIR.Dialects.builtin,
+    ],
     authors="William Moses <wsmoses@illinois.edu>, Valentin Churavy <vchuravy@mit.edu>",
     remotes=Dict(
-            # Just non-repository directories
-            joinpath(@__DIR__, "..") => gh, 
-            Reactant_jll.artifact_dir => TestRemote(),
+        # Just non-repository directories
+        joinpath(@__DIR__, "..") => gh,
+        Reactant_jll.artifact_dir => TestRemote(),
     ),
     sitename="Reactant.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://enzymead.github.io/Reactant.jl/",
-        size_threshold_ignore = ["api.md"],
-        assets = [
-            asset("https://plausible.io/js/plausible.js",
-                    class=:js,
-                    attributes=Dict(Symbol("data-domain") => "enzyme.mit.edu", :defer => "")
-                )
-	    ],
+        size_threshold_ignore=["api.md"],
+        assets=[
+            asset(
+                "https://plausible.io/js/plausible.js";
+                class=:js,
+                attributes=Dict(Symbol("data-domain") => "enzyme.mit.edu", :defer => ""),
+            ),
+        ],
     ),
-    pages = [
-        "Home" => "index.md",
-        "API reference" => "api.md",
-    ],
-    doctest = true,
-    warnonly = true,
+    pages=["Home" => "index.md", "API reference" => "api.md"],
+    doctest=true,
+    warnonly=true,
 )
 
-deploydocs(;
-    repo="github.com/EnzymeAD/Reactant.jl",
-    devbranch = "main",
-    push_preview = true,
-)
+deploydocs(; repo="github.com/EnzymeAD/Reactant.jl", devbranch="main", push_preview=true)
