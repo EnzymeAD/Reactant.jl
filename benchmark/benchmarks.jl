61b0c6dfc5e15edf49885d776f39d50fe9cce657
Avik Pal <avikpal@mit.edu>
2024-09-28 09:43:32 -0400

    perf: try reusing the project file

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,19 +1,3 @@
-if !isfile(joinpath(@__DIR__, "Project.toml"))
-    # Assume the Project.toml wasn't activated yet
-    @info "Installing Benchmark dependencies"
-    import Pkg
-    Pkg.activate(@__DIR__)
-    Pkg.develop(Pkg.PackageSpec(path=joinpath(@__DIR__, "..")))
-    extra_pkgs = [
-        Pkg.PackageSpec(name="BenchmarkTools", version="1.5"),
-        Pkg.PackageSpec(name="Boltz", version="1"),
-        Pkg.PackageSpec(name="Lux", version="1.1"),
-        Pkg.PackageSpec(name="Random", version="1.10"),
-        Pkg.PackageSpec(name="Enzyme", version="0.13"),
-    ]
-    Pkg.add(extra_pkgs)
-end
-
 using BenchmarkTools
 using Reactant
 using Enzyme
