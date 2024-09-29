d237943b3a7343219cfee476d8e36b2335dc4c27
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-29 17:04:58 -0400

    Move Enzyme-related code to new "Enzyme.jl" file

--- src/Reactant.jl
+++ src/Reactant.jl
@@ -3,7 +3,9 @@ module Reactant
 # auxiliary types and functions
 include("OrderedIdDict.jl")
 
-using Enzyme
+include("mlir/MLIR.jl")
+include("XLA.jl")
+include("Interpreter.jl")
 
 abstract type RArray{T,N} <: AbstractArray{T,N} end
 
@@ -11,40 +13,10 @@ function Base.reshape(A::RArray, dims::Tuple{Vararg{Union{Int,Colon}}})
     return reshape(A, Base._reshape_uncolon(A, dims))
 end
 
-function Enzyme.make_zero(
-    ::Type{RT}, seen::IdDict, prev::RT, ::Val{copy_if_inactive}=Val(false)
-)::RT where {copy_if_inactive,RT<:RArray}
-    if haskey(seen, prev)
-        return seen[prev]
-    end
-    if Enzyme.Compiler.guaranteed_const_nongen(RT, nothing)
-        return copy_if_inactive ? Base.deepcopy_internal(prev, seen) : prev
-    end
-    if RT <: ConcreteRArray
-        res = RT(zeros(eltype(RT), size(prev)))
-        seen[prev] = res
-        return res
-    end
-
-    if RT <: TracedRArray
-        res = broadcast_to_size(eltype(RT)(0), size(prev))
-        seen[prev] = res
-        return res
-    end
-
-    attr = fill(MLIR.IR.Attribute(eltype(RT)(0)), mlir_type(prev))
-    cst = MLIR.IR.result(MLIR.Dialects.stablehlo.constant(; value=attr), 1)
-    res = RT((), cst)
-    seen[prev] = res
-    return res
-end
-
-include("mlir/MLIR.jl")
-include("XLA.jl")
-include("Interpreter.jl")
 include("utils.jl")
 include("ConcreteRArray.jl")
 include("TracedRArray.jl")
+
 include("Tracing.jl")
 include("Compiler.jl")
 
