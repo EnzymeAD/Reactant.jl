971cbe2150828b2448ed565c66c7e5a7ace9e00e
Avik Pal <avikpal@mit.edu>
2024-09-30 20:32:24 -0400

    ci: add runtime benchmarking to CI (and ViT)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,6 +5,7 @@ using Boltz, Lux, Random
 
 const SUITE = BenchmarkGroup()
 
+SUITE["runtime"] = BenchmarkGroup()
 SUITE["comptime"] = BenchmarkGroup()
 
 SUITE["comptime"]["basics"] = BenchmarkGroup()
@@ -20,17 +21,52 @@ end setup = begin
     a = Reactant.ConcreteRArray(ones(2, 10))
 end
 
+SUITE["runtime"]["lux neural networks"] = BenchmarkGroup()
 SUITE["comptime"]["lux neural networks"] = BenchmarkGroup()
 
-for depth in [11, 13, 16, 19]
-    SUITE["comptime"]["lux neural networks"]["vgg$depth"] = @benchmarkable Reactant.compile(
-        vgg, (x, ps_concrete, st_concrete)
-    ) setup = begin
-        vgg = Vision.VGG($depth; pretrained=false, batchnorm=false)
+for depth in [11, 13, 16, 19], batchnorm in [false,]#  true] <-- not working yet
+    SUITE["comptime"]["lux neural networks"]["vgg$(depth) bn=$(batchnorm)"] = @benchmarkable begin
+        @compile vgg(x, ps_concrete, st_concrete)
+    end setup = begin
+        vgg = Vision.VGG($depth; pretrained=false, batchnorm=$(batchnorm))
         ps, st = Lux.setup(Random.default_rng(), vgg)
         ps_concrete = Reactant.to_rarray(ps)
         st_concrete = Reactant.to_rarray(Lux.testmode(st))
         x = Reactant.to_rarray(rand(Float32, 224, 224, 3, 16))
+    end
+
+    SUITE["runtime"]["lux neural networks"]["vgg$(depth) bn=$(batchnorm) (compiled)"] = @benchmarkable begin
+        vgg_compiled(x, ps_concrete, st_concrete)
+    end setup = begin
+        vgg = Vision.VGG($depth; pretrained=false, batchnorm=$(batchnorm))
+        ps, st = Lux.setup(Random.default_rng(), vgg)
+        ps_concrete = Reactant.to_rarray(ps)
+        st_concrete = Reactant.to_rarray(Lux.testmode(st))
+        x = Reactant.to_rarray(rand(Float32, 224, 224, 3, 16))
+        vgg_compiled = @compile vgg(x, ps_concrete, st_concrete)
+    end
+end
+
+for version in (:tiny, :base)
+    SUITE["comptime"]["lux neural networks"]["ViT $(version)"] = @benchmarkable begin
+        @compile vit(x, ps_concrete, st_concrete)
+    end setup = begin
+        vit = Vision.ViT($(Meta.quot(version)))
+        ps, st = Lux.setup(Random.default_rng(), vit)
+        ps_concrete = Reactant.to_rarray(ps)
+        st_concrete = Reactant.to_rarray(Lux.testmode(st))
+        x = Reactant.to_rarray(rand(Float32, 256, 256, 3, 16))
+    end
+
+    SUITE["runtime"]["lux neural networks"]["ViT $(version) (compiled)"] = @benchmarkable begin
+        vit_compiled(x, ps_concrete, st_concrete)
+    end setup = begin
+        vit = Vision.ViT($(Meta.quot(version)))
+        ps, st = Lux.setup(Random.default_rng(), vit)
+        ps_concrete = Reactant.to_rarray(ps)
+        st_concrete = Reactant.to_rarray(Lux.testmode(st))
+        x = Reactant.to_rarray(rand(Float32, 256, 256, 3, 16))
+        vit_compiled = @compile vit(x, ps_concrete, st_concrete)
     end
 end
 
