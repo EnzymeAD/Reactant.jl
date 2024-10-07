f55cbbedeb676200670790fc27d838e6b602b415
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-10-07 00:34:17 -0400

    Implement `Base.real`, `Base.imag` for `TracedRNumber`

--- src/TracedRNumber.jl
+++ src/TracedRNumber.jl
@@ -209,6 +209,26 @@ function Base.conj(x::TracedRNumber{T}) where {T<:Complex}
     )
 end
 
+Base.real(x::TracedRNumber) = x
+function Base.real(x::TracedRNumber{Complex{T}}) where {T}
+    return TracedRNumber{T}(
+        (),
+        MLIR.Dialects.MLIR.IR.result(
+            MLIR.Dialects.stablehlo.real(x.mlir_data; result=mlir_type(TracedRNumber{T})), 1
+        ),
+    )
+end
+
+Base.imag(x::TracedRNumber) = x
+function Base.imag(x::TracedRNumber{Complex{T}}) where {T}
+    return TracedRNumber{T}(
+        (),
+        MLIR.Dialects.MLIR.IR.result(
+            MLIR.Dialects.stablehlo.imag(x.mlir_data; result=mlir_type(TracedRNumber{T})), 1
+        ),
+    )
+end
+
 # XXX: Enzyme-MLIR doesn't have `abs` adjoint defined
 Base.abs2(x::TracedRNumber{<:Real}) = x^2
 
