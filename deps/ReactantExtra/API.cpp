a2b871fe7962a380e031307cc600915c3d8c866f
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-10-15 17:37:37 +0200

    Add C-API for `MemoryKind`

--- deps/ReactantExtra/API.cpp
+++ deps/ReactantExtra/API.cpp
@@ -689,17 +689,37 @@ extern "C" const char* ifrt_indexdomain_debug_string(ifrt::IndexDomain* index_do
 }
 
 #pragma mark xla::ifrt::MemoryKind
-// extern "C" ifrt::MemoryKind* ifrt_memorykind() {
-//     return new ifrt::MemoryKind();
-// }
+// Pass a nullptr to create a `MemoryKind` with no memory chosen.
+extern "C" ifrt::MemoryKind* ifrt_memorykind_ctor(const char* memory_kind) {
+    ifrt::MemoryKind tmp{};
+    if (memory_kind != nullptr)
+        tmp = ifrt::MemoryKind(std::string(memory_kind));
+    return new ifrt::MemoryKind(tmp);
+}
 
-// extern "C" ifrt::MemoryKind* ifrt_memorykind_from_string(const char* kind) {
-//     return new ifrt::MemoryKind(std::optional<std::string>{kind});
-// }
+extern "C" void ifrt_memorykind_free(ifrt::MemoryKind* memory_kind) {
+    delete memory_kind;
+}
 
-// extern "C" void ifrt_memorykind_free(ifrt::MemoryKind* memory_kind) {
-//     delete memory_kind;
-// }
+extern "C" bool ifrt_memorykind_eq(ifrt::MemoryKind* mk1, ifrt::MemoryKind* mk2) {
+    return *mk1 == *mk2;
+}
+
+extern "C" bool ifrt_memorykind_ne(ifrt::MemoryKind* mk1, ifrt::MemoryKind* mk2) {
+    return *mk1 != *mk2;
+}
+
+extern "C" const char* ifrt_memorykind_string(ifrt::MemoryKind* memory_kind) {
+    return cstr_from_string(memory_kind->memory_kind());
+}
+
+extern "C" const char* ifrt_memorykind_debug_string(ifrt::MemoryKind* memory_kind) {
+    return cstr_from_string(memory_kind->DebugString());
+}
+
+extern "C" ifrt::MemoryKind* ifrt_memorykind_canonicalize(ifrt::MemoryKind* memory_kind, ifrt::Device* device) {
+    return new ifrt::MemoryKind(CanonicalizeMemoryKind(*memory_kind, device));
+}
 
 #pragma mark xla::ifrt::Memory
 extern "C" ifrt::Memory* ifrt_memory_ctor() {
