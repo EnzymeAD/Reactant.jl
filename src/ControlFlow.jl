882441bc922ad4c3a15e0acbf18c751fb830444a
Avik Pal <avikpal@mit.edu>
2024-10-21 16:35:42 -0400

    feat: use ExpressionsExplorer for parsing

--- src/ControlFlow.jl
+++ src/ControlFlow.jl
@@ -3,11 +3,21 @@ module ControlFlow
 using ..Reactant: Reactant, TracedRNumber, TracedRArray
 using ..MLIR: MLIR
 
+using ExpressionExplorer: ExpressionExplorer
 using MacroTools: MacroTools
 
 macro trace(expr)
     expr.head == :if && return esc(trace_if(__module__, expr))
     return error("Only `if-elseif-else` blocks are currently supported by `@trace`")
+end
+
+function cleanup_expr_to_avoid_boxing(expr, prepend::Symbol, all_vars)
+    return MacroTools.postwalk(expr) do x
+        if x isa Symbol && x ∈ all_vars
+            return Symbol(prepend, x)
+        end
+        return x
+    end
 end
 
 function trace_if(mod, expr)
@@ -16,51 +26,53 @@ function trace_if(mod, expr)
     # XXX: support `elseif` blocks
     @assert expr.args[3].head == :block "`elseif` blocks are not supported yet."
 
-    # `var_list` is a list of input variables that are used in the `if` block
-    # `bound_vars` is a list of variables that are bound in the `if` block
-    true_branch_var_list = Symbol[]
-    true_branch_bound_vars = Symbol[]
-    find_var_uses!(true_branch_var_list, true_branch_bound_vars, expr.args[2])
+    true_branch_symbols = ExpressionExplorer.compute_symbols_state(expr.args[2])
+    true_branch_input_list = [true_branch_symbols.references...]
+    true_branch_assignments = [true_branch_symbols.assignments...]
     true_branch_fn_name = gensym(:true_branch)
 
-    false_branch_var_list = Symbol[]
-    false_branch_bound_vars = Symbol[]
-    find_var_uses!(false_branch_var_list, false_branch_bound_vars, expr.args[3])
+    false_branch_symbols = ExpressionExplorer.compute_symbols_state(expr.args[3])
+    false_branch_input_list = [false_branch_symbols.references...]
+    false_branch_assignments = [false_branch_symbols.assignments...]
     false_branch_fn_name = gensym(:false_branch)
 
-    all_input_vars = true_branch_var_list ∪ false_branch_var_list
-    all_output_vars = true_branch_bound_vars ∪ false_branch_bound_vars
+    all_input_vars = true_branch_input_list ∪ false_branch_input_list
+    all_output_vars = true_branch_assignments ∪ false_branch_assignments
+
+    all_vars = all_input_vars ∪ all_output_vars
 
     true_branch_fn = quote
-        $(true_branch_fn_name) =
-            ($(all_input_vars...),) -> begin
-                $(Expr(:meta, :inline))
-                $(expr.args[2])
-                return ($(all_output_vars...),)
-            end
-    end
-
-    true_branch_fn = MacroTools.prewalk(true_branch_fn) do x
-        if x isa Symbol && x ∈ all_output_vars
-            return Symbol(:true_branch₋, x)
+        () -> begin
+            $(Expr(:meta, :inline))
+            $(expr.args[2])
+            return ($(all_output_vars...),)
         end
-        return x
+    end
+    true_branch_fn = cleanup_expr_to_avoid_boxing(
+        true_branch_fn, true_branch_fn_name, all_vars
+    )
+    true_branch_fn = quote
+        $(true_branch_fn_name) =
+            let $(map(Base.Fix2(makelet, true_branch_fn_name), all_input_vars)...)
+                $(true_branch_fn)
+            end
     end
 
     false_branch_fn = quote
-        $(false_branch_fn_name) =
-            ($(all_input_vars...),) -> begin
-                $(Expr(:meta, :inline))
-                $(expr.args[3])
-                return ($(all_output_vars...),)
-            end
-    end
-
-    false_branch_fn = MacroTools.prewalk(false_branch_fn) do x
-        if x isa Symbol && x ∈ all_output_vars
-            return Symbol(:false_branch₋, x)
+        () -> begin
+            $(Expr(:meta, :inline))
+            $(expr.args[3])
+            return ($(all_output_vars...),)
         end
-        return x
+    end
+    false_branch_fn = cleanup_expr_to_avoid_boxing(
+        false_branch_fn, false_branch_fn_name, all_vars
+    )
+    false_branch_fn = quote
+        $(false_branch_fn_name) =
+            let $(map(Base.Fix2(makelet, false_branch_fn_name), all_input_vars)...)
+                $(false_branch_fn)
+            end
     end
 
     return quote
@@ -68,10 +80,7 @@ function trace_if(mod, expr)
             $(true_branch_fn)
             $(false_branch_fn)
             ($(all_output_vars...),) = $(traced_if)(
-                $(expr.args[1]),
-                $(true_branch_fn_name),
-                $(false_branch_fn_name),
-                ($(all_input_vars...),),
+                $(expr.args[1]), $(true_branch_fn_name), $(false_branch_fn_name)
             )
         else
             $(expr)
@@ -83,28 +92,20 @@ is_traced(x) = false
 is_traced(::TracedRArray) = true
 is_traced(::TracedRNumber) = true
 
-makelet(x) = :($(x) = $(x))
+makelet(x, prepend::Symbol) = :($(Symbol(prepend, x)) = $(x))
 
 # Generate this dummy function and later we remove it during tracing
-function traced_if(cond, true_fn::TFn, false_fn::FFn, args) where {TFn,FFn}
-    if cond
-        return true_fn(args...)
-    else
-        return false_fn(args...)
-    end
+function traced_if(cond, true_fn::TFn, false_fn::FFn) where {TFn,FFn}
+    return cond ? true_fn() : false_fn()
 end
 
-function traced_if(
-    cond::TracedRNumber{Bool}, true_fn::TFn, false_fn::FFn, args
-) where {TFn,FFn}
+function traced_if(cond::TracedRNumber{Bool}, true_fn::TFn, false_fn::FFn) where {TFn,FFn}
     _, true_branch_compiled, true_branch_results, _, _, _, _, _, true_linear_results = Reactant.make_mlir_fn(
-        true_fn, args, (), string(gensym("true_branch")), false;
-        return_dialect=:stablehlo
+        true_fn, (), (), string(gensym("true_branch")), false; return_dialect=:stablehlo
     )
 
     _, false_branch_compiled, false_branch_results, _, _, _, _, _, false_linear_results = Reactant.make_mlir_fn(
-        false_fn, args, (), string(gensym("false_branch")), false;
-        return_dialect=:stablehlo
+        false_fn, (), (), string(gensym("false_branch")), false; return_dialect=:stablehlo
     )
 
     @assert length(true_branch_results) == length(false_branch_results) "true branch returned $(length(true_branch_results)) results, false branch returned $(length(false_branch_results)). This shouldn't happen."
@@ -143,84 +144,6 @@ function traced_if(
         res.mlir_data = MLIR.IR.result(if_compiled, i)
         return res
     end
-end
-
-# XXX: Use `ExpressionExplorer.jl` instead
-# NOTE: Adapted from https://github.com/c42f/FastClosures.jl/blob/master/src/FastClosures.jl
-function find_var_uses!(varlist, bound_vars, ex)
-    if isa(ex, Symbol)
-        var = ex
-        if !(var in bound_vars)
-            var ∈ varlist || push!(varlist, var)
-        end
-        return varlist
-    elseif isa(ex, Expr)
-        if ex.head == :quote || ex.head == :line || ex.head == :inbounds
-            return varlist
-        end
-        if ex.head == :(=)
-            find_var_uses_lhs!(varlist, bound_vars, ex.args[1])
-            find_var_uses!(varlist, bound_vars, ex.args[2])
-        elseif ex.head == :kw
-            find_var_uses!(varlist, bound_vars, ex.args[2])
-        elseif ex.head == :for ||
-            ex.head == :while ||
-            ex.head == :comprehension ||
-            ex.head == :let
-            # New scopes
-            inner_bindings = copy(bound_vars)
-            find_var_uses!(varlist, inner_bindings, ex.args)
-        elseif ex.head == :try
-            # New scope + ex.args[2] is a new binding
-            find_var_uses!(varlist, copy(bound_vars), ex.args[1])
-            catch_bindings = copy(bound_vars)
-            !isa(ex.args[2], Symbol) || push!(catch_bindings, ex.args[2])
-            find_var_uses!(varlist, catch_bindings, ex.args[3])
-            if length(ex.args) > 3
-                finally_bindings = copy(bound_vars)
-                find_var_uses!(varlist, finally_bindings, ex.args[4])
-            end
-        elseif ex.head == :call
-            find_var_uses!(varlist, bound_vars, ex.args[2:end])
-        elseif ex.head == :local
-            foreach(ex.args) do e
-                if !isa(e, Symbol)
-                    find_var_uses!(varlist, bound_vars, e)
-                end
-            end
-        elseif ex.head == :(::)
-            find_var_uses_lhs!(varlist, bound_vars, ex)
-        else
-            find_var_uses!(varlist, bound_vars, ex.args)
-        end
-    end
-    return varlist
-end
-
-function find_var_uses!(varlist, bound_vars, exs::Vector)
-    return foreach(e -> find_var_uses!(varlist, bound_vars, e), exs)
-end
-
-# Find variable uses on the left hand side of an assignment.  Some of what may
-# be variable uses turn into bindings in this context (cf. tuple unpacking).
-function find_var_uses_lhs!(varlist, bound_vars, ex)
-    if isa(ex, Symbol)
-        var = ex
-        var ∈ bound_vars || push!(bound_vars, var)
-    elseif isa(ex, Expr)
-        if ex.head == :tuple
-            find_var_uses_lhs!(varlist, bound_vars, ex.args)
-        elseif ex.head == :(::)
-            find_var_uses!(varlist, bound_vars, ex.args[2])
-            find_var_uses_lhs!(varlist, bound_vars, ex.args[1])
-        else
-            find_var_uses!(varlist, bound_vars, ex.args)
-        end
-    end
-end
-
-function find_var_uses_lhs!(varlist, bound_vars, exs::Vector)
-    return foreach(e -> find_var_uses_lhs!(varlist, bound_vars, e), exs)
 end
 
 export @trace
