adaf47f947034d9faa61c706f2ce9ebf992340b6
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-10-28 09:28:17 +0100

    Fix again depot path env pass to bazel-called julia

--- deps/ReactantExtra/make-bindings.jl
+++ deps/ReactantExtra/make-bindings.jl
@@ -1,13 +1,8 @@
-JULIA_CMD = Base.julia_cmd().exec[1]
-if haskey(ENV, "JULIA_DEPOT_PATH")
-    JULIA_CMD = "JULIA_DEPOT_PATH=$(ENV["JULIA_DEPOT_PATH"]) $(JULIA_CMD)"
-end
-
 function build_file(output_path)
     file = basename(output_path)
     run(
         Cmd(
-            `bazel build --action_env=JULIA=\"$JULIA_CMD\" --repo_env HERMETIC_PYTHON_VERSION="3.10" --check_visibility=false --verbose_failures //:$file`;
+            `bazel build --action_env=JULIA=$(Base.julia_cmd().exec[1]) --action_env=JULIA_DEPOT_PATH=$(Base.DEPOT_PATH) --repo_env HERMETIC_PYTHON_VERSION="3.10" --check_visibility=false --verbose_failures //:$file`;
             dir=@__DIR__,
         ),
     )
