f82ec3cb2917406f1743f0be672544aca8acea9d
Avik Pal <avikpal@mit.edu>
2024-11-02 22:25:27 -0400

    feat: extend support to non-standard dimensions

--- ext/ReactantAbstractFFTsExt.jl
+++ ext/ReactantAbstractFFTsExt.jl
@@ -3,41 +3,77 @@ module ReactantAbstractFFTsExt
 using AbstractFFTs: AbstractFFTs
 using Reactant: Reactant, MLIR, TracedRArray
 
-function contiguous_dims(dims, N)
+function check_contiguous_innermost_dims(dims, N)
+    @assert sort([dims...]) == [dims...] "un-sorted dims are not supported"
     all(i -> dims[i] == dims[i - 1] + 1, 2:(length(dims))) || return false
-    dims[1] != (N - length(dims) + 1) && return false
+    dims[1] != 1 && return false
     return true
+end
+
+function compute_correct_pdims(x::AbstractArray, dims::Int)
+    counter = 0
+    return ntuple(ndims(x)) do i
+        i == 1 && return dims
+        counter += 1
+        return counter
+    end
+end
+
+function compute_correct_pdims(x::AbstractArray, dims)
+    counter = 0
+    return ntuple(ndims(x)) do i
+        i ≤ length(dims) && return dims[i]
+        counter += 1
+        while counter ∈ dims
+            counter += 1
+        end
+        return counter
+    end
 end
 
 for op in (:rfft, :fft, :ifft)
     @eval function AbstractFFTs.$(op)(x::TracedRArray, dims)
         if dims isa Integer
-            if dims != ndims(x)
-                error("Not yet implemented")
+            if dims != 1
+                pdims = compute_correct_pdims(x, dims)
+                return permutedims(
+                    AbstractFFTs.$(op)(permutedims(x, pdims), 1), invperm(pdims)
+                )
             end
-            return generalized_fft(x, $(Meta.quot(op)), nothing, ndims(x) - dims + 1)
+            return generalized_fft(x, $(Meta.quot(op)), nothing, 1)
         end
-        contiguous_dims(dims, ndims(x)) &&
-            return generalized_fft(x, $(Meta.quot(op)), nothing, ndims(x) - first(dims) + 1)
-        return error("Not yet implemented")
+        if !check_contiguous_innermost_dims(dims, ndims(x))
+            pdims = compute_correct_pdims(x, dims)
+            return permutedims(
+                AbstractFFTs.$(op)(permutedims(x, pdims), 1:length(dims)), invperm(pdims)
+            )
+        end
+        return generalized_fft(x, $(Meta.quot(op)), nothing, length(dims))
     end
 end
 
 for op in (:irfft,)
     @eval function AbstractFFTs.$(op)(x::TracedRArray, d::Int, dims)
         if dims isa Integer
-            if dims != ndims(x)
-                error("Not yet implemented")
+            if dims != 1
+                pdims = compute_correct_pdims(x, dims)
+                return permutedims(
+                    AbstractFFTs.$(op)(permutedims(x, pdims), d, 1), invperm(pdims)
+                )
             end
-            return generalized_fft(x, $(Meta.quot(op)), d, ndims(x) - dims + 1)
+            return generalized_fft(x, $(Meta.quot(op)), d, 1)
         end
-        contiguous_dims(dims, ndims(x)) &&
-            return generalized_fft(x, $(Meta.quot(op)), d, ndims(x) - first(dims) + 1)
-        return error("Not yet implemented")
+        if !check_contiguous_innermost_dims(dims, ndims(x))
+            pdims = compute_correct_pdims(x, dims)
+            return permutedims(
+                AbstractFFTs.$(op)(permutedims(x, pdims), d, 1:length(dims)), invperm(pdims)
+            )
+        end
+        return generalized_fft(x, $(Meta.quot(op)), d, length(dims))
     end
 end
 
-function generalized_fft(x::TracedRArray{T,N}, mode::Symbol, d, last_n::Int) where {T,N}
+function generalized_fft(x::TracedRArray{T,N}, mode::Symbol, d, first_n::Int) where {T,N}
     @assert mode ∈ (:rfft, :irfft, :fft, :ifft)
 
     x = permutedims(x, reverse(1:N))
@@ -55,13 +91,13 @@ function generalized_fft(x::TracedRArray{T,N}, mode::Symbol, d, last_n::Int) whe
             rT = T
             res_size = [size(x)...]
         end
-        fft_length = [size(x, i) for i in (ndims(x) - last_n + 1):ndims(x)]
+        fft_length = [size(x, i) for i in (ndims(x) - first_n + 1):ndims(x)]
     else
         @assert mode == :irfft
         @assert T <: Complex
         rT = real(T)
         res_size = [size(x)[1:(end - 1)]..., d]
-        fft_length = [res_size[i] for i in (ndims(x) - last_n + 1):ndims(x)]
+        fft_length = [res_size[i] for i in (ndims(x) - first_n + 1):ndims(x)]
     end
 
     @assert 1 ≤ length(fft_length) ≤ 3 "stablehlo.fft only supports up to rank 3"
