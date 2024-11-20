a50d27c4ed8faa7ececc1c750fab14b0ee0d1b2f
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-11-20 13:36:01 +0100

    fix NaN in test

--- test/ops.jl
+++ test/ops.jl
@@ -536,7 +536,7 @@ end
     @test [-1, 0, 1] == @jit Ops.sign(x)
 
     x = ConcreteRArray([Inf, -Inf, NaN, -NaN, -1.0, -0.0, +0.0, 1.0])
-    @test [1.0, -1.0, NaN, NaN, -1.0, -0.0, 0.0, 1.0] ≈ @jit Ops.sign(x)
+    @test [1.0, -1.0, NaN, NaN, -1.0, -0.0, 0.0, 1.0] ≈ @jit(Ops.sign(x)) nans = true
 
     x = ConcreteRArray([
         NaN + 1.0im, 1.0 + NaN, 0.0 + 0.0im, -1.0 + 2.0im, 0.0 - 3.0im, 1.0 + 4.0im
