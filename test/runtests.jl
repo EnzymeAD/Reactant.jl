7ded1e468ad0620887533881a7937a30f7be439c
William Moses <wsmoses@cyclops.juliacomputing.io>
2024-11-29 17:49:17 -0500

    WIP: kernels

--- test/runtests.jl
+++ test/runtests.jl
@@ -60,17 +60,18 @@ const REACTANT_TEST_GROUP = lowercase(get(ENV, "REACTANT_TEST_GROUP", "all"))
 
     if REACTANT_TEST_GROUP == "all" || REACTANT_TEST_GROUP == "integration"
         @safetestset "Linear Algebra" include("integration/linear_algebra.jl")
+        @safetestset "CUDA" include("cuda.jl")
         @safetestset "AbstractFFTs" include("integration/fft.jl")
     end
 
-    if REACTANT_TEST_GROUP == "all" || REACTANT_TEST_GROUP == "neural_networks"
-        @testset "Neural Networks" begin
-            @safetestset "NNlib Primitives" include("nn/nnlib.jl")
-            @safetestset "Flux.jl Integration" include("nn/flux.jl")
-            if Sys.islinux()
-                @safetestset "LuxLib Primitives" include("nn/luxlib.jl")
-                @safetestset "Lux Integration" include("nn/lux.jl")
-            end
-        end
-    end
+    # if REACTANT_TEST_GROUP == "all" || REACTANT_TEST_GROUP == "neural_networks"
+    #     @testset "Neural Networks" begin
+    #         @safetestset "NNlib Primitives" include("nn/nnlib.jl")
+    #         @safetestset "Flux.jl Integration" include("nn/flux.jl")
+    #         if Sys.islinux()
+    #             @safetestset "LuxLib Primitives" include("nn/luxlib.jl")
+    #             @safetestset "Lux Integration" include("nn/lux.jl")
+    #         end
+    #     end
+    # end
 end
