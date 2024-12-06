ea2f029e2538427436142f447154eea12ce97b6b
Avik Pal <avikpal@mit.edu>
2024-12-06 06:17:48 +0530

    perf: move all benchmarks to buildkite (#327)

--- benchmark/setup.jl
+++ benchmark/setup.jl
@@ -17,6 +17,9 @@ using Zygote: Zygote
 @inline sumabs2(model, x, p, st) = sum(abs2, first(Lux.apply(model, x, p, st)))
 @inline sumabs2(model, x) = sum(abs2, model(x))
 
+@inline sumcos(x) = sum(cos, x)
+@inline ∇sumcos(x) = Enzyme.gradient(Reverse, sumcos, x)
+
 function benchmark_group_to_backend(benchmark_group::String)
     benchmark_group == "CPU" && return CPUDevice()
     benchmark_group == "CUDA" && return CUDADevice()
@@ -34,7 +37,39 @@ end
 function setup_benchmarks!(suite::BenchmarkGroup, backend::String)
     dev = benchmark_group_to_backend(backend)
 
+    # Simple Benchmarks
+    setup_simple_benchmark!(suite, backend)
+
+    # Add Lux Benchmarks
     setup_vit_benchmark!(suite, backend, dev)
+    setup_vgg_benchmark!(suite, backend, dev)
+
+    return nothing
+end
+
+# Some Simple Benchmarks
+function setup_simple_benchmark!(suite::BenchmarkGroup, backend)
+    for opt_pass in (:all, :only_enzyme, :after_enzyme, :before_enzyme)
+        tag = opt_pass == :all ? "Reactant" : "Reactant (optimize = $(Meta.quot(opt_pass)))"
+
+        suite["(Basics) 2D sum (2 x 10)"]["forward (compilation)"][backend][tag] = @benchmarkable begin
+            @compile optimize = $(opt_pass) sum(x)
+        end setup = begin
+            x = Reactant.ConcreteRArray(ones(2, 10))
+        end
+
+        suite["(Basics) sum(cos, x) (2 x 10)"]["forward (compilation)"][backend][tag] = @benchmarkable begin
+            @compile optimize = $(opt_pass) sumcos(x)
+        end setup = begin
+            x = Reactant.ConcreteRArray(ones(2, 10))
+        end
+    end
+
+    suite["Basics ∇sumcos (2 x 10)"]["forward (compilation)"][backend]["Reactant"] = @benchmarkable begin
+        @compile optimize = :all ∇sumcos(x)
+    end setup = begin
+        x = Reactant.ConcreteRArray(ones(2, 10))
+    end
 
     return nothing
 end
@@ -46,6 +81,20 @@ function setup_vit_benchmark!(suite::BenchmarkGroup, backend, dev::AbstractDevic
 
         setup_lux_forward_pass_benchmark!(
             suite, benchmark_name, backend, Vision.ViT(mode), (256, 256, 3, bsize), dev
+        )
+    end
+end
+
+function setup_vgg_benchmark!(suite::BenchmarkGroup, backend, dev::AbstractDevice)
+    for depth in (11, 13, 16, 19), bsize in (4, 16, 32), batchnorm in (false, true)
+        benchmark_name = "VGG$(depth) bn=$(batchnorm) (224 x 224 x 3 x $(bsize))"
+        setup_lux_forward_pass_benchmark!(
+            suite,
+            benchmark_name,
+            backend,
+            Vision.VGG(depth; pretrained=false, batchnorm),
+            (224, 224, 3, bsize),
+            dev,
         )
     end
 end
@@ -86,6 +135,20 @@ function setup_lux_forward_pass_benchmark!(
             apply_compiled = @compile sync = true optimize = $(Meta.quot(opt_pass)) Lux.apply(
                 $model, x_ra, ps_ra, st_test_ra
             )
+            GC.gc()
+            reclaim($dev)
+        end
+
+        suite[benchmark_name]["forward (compilation)"][backend][tag] = @benchmarkable begin
+            @compile optimize = $(opt_pass) Lux.apply($model, x_ra, ps_ra, st_test_ra)
+        end setup = begin
+            GC.gc()
+            reclaim($dev)
+            x, ps, st = general_lux_setup($model, $x_dims)
+            st_test = Lux.testmode(st)
+            x_ra = Reactant.to_rarray(x)
+            ps_ra = Reactant.to_rarray(ps)
+            st_test_ra = Reactant.to_rarray(st_test)
             GC.gc()
             reclaim($dev)
         end
