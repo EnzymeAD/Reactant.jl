aa8f88c6dd10217ca26ad8a762437adf6eb10dbe
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-12-08 21:59:01 +0100

    refactor `Rx`

--- ext/ReactantYaoBlocksExt.jl
+++ ext/ReactantYaoBlocksExt.jl
@@ -18,6 +18,39 @@ function symname(name, ::Type{ParamType}, ::Type{OutElType}) where {ParamType,Ou
 end
 
 function codegen!(
+    ::Val{:rx}, ::Type{ParamType}, ::Type{OutElType}
+) where {ParamType,OutElType}
+    in_tys = [IR.TensorType((), IR.Type(ParamType))]
+    out_tys = [IR.TensorType((2, 2), IR.Type(OutElType))]
+
+    mod = module_top()
+    IR.block!(IR.body(mod)) do
+        f = func.func_(;
+            sym_name=symname("rx", ParamType, OutElType),
+            function_type=IR.FunctionType(in_tys, out_tys),
+            body=IR.Region(),
+        )
+
+        fbody = IR.Block(in_tys, [IR.Location()])
+        push!(IR.region(f, 1), fbody)
+
+        IR.block!(fbody) do
+            θ = TracedRNumber{ParamType}((), IR.argument(fbody, 1))
+            M = Reactant.broadcast_to_size(zero(OutElType), (2, 2))
+            c = cos(θ / 2)
+            s = -im * sin(θ / 2)
+            M[1, 1] = c
+            M[2, 2] = c
+            M[1, 2] = s
+            M[2, 1] = s
+            func.return_([M.mlir_data])
+        end
+
+        return f
+    end
+end
+
+function codegen!(
     ::Val{:ry}, ::Type{ParamType}, ::Type{OutElType}
 ) where {ParamType,OutElType}
     in_tys = [IR.TensorType((), IR.Type(ParamType))]
@@ -95,14 +128,16 @@ function hasfunc(name, ::Type{ParamType}, ::Type{OutElType}) where {ParamType,Ou
 end
 
 function YaoBlocks.mat(::Type{T}, R::RotationGate{D,TracedRNumber{S},<:XGate}) where {D,T,S}
-    M = Reactant.broadcast_to_size(zero(T), (2, 2))
-    c = cos(R.theta / 2)
-    s = -im * sin(R.theta / 2)
-    M[1, 1] = c
-    M[2, 2] = c
-    M[1, 2] = s
-    M[2, 1] = s
-    return M
+    hasfunc("rx", S, T) || codegen!(Val(:rx), S, T)
+
+    res = IR.result(
+        func.call(
+            [R.theta.mlir_data];
+            result_0=[IR.TensorType((2, 2), IR.Type(T))],
+            callee=IR.FlatSymbolRefAttribute(symname("rx", S, T)),
+        ),
+    )
+    return TracedRArray{T,2}((), res, (2, 2))
 end
 
 function YaoBlocks.mat(::Type{T}, R::RotationGate{D,TracedRNumber{S},<:YGate}) where {D,T,S}
