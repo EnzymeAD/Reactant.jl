71c023bd2c965a0da52b9993b9753a01a37db05a
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-12-08 18:59:44 +0100

    reduce codegen on `YaoBlocks.mat` methods

--- ext/ReactantYaoBlocksExt.jl
+++ ext/ReactantYaoBlocksExt.jl
@@ -7,10 +7,12 @@ function YaoBlocks.mat(
     ::Type{T}, R::RotationGate{D,Reactant.TracedRNumber{S},<:XGate}
 ) where {D,T,S}
     M = Reactant.broadcast_to_size(zero(T), (2, 2))
-    M[1, 1] = cos(R.theta / 2)
-    M[2, 2] = cos(R.theta / 2)
-    M[1, 2] = -im * sin(R.theta / 2)
-    M[2, 1] = -im * sin(R.theta / 2)
+    c = cos(R.theta / 2)
+    s = -im * sin(R.theta / 2)
+    M[1, 1] = c
+    M[2, 2] = c
+    M[1, 2] = s
+    M[2, 1] = s
     return M
 end
 
@@ -18,10 +20,12 @@ function YaoBlocks.mat(
     ::Type{T}, R::RotationGate{D,Reactant.TracedRNumber{S},<:YGate}
 ) where {D,T,S}
     M = Reactant.broadcast_to_size(zero(T), (2, 2))
-    M[1, 1] = cos(R.theta / 2)
-    M[2, 2] = cos(R.theta / 2)
-    M[1, 2] = -sin(R.theta / 2)
-    M[2, 1] = sin(R.theta / 2)
+    c = cos(R.theta / 2)
+    s = sin(R.theta / 2)
+    M[1, 1] = c
+    M[2, 2] = c
+    M[1, 2] = -s
+    M[2, 1] = s
     return M
 end
 
@@ -29,8 +33,9 @@ function YaoBlocks.mat(
     ::Type{T}, R::RotationGate{D,Reactant.TracedRNumber{S},<:ZGate}
 ) where {D,T,S}
     M = Reactant.broadcast_to_size(zero(T), (2, 2))
-    M[1, 1] = exp(-im * R.theta / 2)
-    M[2, 2] = exp(im * R.theta / 2)
+    x = exp(im * R.theta / 2)
+    M[1, 1] = conj(x)
+    M[2, 2] = x
     return M
 end
 
