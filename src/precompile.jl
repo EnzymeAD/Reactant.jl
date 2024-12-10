9d2c0d26dcd757092e89152b985c60ffda4bba73
Gaëtan LOUNES <gaetan.lounes@irisa.fr>
2024-12-10 20:53:41 +0100

    try to fix CI

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,9 +2,7 @@ using PrecompileTools: @setup_workload, @compile_workload
 
 @setup_workload begin
     @compile_workload begin
-        Reactant.__init__()
-        cpu = XLA.CPUClient()
-        x = Reactant.ConcreteRArray(randn(Float64, 2, 2); client=cpu)
-        @code_hlo optimize = false sum(x)
+        interp = Reactant.ReactantInterpreter()
+        Base.code_ircode(sum, (Reactant.TracedRArray{Float64,2},); interp)
     end
 end
