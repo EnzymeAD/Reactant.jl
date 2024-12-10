67ca30467518157192c66e73e77404ccc2a36435
Gaëtan LOUNES <gaetan.lounes@irisa.fr>
2024-12-10 21:22:37 +0100

    forgotten init

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,6 +2,7 @@ using PrecompileTools: @setup_workload, @compile_workload
 
 @setup_workload begin
     @compile_workload begin
+        Reactant.__init__()
         interp = Reactant.ReactantInterpreter()
         Base.code_ircode(sum, (Reactant.TracedRArray{Float64,2},); interp)
     end
