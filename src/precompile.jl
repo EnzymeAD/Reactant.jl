9ebf3da39e814cd5e54ec9aa8268fc6a42c7b487
Gaëtan LOUNES <gaetan.lounes@irisa.fr>
2024-12-10 15:12:34 +0100

    keep only one workload

--- src/precompile.jl
+++ src/precompile.jl
@@ -7,9 +7,4 @@ using PrecompileTools: @setup_workload, @compile_workload
         x = Reactant.ConcreteRArray(randn(Float64, 2, 2); client=cpu)
         @code_hlo optimize = false sum(x)
     end
-
-    @compile_workload begin
-        interp = Reactant.ReactantInterpreter()
-        Base.code_ircode(sum, (Reactant.TracedRArray{Float64,2},); interp)
-    end
 end
