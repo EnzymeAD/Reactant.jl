1a1843cfe6f5338dda8e89f92cc4bb8032a4898e
Avik Pal <avikpal@mit.edu>
2024-12-12 20:31:06 +0530

    feat: generalize diagm

--- src/stdlibs/LinearAlgebra.jl
+++ src/stdlibs/LinearAlgebra.jl
@@ -365,15 +365,14 @@ function simple_scatter_op(
     return TracedRArray{T,2}((), res, shape)
 end
 
-# The cartesian version doesn't exist in julia 1.10
+## The cartesian version doesn't exist in julia 1.10
 function diagonal_indices_zero_indexed(m::Integer, n::Integer, k::Integer=0)
-    Cstart = CartesianIndex(1 + max(0, -k), 1 + max(0, k))
-    Cstep = CartesianIndex(1, 1)
-    res = StepRangeLen(Cstart, Cstep, max(0, k <= 0 ? min(m + k, n) : min(m, n - k)))
-    indices = Matrix{Int}(undef, (length(res), 2))
-    for (i, idx) in enumerate(res)
-        indices[i, 1] = idx[1] - 1
-        indices[i, 2] = idx[2] - 1
+    idx1, idx2 = 1 + max(0, -k), 1 + max(0, k)
+    L = max(0, k ≤ 0 ? min(m + k, n) : min(m, n - k))
+    indices = Matrix{Int}(undef, (L, 2))
+    for i in axes(indices, 1)
+        indices[i, 1] = idx1 + i - 2
+        indices[i, 2] = idx2 + i - 2
     end
     return indices
 end
