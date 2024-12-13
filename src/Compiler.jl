9ef7f6fd554a776406aaed080ca238c6e193dff0
jumerckx <julesmerckx12@gmail.com>
2024-12-13 21:08:27 +0100

    actually use ScopedValues...

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -13,7 +13,7 @@ import ..Reactant:
     TracedToConcrete,
     append_path,
     TracedType
-using Base.ScopedValues
+using ScopedValues
 import ReactantCore: enable_tracing
 
 @inline traced_getfield(@nospecialize(obj), field) = Base.getfield(obj, field)
