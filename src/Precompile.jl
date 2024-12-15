f8b79334f03956f08f63a675dd03f1f03ea82cb0
Gaëtan LOUNES <gaetan.lounes@irisa.fr>
2024-12-15 03:47:45 +0100

    Revert "remove CI ENV hack"
    This reverts commit a688556ec469f6bdb0a5b3ec53ede9c8b6bc4ac3.

--- src/Precompile.jl
+++ src/Precompile.jl
@@ -1,6 +1,9 @@
 using PrecompileTools: @setup_workload, @compile_workload
 
 @setup_workload begin
+    @static if haskey(ENV, "REACTANT_TEST_GROUP")
+        return
+    end
     @compile_workload begin
         initialize_dialect()
         cpu = XLA.CPUClient()
