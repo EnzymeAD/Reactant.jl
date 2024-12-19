17a2e12410a0ff985874988719ae41d403136c3e
William Moses <gh@wsmoses.com>
2024-12-18 21:55:11 -0500

    Update ReactantCUDAExt.jl

--- ext/ReactantCUDAExt.jl
+++ ext/ReactantCUDAExt.jl
@@ -461,15 +461,15 @@ Reactant.@reactant_overlay @noinline function CUDA.cufunction(
 end
 
 function __init__()
-   ptr1 = Reactant.XLA.Libdl.dlsym(CUDA.CUDA_Driver_jll.libcuda_handle, "cuLaunchKernel"; throw_error=false)
+   ptr1 = Reactant.XLA.Libdl.dlsym(CUDA.CUDA_Driver_jll.libcuda, "cuLaunchKernel"; throw_error=false)
    if ptr1 === nothing
 	ptr1 = C_NULL
    end
-   ptr2 = Reactant.XLA.Libdl.dlsym(CUDA.CUDA_Driver_jll.libcuda_handle, "cuModuleLoadData"; throw_error=false)
+   ptr2 = Reactant.XLA.Libdl.dlsym(CUDA.CUDA_Driver_jll.libcuda, "cuModuleLoadData"; throw_error=false)
    if ptr2 === nothing
 	ptr2 = C_NULL
    end
-   ptr3 = Reactant.XLA.Libdl.dlsym(CUDA.CUDA_Driver_jll.libcuda_handle, "cuModuleGetFunction"; throw_error=false)
+   ptr3 = Reactant.XLA.Libdl.dlsym(CUDA.CUDA_Driver_jll.libcuda, "cuModuleGetFunction"; throw_error=false)
    if ptr3 === nothing
 	ptr3 = C_NULL
    end
