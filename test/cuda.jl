2ce476016b597f7fbd429dad5c87d4283ebb5393
William Moses <gh@wsmoses.com>
2024-12-18 22:10:27 -0500

    Update cuda.jl

--- test/cuda.jl
+++ test/cuda.jl
@@ -18,10 +18,7 @@ end
 @testset "Square Kernel" begin
     oA = collect(1:1:64)
     A = Reactant.to_rarray(oA)
-    @show @code_hlo optimize=false square!(A)
-    @show @code_hlo optimize=:before_kernel square!(A)
-    @show @code_hlo square!(A)
-    func = @compile square!(A)
+    func! = @compile square!(A)
     func!(A)
     @test all(Array(A) .≈ (oA .* oA))
 end
