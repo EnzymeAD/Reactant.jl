7ae97645386a892fb141fcde1c2a4c39d981060f
William S. Moses <gh@wsmoses.com>
2024-12-20 12:37:27 -0500

    restore testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -41,4 +41,37 @@ end
 
 const REACTANT_TEST_GROUP = lowercase(get(ENV, "REACTANT_TEST_GROUP", "all"))
 
-include("cuda.jl")
+@testset "Reactant.jl Tests" begin
+    if REACTANT_TEST_GROUP == "all" || REACTANT_TEST_GROUP == "core"
+        @safetestset "Layout" include("layout.jl")
+        @safetestset "Tracing" include("tracing.jl")
+        @safetestset "Basic" include("basic.jl")
+        @safetestset "Autodiff" include("autodiff.jl")
+        @safetestset "Complex" include("complex.jl")
+        @safetestset "Broadcast" include("bcast.jl")
+        @safetestset "Struct" include("struct.jl")
+        @safetestset "Closure" include("closure.jl")
+        @safetestset "Compile" include("compile.jl")
+        @safetestset "Buffer Donation" include("buffer_donation.jl")
+        @safetestset "Shortcuts to MLIR ops" include("ops.jl")
+        @safetestset "Wrapped Arrays" include("wrapped_arrays.jl")
+        @safetestset "Control Flow" include("control_flow.jl")
+    end
+
+    if REACTANT_TEST_GROUP == "all" || REACTANT_TEST_GROUP == "integration"
+        @safetestset "Linear Algebra" include("integration/linear_algebra.jl")
+        @safetestset "AbstractFFTs" include("integration/fft.jl")
+        @safetestset "Random" include("integration/random.jl")
+    end
+
+    if REACTANT_TEST_GROUP == "all" || REACTANT_TEST_GROUP == "neural_networks"
+        @testset "Neural Networks" begin
+            @safetestset "NNlib Primitives" include("nn/nnlib.jl")
+            @safetestset "Flux.jl Integration" include("nn/flux.jl")
+            if Sys.islinux()
+                @safetestset "LuxLib Primitives" include("nn/luxlib.jl")
+                @safetestset "Lux Integration" include("nn/lux.jl")
+            end
+        end
+    end
+end
