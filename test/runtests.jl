6afa9433ee4057ae5fc25d5ce2e8f3a78f3ce87c
Avik Pal <avikpal@mit.edu>
2024-12-21 23:28:44 +0530

    refactor: move python testing to integration (#415)

--- test/runtests.jl
+++ test/runtests.jl
@@ -56,13 +56,13 @@ const REACTANT_TEST_GROUP = lowercase(get(ENV, "REACTANT_TEST_GROUP", "all"))
         @safetestset "Shortcuts to MLIR ops" include("ops.jl")
         @safetestset "Wrapped Arrays" include("wrapped_arrays.jl")
         @safetestset "Control Flow" include("control_flow.jl")
-        @safetestset "Python" include("python.jl")
     end
 
     if REACTANT_TEST_GROUP == "all" || REACTANT_TEST_GROUP == "integration"
         @safetestset "Linear Algebra" include("integration/linear_algebra.jl")
         @safetestset "AbstractFFTs" include("integration/fft.jl")
         @safetestset "Random" include("integration/random.jl")
+        @safetestset "Python" include("integration/python.jl")
     end
 
     if REACTANT_TEST_GROUP == "all" || REACTANT_TEST_GROUP == "neural_networks"
