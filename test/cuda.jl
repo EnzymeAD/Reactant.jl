f8d8c95726b79c3a78ca895fb6f9052f9d9f4157
William Moses <wsmoses@cyclops.juliacomputing.io>
2024-12-23 14:35:15 -0500

    Cuda kernel v2

--- test/cuda.jl
+++ test/cuda.jl
@@ -2,9 +2,16 @@ using Reactant
 using Test
 using CUDA
 
+using Reactant_jll
+@show Reactant_jll.libReactantExtra_path
+
 function square_kernel!(x)
-    i = threadIdx().x
-    x[i] *= x[i]
+    #i = threadIdx().x
+    #x[i] *= x[i]
+    #@cuprintf("overwrote value of %f was thrown during kernel execution on thread (%d, %d, %d) in block (%d, %d, %d).\n",
+    #	      0.0, threadIdx().x, threadIdx().y, threadIdx().z, blockIdx().x, blockIdx().y, blockIdx().z)
+	      #x[i], threadIdx().x, threadIdx().y, threadIdx().z, blockIdx().x, blockIdx().y, blockIdx().z)
+
     # sync_threads()
     return nothing
 end
@@ -18,9 +25,9 @@ end
 @testset "Square Kernel" begin
     oA = collect(1:1:64)
     A = Reactant.to_rarray(oA)
-    @show @code_hlo optimize = false square!(A)
-    @show @code_hlo optimize=:before_kernel square!(A)
-    @show @code_hlo square!(A)
+    #@show @code_hlo optimize = false square!(A)
+    #@show @code_hlo optimize=:before_kernel square!(A)
+    #@show @code_hlo square!(A)
     func! = @compile square!(A)
     func!(A)
     @show A
