1c8b1294d1529f6a0a3829c849c1b5a88af4eb08
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-12-28 23:40:37 +0100

    run testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -63,6 +63,7 @@ const REACTANT_TEST_GROUP = lowercase(get(ENV, "REACTANT_TEST_GROUP", "all"))
         @safetestset "AbstractFFTs" include("integration/fft.jl")
         @safetestset "Random" include("integration/random.jl")
         @safetestset "Python" include("integration/python.jl")
+        @safetestset "YaoBlocks" include("integration/yao.jl")
     end
 
     if REACTANT_TEST_GROUP == "all" || REACTANT_TEST_GROUP == "neural_networks"
