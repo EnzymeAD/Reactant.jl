d4e7c762635fc4e5852d9385d94d6a56270e6eab
William Moses <gh@wsmoses.com>
2024-12-28 23:05:51 -0500

    CUDA kernels take 3 (#427)
    * CUDA take 3
    
    * conditional run cuda
    
    * Update test/integration/cuda.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * bump enzymexla
    
    * fix
    
    * fix gpu reg
    
    * Update BUILD
    
    * Update BUILD
    
    * Update Project.toml
    
    * Update ReactantCUDAExt.jl
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix reactant method blocker
    
    * Update ReactantCUDAExt.jl
    
    * only do compile
    
    * use names in cache
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * cleanup further gc issues
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ test/integration/cuda.jl
@@ -0,0 +1,29 @@
+using Reactant
+using Test
+using CUDA
+
+function square_kernel!(x, y)
+    i = threadIdx().x
+    x[i] *= y[i]
+    sync_threads()
+    return nothing
+end
+
+# basic squaring on GPU
+function square!(x, y)
+    @cuda blocks = 1 threads = length(x) square_kernel!(x, y)
+    return nothing
+end
+
+@testset "Square Kernel" begin
+    oA = collect(1:1:64)
+    A = Reactant.to_rarray(oA)
+    B = Reactant.to_rarray(100 .* oA)
+    if CUDA.functional()
+        @jit square!(A, B)
+        @test all(Array(A) .≈ (oA .* oA .* 100))
+        @test all(Array(B) .≈ (oA .* 100))
+    else
+        @code_hlo optimize = :before_kernel square!(A, B)
+    end
+end
