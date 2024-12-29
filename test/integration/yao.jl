d54bf26f8a3484c466a2ccff51d258092b27dcba
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2024-12-29 14:53:41 +0100

    Revert last commit

--- test/integration/yao.jl
+++ test/integration/yao.jl
@@ -1,13 +1,11 @@
 using YaoBlocks, Reactant
 
 @testset "YaoBlocks" begin
-    if !(Sys.isapple() && Sys.ARCH === :x86_64)
-        f(θ) = mat(Rx(θ))
+    f(θ) = mat(Rx(θ))
 
-        x = ConcreteRNumber(0.0)
-        @test @jit(f(x)) ≈ f(0.0)
+    x = ConcreteRNumber(0.0)
+    @test @jit(f(x)) ≈ f(0.0)
 
-        x = ConcreteRNumber(1.0)
-        @test @jit(f(x)) ≈ f(1.0)
-    end
+    x = ConcreteRNumber(1.0)
+    @test @jit(f(x)) ≈ f(1.0)
 end
