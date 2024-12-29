eef365907676ea825caddc4ed2a27c5e3f2c43c5
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-12-29 02:08:43 +0100

    skip test on macOS x64

--- test/integration/yao.jl
+++ test/integration/yao.jl
@@ -1,11 +1,13 @@
 using YaoBlocks, Reactant
 
 @testset "YaoBlocks" begin
-    f(θ) = mat(Rx(θ))
+    if !(Sys.isapple() && Sys.ARCH === :x86_64)
+        f(θ) = mat(Rx(θ))
 
-    x = ConcreteRNumber(0.0)
-    @test @jit(f(x)) ≈ f(0.0)
+        x = ConcreteRNumber(0.0)
+        @test @jit(f(x)) ≈ f(0.0)
 
-    x = ConcreteRNumber(1.0)
-    @test @jit(f(x)) ≈ f(1.0)
+        x = ConcreteRNumber(1.0)
+        @test @jit(f(x)) ≈ f(1.0)
+    end
 end
