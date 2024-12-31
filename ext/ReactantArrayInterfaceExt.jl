4518113cce137207c8ced570a9acf395c4064794
Avik Pal <avikpal@mit.edu>
2024-12-31 07:32:16 -0500

    Update ext/ReactantArrayInterfaceExt.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/ReactantArrayInterfaceExt.jl
+++ ext/ReactantArrayInterfaceExt.jl
@@ -7,7 +7,8 @@ using Reactant:
 ArrayInterface.can_setindex(::Type{<:RArray}) = false
 ArrayInterface.fast_scalar_indexing(::Type{<:RArray}) = false
 
-for aType in (AbstractArray{<:ConcreteRNumber},AbstractArray{<:TracedRNumber}, TracedRArray)
+for aType in
+    (AbstractArray{<:ConcreteRNumber}, AbstractArray{<:TracedRNumber}, TracedRArray)
     @eval ArrayInterface.aos_to_soa(x::$aType) = Reactant.aos_to_soa(x)
 end
 
