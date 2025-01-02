976f083c26d0059aaa1f5a36abbfb7e93f966085
William Moses <gh@wsmoses.com>
2025-01-02 00:22:54 -0500

    Update autodiff.jl

--- test/autodiff.jl
+++ test/autodiff.jl
@@ -123,10 +123,9 @@ end
 
 @testset "Nested AD" begin
     x = ConcreteRNumber(3.1)
-    f(x) = x^4
+    f(x) = x*x*x*x
     df(x) = Enzyme.gradient(Reverse, f, x)[1]
     @test @jit df(x) ≈ 4 * 3.1^3
-    ddf(x) = Enzyme.gradient(Reverse, df, x)[1]
     ddf(x) = Enzyme.gradient(Reverse, df, x)[1]
     @test @jit ddf(x) ≈ 4 * 3 * 3.1^2
 end
