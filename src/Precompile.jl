598860ab4175737115ef02514488003407333880
William S. Moses <gh@wsmoses.com>
2025-01-03 18:16:23 -0500

    more attempts

--- src/Precompile.jl
+++ src/Precompile.jl
@@ -1,16 +1,46 @@
 using PrecompileTools: @setup_workload, @compile_workload
 
+function infer_sig(sig)
+    interp = ReactantInterpreter()
+
+    min_world = Ref{UInt}(typemin(UInt))
+    max_world = Ref{UInt}(typemax(UInt))
+
+    lookup_result = Reactant.lookup_world(
+        sig, interp.world, Core.Compiler.method_table(interp), min_world, max_world
+    )
+    match = lookup_result::Core.MethodMatch
+    # look up the method and code instance
+    mi = ccall(
+        :jl_specializations_get_linfo,
+        Ref{Core.MethodInstance},
+        (Any, Any, Any),
+        match.method,
+        match.spec_types,
+        match.sparams,
+    )
+
+    @static if VERSION < v"1.11"
+        # For older Julia versions, we vendor in some of the code to prevent
+        # having to build the MethodInstance twice.
+        result = CC.InferenceResult(mi, CC.typeinf_lattice(interp))
+        frame = CC.InferenceState(result, :no, interp)
+        @assert !isnothing(frame)
+        CC.typeinf(interp, frame)
+        ir = CC.run_passes(frame.src, CC.OptimizationState(frame, interp), result, nothing)
+        rt = CC.widenconst(CC.ignorelimited(result.result))
+    else
+        ir, rt = CC.typeinf_ircode(interp, mi, nothing)
+    end
+end
+
 @setup_workload begin
     initialize_dialect()
-    client = XLA.CPUClient(; checkcount=false)
     @compile_workload begin
-        x = ConcreteRNumber(2.0; client)
-        Reactant.compile(sin, (x,); client)
-        # interp = Reactant.ReactantInterpreter()
-        # Base.code_ircode(Base.sin, (Reactant.TracedRNumber{Float64},); interp)
+        # infer_sig(Tuple{typeof(Base.sum), Reactant.TracedRArray{Float64, 2}})
+
+        infer_sig(Tuple{typeof(Base.sin), Reactant.TracedRNumber{Float64}})
     end
-    XLA.free_client(client)
-    client.client = C_NULL
     deinitialize_dialect()
     for v in oc_capture_vec
         if v isa Base.RefValue
