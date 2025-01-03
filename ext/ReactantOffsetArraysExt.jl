98c25ef32bfe50c9ac405766513a727cecf6332c
William Moses <gh@wsmoses.com>
2025-01-03 14:55:24 -0500

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/ReactantOffsetArraysExt.jl
+++ ext/ReactantOffsetArraysExt.jl
@@ -4,10 +4,10 @@ using OffsetArrays: OffsetArray
 using Reactant: Reactant, MLIR, Ops, TracedRArray
 
 function Reactant.traced_type(
-    ::Type{<:OffsetArray{<:Any,N, T}}, seen::ST, ::Val{mode}, track_numbers
-) where {T, N,ST,mode}
+    ::Type{<:OffsetArray{<:Any,N,T}}, seen::ST, ::Val{mode}, track_numbers
+) where {T,N,ST,mode}
     T2 = Reactant.traced_type(T, seen, Val(mode), track_numbers)
-    return OffsetArray{eltype(T2), N, T2}
+    return OffsetArray{eltype(T2),N,T2}
 end
 
 end
