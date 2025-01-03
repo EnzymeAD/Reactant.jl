a94fea827504e7dc84f790f4dfac90203ff533ea
Billy Moses <wmoses@google.com>
2025-01-03 14:52:31 -0500

    Fix offsetarrays support

---
+++ ext/ReactantOffsetArraysExt.jl
@@ -0,0 +1,13 @@
+module ReactantOffsetArraysExt
+
+using OffsetArrays: OffsetArray
+using Reactant: Reactant, MLIR, Ops, TracedRArray
+
+function Reactant.traced_type(
+    ::Type{<:OffsetArray{<:Any,N, T}}, seen::ST, ::Val{mode}, track_numbers
+) where {T, N,ST,mode}
+    T2 = Reactant.traced_type(T, seen, Val(mode), track_numbers)
+    return OffsetArray{eltype(T2), N, T2}
+end
+
+end
