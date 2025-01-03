4031e9cff412daae24c6cc1925ef1c67d944eff6
William S. Moses <gh@wsmoses.com>
2025-01-03 16:12:04 -0500

    de initialize opaque closure cache

--- src/Precompile.jl
+++ src/Precompile.jl
@@ -7,4 +7,12 @@ using PrecompileTools: @setup_workload, @compile_workload
         Base.code_ircode(sum, (Reactant.TracedRArray{Float64,2},); interp)
     end
     deinitialize_dialect()
+    for v in oc_capture_vec
+        if v isa Base.RefValue
+            p = Ptr{Ptr{Cvoid}}(pointer_from_objref(r))
+            Base.atomic_pointerset(p, C_NULL, :monotonic)
+        else
+            empty!(v)
+        end
+    end
 end
