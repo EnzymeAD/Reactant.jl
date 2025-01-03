e1dbc0602034b98e76d7b71c98de3e1679f6a543
Avik Pal <avikpal@mit.edu>
2025-01-02 20:45:06 -0500

    refactor: move into a function

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -113,204 +113,188 @@ function create_result(
 end
 
 # Optimization passes via transform dialect
-const transform_passes_list = [
-    "patterns=compare_op_canon<16>",
-    "transpose_transpose<16>",
-    "broadcast_in_dim_op_canon<16>",
-    "convert_op_canon<16>",
-    "dynamic_broadcast_in_dim_op_not_actually_dynamic<16>",
-    "chained_dynamic_broadcast_in_dim_canonicalization<16>",
-    "dynamic_broadcast_in_dim_all_dims_non_expanding<16>",
-    "noop_reduce_op_canon<16>",
-    "empty_reduce_op_canon<16>",
-    "dynamic_reshape_op_canon<16>",
-    "get_tuple_element_op_canon<16>",
-    "real_op_canon<16>",
-    "imag_op_canon<16>",
-    "conj_complex_negate<16>",
-    "get_dimension_size_op_canon<16>",
-    "gather_op_canon<16>",
-    "reshape_op_canon<16>",
-    "merge_consecutive_reshapes<16>",
-    "transpose_is_reshape<16>",
-    "zero_extent_tensor_canon<16>",
-    "reorder_elementwise_and_shape_op<16>",
-    "cse_broadcast_in_dim<16>",
-    "cse_slice<16>",
-    "cse_transpose<16>",
-    "cse_convert<16>",
-    "cse_pad<16>",
-    "cse_dot_general<16>",
-    "cse_reshape<16>",
-    "cse_mul<16>",
-    "cse_div<16>",
-    "cse_add<16>",
-    "cse_subtract<16>",
-    "cse_min<16>",
-    "cse_max<16>",
-    "cse_neg<16>",
-    "cse_concatenate<16>",
-    "concatenate_op_canon<16>(1024)",
-    "select_op_canon<16>(1024)",
-    "add_simplify<16>",
-    "sub_simplify<16>",
-    "and_simplify<16>",
-    "max_simplify<16>",
-    "min_simplify<16>",
-    "or_simplify<16>",
-    "negate_simplify<16>",
-    "mul_simplify<16>",
-    "div_simplify<16>",
-    "rem_simplify<16>",
-    "pow_simplify<16>",
-    "sqrt_simplify<16>",
-    "cos_simplify<16>",
-    "sin_simplify<16>",
-    "noop_slice<16>",
-    "noop_reverse<16>",
-    "const_prop_through_barrier<16>",
-    "slice_slice<16>",
-    "shift_right_logical_simplify<16>",
-    "pad_simplify<16>",
-    "negative_pad_to_slice<16>",
-    "tanh_simplify<16>",
-    "exp_simplify<16>",
-    "slice_simplify<16>",
-    "convert_simplify<16>",
-    "dynamic_slice_to_static<16>",
-    "dynamic_update_slice_elim<16>",
-    "concat_to_broadcast<16>",
-    "reduce_to_reshape<16>",
-    "broadcast_to_reshape<16>",
-    "gather_simplify<16>",
-    "iota_simplify<16>(1024)",
-    "broadcast_in_dim_simplify<16>(1024)",
-    "convert_concat<1>",
-    "dynamic_update_to_concat<1>",
-    "slice_of_dynamic_update<1>",
-    "slice_elementwise<1>",
-    "slice_pad<1>",
-    "dot_reshape_dot<1>",
-    "concat_const_prop<1>",
-    "concat_fuse<1>",
-    "pad_reshape_pad<1>",
-    "pad_pad<1>",
-    "concat_push_binop_add<1>",
-    "concat_push_binop_mul<1>",
-    "scatter_to_dynamic_update_slice<1>",
-    "reduce_concat<1>",
-    "slice_concat<1>",
-    "concat_slice<1>",
-    "bin_broadcast_splat_add<1>",
-    "bin_broadcast_splat_subtract<1>",
-    "bin_broadcast_splat_div<1>",
-    "bin_broadcast_splat_mul<1>",
-    "reshape_iota<16>",
-    "slice_reshape_slice<1>",
-    "dot_general_simplify<16>",
-    "transpose_simplify<16>",
-    "reshape_empty_broadcast<1>",
-    "add_pad_pad_to_concat<1>",
-    "broadcast_reshape<1>",
-    "slice_reshape_concat<1>",
-    "slice_reshape_elementwise<1>",
-    "slice_reshape_transpose<1>",
-    "slice_reshape_dot_general<1>",
-    "concat_pad<1>",
-    "reduce_pad<1>",
-    "broadcast_pad<1>",
-    "zero_product_reshape_pad<1>",
-    "mul_zero_pad<1>",
-    "div_zero_pad<1>",
-    "binop_const_reshape_pad<1>",
-    "binop_const_pad_add<1>",
-    "binop_const_pad_subtract<1>",
-    "binop_const_pad_mul<1>",
-    "binop_const_pad_div<1>",
-    "slice_reshape_pad<1>",
-    "binop_binop_pad_pad_add<1>",
-    "binop_binop_pad_pad_mul<1>",
-    "binop_pad_pad_add<1>",
-    "binop_pad_pad_subtract<1>",
-    "binop_pad_pad_mul<1>",
-    "binop_pad_pad_div<1>",
-    "binop_pad_pad_min<1>",
-    "binop_pad_pad_max<1>",
-    "unary_pad_push_convert<1>",
-    "unary_pad_push_tanh<1>",
-    "unary_pad_push_exp<1>",
-    "transpose_pad<1>",
-    "transpose_dot_reorder<1>",
-    "dot_transpose<1>",
-    "transpose_einsum<1>",
-    "einsum_transpose<1>",
-    "transpose_convolution<1>",
-    "convolution_transpose<1>",
-    "convert_convert_float<1>",
-    "concat_to_pad<1>",
-    "concat_appending_reshape<1>",
-    "reshape_iota<1>",
-    "broadcast_reduce<1>",
-    "slice_dot_general<1>",
-    "dot_reshape_pad<1>",
-    "pad_dot_general<1>(0)",
-    "dot_reshape_pad<1>",
-    "pad_dot_general<1>(1)",
-    "if_inline<1>",
-    "if_to_select<1>",
-    "dynamic_update_slice_const_prop",
-    "dynamic_gather_op_is_not_dynamic<16>",
-    "binary_op_transpose_simplify_add",
-    "binary_op_transpose_simplify_sub",
-    "binary_op_transpose_simplify_mul",
-    "binary_op_transpose_simplify_div",
-    "binary_op_transpose_simplify_min",
-    "binary_op_transpose_simplify_max",
-    "binary_op_transpose_simplify_pow",
-    "binary_op_transpose_simplify_rem",
-    "binary_op_transpose_simplify_or",
-    "binary_op_transpose_simplify_and",
-    "binary_op_transpose_simplify_xor",
-    "replace_neg_add_with_subtract",
-]
-
-const no_nan_transform_passes_list = [
-    "no_nan", "no_nan_self_sub_simplify", "no_nan_add_sub_simplify"
-]
-
-const transform_passes::String = join(
-    [
-        "enzyme-hlo-generate-td{" * join(transform_passes_list, ';') * "}",
-        "transform-interpreter",
-        "enzyme-hlo-remove-transform",
-    ],
-    ",",
-)
-const no_nan_transform_passes::String = join(
-    [
-        "enzyme-hlo-generate-td{" *
-        join(vcat(transform_passes_list, no_nan_transform_passes_list), ';') *
-        "}",
-        "transform-interpreter",
-        "enzyme-hlo-remove-transform",
-    ],
-    ",",
-)
-
-# Optimization passes which apply to an individual function
-const func_passes::String = join(
-    ["canonicalize,cse", "canonicalize", transform_passes], ","
-)
-const no_nan_func_passes::String = join(
-    ["canonicalize,cse", "canonicalize", no_nan_transform_passes], ","
-)
-
-const opt_passes::String = join(
-    ["inline{default-pipeline=canonicalize max-iterations=4}", func_passes], ','
-)
-const no_nan_opt_passes::String = join(
-    ["inline{default-pipeline=canonicalize max-iterations=4}", no_nan_func_passes], ','
-)
+function optimization_passes(; no_nan::Bool=false)
+    transform_passes_list = [
+        "patterns=compare_op_canon<16>",
+        "transpose_transpose<16>",
+        "broadcast_in_dim_op_canon<16>",
+        "convert_op_canon<16>",
+        "dynamic_broadcast_in_dim_op_not_actually_dynamic<16>",
+        "chained_dynamic_broadcast_in_dim_canonicalization<16>",
+        "dynamic_broadcast_in_dim_all_dims_non_expanding<16>",
+        "noop_reduce_op_canon<16>",
+        "empty_reduce_op_canon<16>",
+        "dynamic_reshape_op_canon<16>",
+        "get_tuple_element_op_canon<16>",
+        "real_op_canon<16>",
+        "imag_op_canon<16>",
+        "conj_complex_negate<16>",
+        "get_dimension_size_op_canon<16>",
+        "gather_op_canon<16>",
+        "reshape_op_canon<16>",
+        "merge_consecutive_reshapes<16>",
+        "transpose_is_reshape<16>",
+        "zero_extent_tensor_canon<16>",
+        "reorder_elementwise_and_shape_op<16>",
+        "cse_broadcast_in_dim<16>",
+        "cse_slice<16>",
+        "cse_transpose<16>",
+        "cse_convert<16>",
+        "cse_pad<16>",
+        "cse_dot_general<16>",
+        "cse_reshape<16>",
+        "cse_mul<16>",
+        "cse_div<16>",
+        "cse_add<16>",
+        "cse_subtract<16>",
+        "cse_min<16>",
+        "cse_max<16>",
+        "cse_neg<16>",
+        "cse_concatenate<16>",
+        "concatenate_op_canon<16>(1024)",
+        "select_op_canon<16>(1024)",
+        "add_simplify<16>",
+        "sub_simplify<16>",
+        "and_simplify<16>",
+        "max_simplify<16>",
+        "min_simplify<16>",
+        "or_simplify<16>",
+        "negate_simplify<16>",
+        "mul_simplify<16>",
+        "div_simplify<16>",
+        "rem_simplify<16>",
+        "pow_simplify<16>",
+        "sqrt_simplify<16>",
+        "cos_simplify<16>",
+        "sin_simplify<16>",
+        "noop_slice<16>",
+        "noop_reverse<16>",
+        "const_prop_through_barrier<16>",
+        "slice_slice<16>",
+        "shift_right_logical_simplify<16>",
+        "pad_simplify<16>",
+        "negative_pad_to_slice<16>",
+        "tanh_simplify<16>",
+        "exp_simplify<16>",
+        "slice_simplify<16>",
+        "convert_simplify<16>",
+        "dynamic_slice_to_static<16>",
+        "dynamic_update_slice_elim<16>",
+        "concat_to_broadcast<16>",
+        "reduce_to_reshape<16>",
+        "broadcast_to_reshape<16>",
+        "gather_simplify<16>",
+        "iota_simplify<16>(1024)",
+        "broadcast_in_dim_simplify<16>(1024)",
+        "convert_concat<1>",
+        "dynamic_update_to_concat<1>",
+        "slice_of_dynamic_update<1>",
+        "slice_elementwise<1>",
+        "slice_pad<1>",
+        "dot_reshape_dot<1>",
+        "concat_const_prop<1>",
+        "concat_fuse<1>",
+        "pad_reshape_pad<1>",
+        "pad_pad<1>",
+        "concat_push_binop_add<1>",
+        "concat_push_binop_mul<1>",
+        "scatter_to_dynamic_update_slice<1>",
+        "reduce_concat<1>",
+        "slice_concat<1>",
+        "concat_slice<1>",
+        "bin_broadcast_splat_add<1>",
+        "bin_broadcast_splat_subtract<1>",
+        "bin_broadcast_splat_div<1>",
+        "bin_broadcast_splat_mul<1>",
+        "reshape_iota<16>",
+        "slice_reshape_slice<1>",
+        "dot_general_simplify<16>",
+        "transpose_simplify<16>",
+        "reshape_empty_broadcast<1>",
+        "add_pad_pad_to_concat<1>",
+        "broadcast_reshape<1>",
+        "slice_reshape_concat<1>",
+        "slice_reshape_elementwise<1>",
+        "slice_reshape_transpose<1>",
+        "slice_reshape_dot_general<1>",
+        "concat_pad<1>",
+        "reduce_pad<1>",
+        "broadcast_pad<1>",
+        "zero_product_reshape_pad<1>",
+        "mul_zero_pad<1>",
+        "div_zero_pad<1>",
+        "binop_const_reshape_pad<1>",
+        "binop_const_pad_add<1>",
+        "binop_const_pad_subtract<1>",
+        "binop_const_pad_mul<1>",
+        "binop_const_pad_div<1>",
+        "slice_reshape_pad<1>",
+        "binop_binop_pad_pad_add<1>",
+        "binop_binop_pad_pad_mul<1>",
+        "binop_pad_pad_add<1>",
+        "binop_pad_pad_subtract<1>",
+        "binop_pad_pad_mul<1>",
+        "binop_pad_pad_div<1>",
+        "binop_pad_pad_min<1>",
+        "binop_pad_pad_max<1>",
+        "unary_pad_push_convert<1>",
+        "unary_pad_push_tanh<1>",
+        "unary_pad_push_exp<1>",
+        "transpose_pad<1>",
+        "transpose_dot_reorder<1>",
+        "dot_transpose<1>",
+        "transpose_einsum<1>",
+        "einsum_transpose<1>",
+        "transpose_convolution<1>",
+        "convolution_transpose<1>",
+        "convert_convert_float<1>",
+        "concat_to_pad<1>",
+        "concat_appending_reshape<1>",
+        "reshape_iota<1>",
+        "broadcast_reduce<1>",
+        "slice_dot_general<1>",
+        "dot_reshape_pad<1>",
+        "pad_dot_general<1>(0)",
+        "dot_reshape_pad<1>",
+        "pad_dot_general<1>(1)",
+        "if_inline<1>",
+        "if_to_select<1>",
+        "dynamic_update_slice_const_prop",
+        "dynamic_gather_op_is_not_dynamic<16>",
+        "binary_op_transpose_simplify_add",
+        "binary_op_transpose_simplify_sub",
+        "binary_op_transpose_simplify_mul",
+        "binary_op_transpose_simplify_div",
+        "binary_op_transpose_simplify_min",
+        "binary_op_transpose_simplify_max",
+        "binary_op_transpose_simplify_pow",
+        "binary_op_transpose_simplify_rem",
+        "binary_op_transpose_simplify_or",
+        "binary_op_transpose_simplify_and",
+        "binary_op_transpose_simplify_xor",
+        "replace_neg_add_with_subtract",
+    ]
+    if no_nan
+        append!(
+            transform_passes_list,
+            ["no_nan", "no_nan_self_sub_simplify", "no_nan_add_sub_simplify"],
+        )
+    end
+    transform_passes = join(
+        [
+            "enzyme-hlo-generate-td{" * join(transform_passes_list, ';') * "}",
+            "transform-interpreter",
+            "enzyme-hlo-remove-transform",
+        ],
+        ",",
+    )
+    func_passes = join(
+        ["canonicalize", "cse", "canonicalize", transform_passes], ","
+    )
+    return join(
+        ["inline{default-pipeline=canonicalize max-iterations=4}", func_passes], ','
+    )
+end
 
 # TODO we want to be able to run the more advanced passes via transform dialect as an enzyme intermediate
 # However, this errs as we cannot attach the transform with to the funcop itself [as we run a functionpass].
@@ -374,10 +358,10 @@ function compile_mlir!(mod, f, args; optimize::Union{Bool,Symbol}=true, no_nan::
     end
     kern = "lower-kernel{run_init=true toolkitPath=$toolkit cuLaunchKernelPtr=$(cuLaunch[]) cuModuleLoadDataPtr=$(cuModule[]) cuModuleGetFunctionPtr=$(cuFunc[])}"
 
-    _opt_passes = no_nan ? no_nan_opt_passes : opt_passes
+    opt_passes = optimization_passes(; no_nan)
 
     if optimize === :all
-        run_pass_pipeline!(mod, join([_opt_passes, "enzyme-batch", _opt_passes], ","))
+        run_pass_pipeline!(mod, join([opt_passes, "enzyme-batch", opt_passes], ","))
         run_pass_pipeline!(
             mod, "$enzyme_pass,arith-raise{stablehlo=true}"; enable_verifier=false
         )
@@ -388,14 +372,14 @@ function compile_mlir!(mod, f, args; optimize::Union{Bool,Symbol}=true, no_nan::
                     "canonicalize",
                     "remove-unnecessary-enzyme-ops",
                     "enzyme-simplify-math",
-                    _opt_passes,
+                    opt_passes,
                     kern,
                 ],
                 ',',
             ),
         )
     elseif optimize === :before_kernel
-        run_pass_pipeline!(mod, join([_opt_passes, "enzyme-batch", _opt_passes], ","))
+        run_pass_pipeline!(mod, join([opt_passes, "enzyme-batch", opt_passes], ","))
         run_pass_pipeline!(
             mod, "$enzyme_pass,arith-raise{stablehlo=true}"; enable_verifier=false
         )
@@ -406,13 +390,13 @@ function compile_mlir!(mod, f, args; optimize::Union{Bool,Symbol}=true, no_nan::
                     "canonicalize",
                     "remove-unnecessary-enzyme-ops",
                     "enzyme-simplify-math",
-                    _opt_passes,
+                    opt_passes,
                 ],
                 ',',
             ),
         )
     elseif optimize === :no_enzyme
-        run_pass_pipeline!(mod, join([_opt_passes, "enzyme-batch", _opt_passes], ","))
+        run_pass_pipeline!(mod, join([opt_passes, "enzyme-batch", opt_passes], ","))
         run_pass_pipeline!(mod, "arith-raise{stablehlo=true}"; enable_verifier=false)
         run_pass_pipeline!(
             mod,
@@ -421,7 +405,7 @@ function compile_mlir!(mod, f, args; optimize::Union{Bool,Symbol}=true, no_nan::
                     "canonicalize",
                     "remove-unnecessary-enzyme-ops",
                     "enzyme-simplify-math",
-                    _opt_passes,
+                    opt_passes,
                 ],
                 ',',
             ),
@@ -450,14 +434,14 @@ function compile_mlir!(mod, f, args; optimize::Union{Bool,Symbol}=true, no_nan::
                     "canonicalize",
                     "remove-unnecessary-enzyme-ops",
                     "enzyme-simplify-math",
-                    _opt_passes,
+                    opt_passes,
                     kern,
                 ],
                 ',',
             ),
         )
     elseif optimize === :before_enzyme
-        run_pass_pipeline!(mod, join([_opt_passes, "enzyme-batch", _opt_passes], ","))
+        run_pass_pipeline!(mod, join([opt_passes, "enzyme-batch", opt_passes], ","))
         run_pass_pipeline!(
             mod, "$enzyme_pass,arith-raise{stablehlo=true}"; enable_verifier=false
         )
