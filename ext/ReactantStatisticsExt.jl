e4343f3cfb503ea1e5079e1239bebbbf66ffca50
Avik Pal <avikpal@mit.edu>
2025-01-04 23:02:37 -0500

    feat: missing mean(f, ...) dispatches

--- ext/ReactantStatisticsExt.jl
+++ ext/ReactantStatisticsExt.jl
@@ -4,18 +4,20 @@ using Reactant: AnyTracedRArray
 using Reactant.TracedUtils: materialize_traced_array
 using Statistics: Statistics
 
-function Statistics.mean(A::AnyTracedRArray{T,N}; dims=:) where {T,N}
-    A = materialize_traced_array(A)
+function Statistics._mean(f::F, A::AnyTracedRArray{T,N}, dims) where {F,T,N}
     denom = dims isa Colon ? length(A) : prod(Base.Fix1(size, A), dims)
-    return mapreduce(identity, +, A; dims) / denom
+    return mapreduce(f, +, A; dims) / denom
 end
 
-function Statistics.var(
-    A::AnyTracedRArray{T,N}; dims=:, mean=nothing, corrected=true
-) where {T,N}
-    A = materialize_traced_array(A)
+function Statistics._var(A::AnyTracedRArray{T,N}, corrected::Bool, mean, ::Colon) where {T,N}
+    mean === nothing && (mean = Statistics.mean(A))
+    denom = length(A) - corrected
+    return mapreduce(abs2, +, A .- mean; dims=:) / denom
+end
+
+function Statistics._var(A::AnyTracedRArray{T,N}, corrected::Bool, mean, dims) where {T,N}
     mean === nothing && (mean = Statistics.mean(A; dims))
-    denom = (dims isa Colon ? length(A) : prod(Base.Fix1(size, A), dims)) - corrected
+    denom = prod(Base.Fix1(size, A), dims) - corrected
     return mapreduce(abs2, +, A .- mean; dims) / denom
 end
 
