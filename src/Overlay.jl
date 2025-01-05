0c0163b5ace74e8c5060a6e4bc45fe03aa1a6273
Billy Moses <wmoses@google.com>
2025-01-05 15:46:45 -0500

    fix

--- src/Overlay.jl
+++ src/Overlay.jl
@@ -148,7 +148,7 @@ end
 # Base overloads
 @reactant_overlay @noinline function Base._stack(dims::Union{Integer,Colon}, iter)
    iter2 = collect(iter)
-   if any(use_overlayed_version, iter2)
+   if use_overlayed_version(iter2) || any(use_overlayed_version, iter2)
         return TracedRArrayOverrides.overloaded_stack(dims, iter2)
     else
         return Base._stack(dims, iter2)
