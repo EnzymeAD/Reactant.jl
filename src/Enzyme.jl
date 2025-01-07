cc92473a9511a38aadbddcdbb02a918af203c8c3
Avik Pal <avikpal@mit.edu>
2025-01-07 16:24:31 -0500

    chore: formatting

--- src/Enzyme.jl
+++ src/Enzyme.jl
@@ -3,5 +3,5 @@
 # The default `onehot` will lead to scalar indexing
 function Enzyme.onehot(x::TracedRArray{T,N}) where {T,N}
     x_arr = zeros(T, size(x))
-    return map(Base.Fix1(TracedUtils.promote_to, TracedRArray{T, N}), Enzyme.onehot(x_arr))
+    return map(Base.Fix1(TracedUtils.promote_to, TracedRArray{T,N}), Enzyme.onehot(x_arr))
 end
