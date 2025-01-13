ef88fdbeeac11b7ac7623fe5cf483fe4009b32dd
Jaemin Oh <jaeminoh.math@gmail.com>
2025-01-12 21:12:02 -0600

    Make v and hcat with numbers work. (#514)
    * Make v and hcat with numbers work.
    
    * change RArray to Union{AnyConcrerteRArray, AnyTracedRArray}; add a unit test for this change
    
    * update unit tests
    
    * update unit tests: confirming vcat and hcat are working as expected
    
    * update test: improve reproducibility of test
    
    * update test: w/ deterministic values

--- test/basic.jl
+++ test/basic.jl
@@ -368,6 +368,34 @@ end
         @test y == test_typed_hvncat(x)
         @test eltype(y) === Int
     end
+
+    @testset "Number and RArray" for a in [1.0f0, 1.0e0]
+        typeof_a = typeof(a)
+        _b = [2.0, 3.0, 4.0] .|> typeof_a
+        _c = [2.0 3.0 4.0] .|> typeof_a
+        b = Reactant.to_rarray(_b)
+        c = Reactant.to_rarray(_c)
+    
+        # vcat test        
+        y = @jit vcat(a, b)
+        @test y == vcat(a, _b)
+        @test y isa ConcreteRArray{typeof_a,1}
+    
+        ## vcat test - adjoint
+        y1 = @jit vcat(a, c')
+        @test y1 == vcat(a, _c')
+        @test y1 isa ConcreteRArray{typeof_a,2}
+    
+        # hcat test
+        z = @jit hcat(a, c)
+        @test z == hcat(a, _c)
+        @test z isa ConcreteRArray{typeof_a,2}
+    
+        ## hcat test - adjoint
+        z1 = @jit hcat(a, b')
+        @test z1 == hcat(a, _b')
+        @test z1 isa ConcreteRArray{typeof_a,2}
+    end
 end
 
 @testset "repeat" begin
