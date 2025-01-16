08620450e67c970049448d683eb13c0b1c0f0d0b
Avik Pal <avikpal@mit.edu>
2025-01-16 14:59:28 -0500

    fix: tests used older defn

--- test/tracing.jl
+++ test/tracing.jl
@@ -144,7 +144,9 @@ using Test
                     Base.Pairs{Symbol,Union{}},
                 ),
             ]
-                tracedty = traced_type(origty, Val(ConcreteToTraced), Union{})
+                tracedty = traced_type(
+                    origty, Val(ConcreteToTraced), Union{}, Reactant.BatchNone, nothing
+                )
                 @test tracedty == targetty
 
                 tracedty2 = traced_type(origty, Val(ConcreteToTraced), ReactantPrimitive)
@@ -158,7 +160,7 @@ using Test
                 TracedRArray{Float64,3},
             ]
                 @test_throws Union{ErrorException,String} traced_type(
-                    type, Val(ConcreteToTraced), Union{}
+                    type, Val(ConcreteToTraced), Union{}, Reactant.BatchNone, nothing
                 )
             end
         end
@@ -167,7 +169,9 @@ using Test
                 x::Vector{Float64}
                 y::Union{Nothing,Node}
             end
-            @test_throws NoFieldMatchError traced_type(Node, Val(ArrayToConcrete), Union{})
+            @test_throws NoFieldMatchError traced_type(
+                Node, Val(ArrayToConcrete), Union{}, Reactant.BatchNone, nothing
+            )
         end
     end
 
