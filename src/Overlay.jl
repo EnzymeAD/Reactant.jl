354f0fbd98033165e134c9f0ec6ca5aa0a543cf4
William S. Moses <gh@wsmoses.com>
2025-01-16 22:45:24 -0600

    fix

--- src/Overlay.jl
+++ src/Overlay.jl
@@ -10,10 +10,9 @@
     return f
 end
 
-
 @reactant_overlay @noinline function Base.setindex!(a::AnyTracedRArray{T,N}, v, indices::Vararg{Any,N}) where {T,N}
     ancestor_indices = TracedUtils.get_ancestor_indices(a, indices...)
-    Base.inferencebarrier(call_with_reactant)(setindex!, ancestor(a), v, ancestor_indices...)
+    (Base.inferencebarrier(setindex!))(ancestor(a), v, ancestor_indices...)
     return a
 end
 
--- src/TracedUtils.jl
+++ src/TracedUtils.jl
@@ -68,7 +68,8 @@ function set_mlir_data!(
 end
 
 function set_mlir_data!(x::AnyTracedRArray{T}, data) where {T}
-    Reactant.call_with_reactant(setindex!, x, TracedRArray{T}(data), axes(x)...)
+    ancestor_indices = TracedUtils.get_ancestor_indices(a, axes(x)...)
+    setindex!(ancestor(a), v, ancestor_indices...)
     return x
 end
 
