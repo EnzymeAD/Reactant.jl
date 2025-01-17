f8a7e5e06019648668e2679cea419b54c4603924
William S. Moses <gh@wsmoses.com>
2025-01-16 21:56:03 -0600

    try fix

--- src/TracedUtils.jl
+++ src/TracedUtils.jl
@@ -68,7 +68,7 @@ function set_mlir_data!(
 end
 
 function set_mlir_data!(x::AnyTracedRArray{T}, data) where {T}
-    setindex!(x, TracedRArray{T}(data), axes(x)...)
+    call_with_reactant(setindex!, x, TracedRArray{T}(data), axes(x)...)
     return x
 end
 
