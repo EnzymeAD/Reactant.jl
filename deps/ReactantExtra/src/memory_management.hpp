2ecb10be3358947e12f333586170641e3d3d2f4e
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-01-19 14:21:37 +0100

    fix const cast in `capture_shared`

--- deps/ReactantExtra/src/memory_management.hpp
+++ deps/ReactantExtra/src/memory_management.hpp
@@ -1,11 +1,21 @@
 #include <memory>
 
 extern "C" void reactant_release_shared(void* ptr);
-extern "C" void reactant_capture_shared(std::shared_ptr<void>& ptr);
 extern "C" bool reactant_contains_shared(void* ptr);
-extern "C" void reactant_generic_llvm_rtti_root_dtor(llvm::RTTIRoot* root);
 
 namespace reactant {
+template <typename T, typename G = std::remove_cv_t<T>>
+inline G* capture_shared(std::shared_ptr<T> ptr) {
+    return reinterpret_cast<G*>(
+        capture_shared(
+            std::const_pointer_cast<G>(ptr)
+        )
+    );
+}
+
+template<>
+void* capture_shared(std::shared_ptr<void> ptr);
+
 template<typename  T>
 inline void destruct_or_release_if_shared(T* ptr) {
     if (reactant_contains_shared(ptr))
