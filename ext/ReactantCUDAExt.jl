c842d9352f78420c899855b5fb81401cbd4025fb
William Moses <gh@wsmoses.com>
2025-01-18 23:50:45 -0600

    Update ext/ReactantCUDAExt.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/ReactantCUDAExt.jl
+++ ext/ReactantCUDAExt.jl
@@ -528,10 +528,11 @@ Reactant.@reactant_overlay @noinline function (func::LLVMFunc{F,tt})(
     push!(MLIR.IR.region(wrapfunc, 1), wrapbody)
     for i in 1:length(wrapper_tys)
         @ccall MLIR.API.mlir_c.ReactantFuncSetArgAttr(
-                  wrapfunc::MLIR.API.MlirOperation, (i-1)::Csize_t,
-                  "llvm.noalias"::MLIR.API.MlirStringRef,
-                  MLIR.IR.UnitAttribute()::MLIR.API.MlirAttribute
-                 )::Cvoid
+            wrapfunc::MLIR.API.MlirOperation,
+            (i - 1)::Csize_t,
+            "llvm.noalias"::MLIR.API.MlirStringRef,
+            MLIR.IR.UnitAttribute()::MLIR.API.MlirAttribute,
+        )::Cvoid
     end
 
     wrapargs = MLIR.IR.Value[]
