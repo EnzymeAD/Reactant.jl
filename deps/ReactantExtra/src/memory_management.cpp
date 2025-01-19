f19c30631f027d7a507d4435b5cc55b078386553
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-01-19 13:35:08 +0100

    fixes for memory management

--- deps/ReactantExtra/src/memory_management.cpp
+++ deps/ReactantExtra/src/memory_management.cpp
@@ -3,23 +3,21 @@
 #include <map>
 #include <memory>
 
-std::map<void*, std::shared_ptr<void>> captured_shared_objects;
+std::map<void*, std::shared_ptr<void>> captured_shared_ptr;
 
 extern "C" void reactant_release_shared(void* ptr) {
-    captured_shared_objects.erase(ptr);
+    captured_shared_ptr.erase(ptr);
 }
 
 extern "C" void reactant_capture_shared(std::shared_ptr<void>& ptr) {
-    captured_shared_objects[ptr.get()] = ptr;
+    captured_shared_ptr[ptr.get()] = ptr;
 }
 
+// `map::contains` was introduced in C++20
 extern "C" bool reactant_contains_shared(void* ptr) {
-    return captured_shared_objects.contains(ptr);
+    return captured_shared_ptr.find(ptr) != captured_shared_ptr.end();
 }
 
 extern "C" void reactant_generic_llvm_rtti_root_dtor(llvm::RTTIRoot* root) {
-    if (captured_shared_objects.contains(root))
-        reactant_release_shared(root);
-    else
-        delete root;
+    reactant::destruct_or_release_if_shared(root);
 }
