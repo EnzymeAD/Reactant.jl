635e48f024f83b345421ecd8bbbf75813bb85cfd
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-01-19 17:07:23 +0100

    implement missing methods of `Array`

--- deps/ReactantExtra/src/ifrt/Array.cpp
+++ deps/ReactantExtra/src/ifrt/Array.cpp
@@ -1,5 +1,6 @@
 #include "src/type_conversion.hpp"
 #include "src/error_handling.hpp"
+#include "src/memory_management.hpp"
 #include "xla/python/ifrt/array.h"
 
 using namespace xla::ifrt;
@@ -12,22 +13,29 @@ extern "C" DType* ifrt_array_dtype(Array* array)
 
 extern "C" const Shape* ifrt_array_shape(Array* array)
 {
-    return &(array->shape());
+    return new Shape(array->shape());
 }
 
 extern "C" const Sharding* ifrt_array_sharding(Array* array)
 {
-    return &(array->sharding());
+    return capture_shared(array->shared_ptr_sharding());
 }
 
-// TODO now it returns a `shared_ptr<PjRtLayout>`
-// extern "C" xla::PjRtLayout* ifrt_array_layout(Array* array)
-// {
-//     return MyValueOrThrow(array->layout()).release();
-// }
+extern "C" xla::PjRtLayout* ifrt_array_layout(Array* array)
+{
+    return capture_shared(MyValueOrThrow(array->layout()));
+}
 
-// TODO xla::Array::DisassembleIntoSingleDeviceArrays
-// TODO xla::Array::FullyReplicatedShard
+extern "C" span<Array*> ifrt_array_disassemble_into_single_device_arrays(Array* array, ArrayCopySemantics copy_semantics, SingleDeviceShardSemantics shard_semantics)
+{
+    auto arrays = MyValueOrThrow(array->DisassembleIntoSingleDeviceArrays(copy_semantics, shard_semantics));
+    return convert(Type<span<Array*>>(), arrays);
+}
+
+extern "C" Array* ifrt_array_fully_replicated_shard(Array* array, ArrayCopySemantics copy_semantics)
+{
+    return MyValueOrThrow(array->FullyReplicatedShard(copy_semantics)).release();
+}
 
 extern "C" Future<>* ifrt_array_copy_to_host_buffer(Array* array, void* data, const int64_t* byte_strides, int semantics)
 {
