719e0173bf295290d75d1d292bf358e279398da3
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-01-19 10:33:48 +0100

    update `ifrt_pjrt_executable_ctor`

--- deps/ReactantExtra/src/pjrt_ifrt/PjRtExecutable.cpp
+++ deps/ReactantExtra/src/pjrt_ifrt/PjRtExecutable.cpp
@@ -6,10 +6,9 @@ using namespace xla::ifrt;
 using namespace reactant;
 
 // TODO is there any problem with ownership by using `std::shared_ptr` here?
-extern "C" Executable* ifrt_pjrt_executable_ctor(xla::PjRtExecutable* c_pjrt_executable, XlaCompileOptions* c_compile_options) {
+extern "C" Executable* ifrt_pjrt_executable_ctor(xla::PjRtExecutable* c_pjrt_executable) {
     auto pjrt_executable = std::shared_ptr<xla::PjRtExecutable>(c_pjrt_executable);
-    auto compile_options = std::make_unique<XlaCompileOptions>(*c_compile_options);
-    return MyValueOrThrow(PjRtExecutable::Create(pjrt_executable, std::move(compile_options))).release();
+    return MyValueOrThrow(PjRtExecutable::Create(pjrt_executable)).release();
 }
 
 extern "C" void ifrt_pjrt_executable_free(PjRtExecutable* executable) { delete executable; }
