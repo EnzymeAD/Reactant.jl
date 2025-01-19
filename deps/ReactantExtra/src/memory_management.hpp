8b224918f4e1e8e36ba8e2c7d923004e0ccd02b8
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-01-19 13:29:20 +0100

    Add infrastructure to capture `shared_ptr`

---
+++ deps/ReactantExtra/src/memory_management.hpp
@@ -0,0 +1,15 @@
+#include <memory>
+
+extern "C" void reactant_release_shared(void* ptr);
+extern "C" void reactant_capture_shared(std::shared_ptr<void>& ptr);
+extern "C" bool reactant_contains_shared(void* ptr);
+
+namespace reactant {
+template<typename  T>
+inline void destruct_or_release_if_shared(T* ptr) {
+    if (reactant_contains_shared(ptr))
+        reactant_release_shared(ptr);
+    else
+        delete ptr;
+}
+}
