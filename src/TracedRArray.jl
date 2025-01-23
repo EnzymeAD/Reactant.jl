077e59192051375e263c3d3257368d1fe6c293b8
William Moses <gh@wsmoses.com>
2025-01-23 12:23:41 -0600

    Add convert (#598)
    * Add convert
    
    * syntax error fix
    
    * Update src/TracedRArray.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * add x
    
    * up
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/TracedRArray.jl
+++ src/TracedRArray.jl
@@ -26,6 +26,14 @@ ReactantCore.is_traced(::TracedRArray) = true
 
 Base.strides(x::TracedRArray) = Base.size_to_strides(1, size(x)...)
 
+function Base.convert(::Type{TracedRArray}, x::AnyTracedRArray)
+    return Base.convert(TracedRArray{unwrapped_eltype(x),ndims(x)}, x)
+end
+
+function Base.convert(::Type{TracedRArray}, x::AbstractArray)
+    return Base.convert(TracedRArray{eltype(x),ndims(x)}, x)
+end
+
 function Base.convert(::Type{TracedRArray{T,N}}, x::AbstractArray) where {T,N}
     @assert ndims(x) == N
     if x isa TracedRArray
