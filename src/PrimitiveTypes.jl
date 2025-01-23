cd8fa3218165064db6620614d4cd27b45b065275
Avik Pal <avikpal@mit.edu>
2025-01-22 21:21:34 -0500

    chore: run formatter

--- src/PrimitiveTypes.jl
+++ src/PrimitiveTypes.jl
@@ -40,7 +40,7 @@ else
     const ReactantFloat = Union{Float16,Float32,Float64,Base.uniontypes(ReactantFloat8)...}
 end
 
-const ReactantComplexFloat = Union{Complex{Float32}, Complex{Float64}}
+const ReactantComplexFloat = Union{Complex{Float32},Complex{Float64}}
 
 const ReactantInt = Union{Int8,UInt8,Int16,UInt16,Int32,UInt32,Int64,UInt64}
 
