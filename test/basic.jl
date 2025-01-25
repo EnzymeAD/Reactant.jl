94032fa58c700bef8562ffde9396ba531d3201e8
William S. Moses <gh@wsmoses.com>
2025-01-25 17:21:10 -0600

    fix parsing

--- test/basic.jl
+++ test/basic.jl
@@ -590,7 +590,8 @@ end
 @testset "$op" for op in [:round, :ceil, :floor]
     for x in (rand(Float32, (3, 3)), rand(Float64))
         @eval @test @jit($op.(ConcreteRNumber.($x))) == $op.($x)
-        @eval @test @jit($op.(Int, ConcreteRNumber.($x))) == $op.(Int, $x)
+        int_$op(x) = $op(Int, x)
+        @eval @test @jit($int_$op.(ConcreteRNumber.($x))) == int_$op.($x)
     end
 end
 
