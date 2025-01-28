ed5537aff16b0e1f02b851646fb2bd556401ce13
Avik Pal <avikpal@mit.edu>
2025-01-27 22:35:35 -0500

    feat: add dispatch for KA get_backend

--- test/integration/kernelabstractions.jl
+++ test/integration/kernelabstractions.jl
@@ -1,8 +1,4 @@
-using CUDA
-using KernelAbstractions
-using Reactant
-
-using CUDA: CuArray
+using CUDA, KernelAbstractions, Reactant
 
 # Simple kernel for matrix multiplication
 @kernel function matmul_kernel!(output, a)
@@ -11,23 +7,41 @@ using CUDA: CuArray
 
     tmp_sum = zero(eltype(output))
     for k in 1:size(a)[2]
-        tmp_sum += a[i, k] * a[k, j]
+        @inbounds tmp_sum += a[i, k] * a[k, j]
     end
 
-    return output[i, j] = tmp_sum
+    @inbounds output[i, j] = tmp_sum
 end
 
 # Creating a wrapper kernel for launching with error checks
-function matmul!(output, a, backend)
+function matmul!(output, a)
+    backend = KernelAbstractions.get_backend(output)
     kernel! = matmul_kernel!(backend)
     kernel!(output, a; ndrange=size(output))
     return KernelAbstractions.synchronize(backend)
 end
 
 @testset "KernelAbstractions Call" begin
-    backend = KernelAbstractions.get_backend(CuArray(ones(1)))
-    A = Reactant.to_rarray(CuArray(ones(100, 100)))
-    out = Reactant.to_rarray(CuArray(ones(100, 100)))
-    @jit matmul!(out, A, backend)
-    @test all(Array(out) .≈ 100)
+    A = Reactant.to_rarray(ones(100, 100))
+    out = Reactant.to_rarray(ones(100, 100))
+    @test all(Array(@jit(matmul!(out, A))) .≈ 100) broken = true
+end
+
+# simple square kernel
+@kernel function square_kernel!(y, @Const(x))
+    i = @index(Global)
+    @inbounds y[i] = x[i] * x[i]
+end
+
+function square(x)
+    y = similar(x)
+    backend = KernelAbstractions.get_backend(x)
+    kernel! = square_kernel!(backend)
+    kernel!(y, x; ndrange=length(x))
+    return y
+end
+
+@testset "Squaring Kernel" begin
+    x = Reactant.to_rarray(collect(1:1:64) ./ 64)
+    @test all(Array(@jit(square(x))) .≈ Array(x) .* Array(x))
 end
