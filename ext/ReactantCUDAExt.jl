1925f701859313baaa3afe31764a818b75a3678b
William Moses <gh@wsmoses.com>
2025-01-30 11:37:22 +0100

    CPU backend (#647)
    * CPU backend
    
    * add build
    
    * cpu handle
    
    * KA backend
    
    * Update WORKSPACE
    
    * Update WORKSPACE
    
    * Update WORKSPACE
    
    * Update WORKSPACE
    
    * Update WORKSPACE
    
    * Update Project.toml
    
    * Update Compiler.jl
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update BUILD
    
    * Update ext/ReactantCUDAExt.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update Project.toml
    
    * Adapt to compile abi change
    
    * move device
    
    * rm redundant
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    Co-authored-by: Mosè Giordano <m.giordano@ucl.ac.uk>
    Co-authored-by: Avik Pal <avikpal@mit.edu>

--- ext/ReactantCUDAExt.jl
+++ ext/ReactantCUDAExt.jl
@@ -1,7 +1,8 @@
 module ReactantCUDAExt
 
 using CUDA
-using Reactant: Reactant, TracedRArray, AnyTracedRArray, MLIR, TracedRNumber
+using Reactant:
+    Reactant, TracedRArray, AnyTracedRArray, AnyConcreteRArray, MLIR, TracedRNumber
 using ReactantCore: @trace
 using KernelAbstractions: KernelAbstractions
 using Libdl
@@ -9,6 +10,7 @@ using Libdl
 using Adapt
 
 KernelAbstractions.get_backend(::AnyTracedRArray) = CUDABackend()
+KernelAbstractions.get_backend(::AnyConcreteRArray) = CUDABackend()
 
 struct CuTracedArray{T,N,A,Size} <: DenseArray{T,N}
     ptr::Core.LLVMPtr{T,A}
