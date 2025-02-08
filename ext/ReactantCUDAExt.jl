13d39b0f4a413a160044f9d4a64e663048ade75d
William S. Moses <gh@wsmoses.com>
2025-02-07 23:37:50 -0500

    fix

--- ext/ReactantCUDAExt.jl
+++ ext/ReactantCUDAExt.jl
@@ -297,7 +297,7 @@ function ka_with_reactant(ndrange, workgroupsize, obj, args...)
 
     # figure out the optimal workgroupsize automatically
     if KA.workgroupsize(obj) <: KA.DynamicSize && workgroupsize === nothing
-        if !Reactant.Compiler.PartitionKA[]
+        if !Reactant.Compiler.PartitionKA[] || Reactant.Compiler.Raise[]
             threads = prod(ndrange)
         else
             config = CUDA.launch_configuration(kernel.fun; max_threads=prod(ndrange))
