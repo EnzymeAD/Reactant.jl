eeff6843107b3d1770b3ae8615fb046d61586d85
Avik Pal <avikpal@iitk.ac.in>
2025-02-09 15:56:46 -0500

    feat: add convertion from JLOpSharding to OpSharding

--- src/xla/Sharding.jl
+++ src/xla/Sharding.jl
@@ -13,8 +13,7 @@ end
 end
 
 # https://github.com/openxla/xla/blob/8bac4a2c3c32144e39b1602450fe318bfab8e15d/xla/xla_data.proto#L897
-# TODO: tuple sharding
-# TODO: op metadata
+# TODO: tuple sharding / op metadata
 struct JLOpSharding
     type::Int32
     replicate_on_last_tile_dim::Bool
@@ -46,6 +45,22 @@ struct OpSharding
     shard_group_type::ShardGroupType.T
 end
 
+function int_to_op_sharding_type(i::Int32)
+    i == 0 && return OpShardingType.Replicated
+    i == 1 && return OpShardingType.Maximal
+    i == 2 && return OpShardingType.Tuple
+    i == 3 && return OpShardingType.Other
+    i == 4 && return OpShardingType.Manual
+    i == 5 && return OpShardingType.Unknown
+    return error("Invalid OpShardingType $i")
+end
+
+function int_to_shard_group_type(i::Int32)
+    i == 0 && return ShardGroupType.As
+    i == 1 && return ShardGroupType.Like
+    return error("Invalid ShardGroupType $i")
+end
+
 function get_output_shardings(exec::LoadedExecutable)
     exec.is_sharded || return OpSharding[]
 
@@ -54,10 +69,58 @@ function get_output_shardings(exec::LoadedExecutable)
         @ccall MLIR.API.mlir_c.PjRtLoadedExecutableGetOuputShardings(
             exec.exec::Ptr{Cvoid},
             Base.unsafe_convert(Ptr{Cvoid}, jl_op_shardings)::Ptr{Cvoid},
-            exec.num_results::Int32
+            exec.num_results::Int32,
         )::Cvoid
     end
 
-    # TODO: copy the results into `OpSharding`
-    return jl_op_shardings
+    op_shardings = Vector{OpSharding}(undef, exec.num_results)
+    for (i, jl_op_sharding) in enumerate(jl_op_shardings)
+        last_tile_dims = Vector{Int32}(undef, jl_op_sharding.n_last_tile_dims)
+        for j in 1:(jl_op_sharding.n_last_tile_dims)
+            last_tile_dims[j] = unsafe_load(jl_op_sharding.last_tile_dims, j)
+        end
+
+        tile_assignment_dimensions = Vector{Int64}(
+            undef, jl_op_sharding.n_tile_assignment_dimensions
+        )
+        for j in 1:(jl_op_sharding.n_tile_assignment_dimensions)
+            tile_assignment_dimensions[j] = unsafe_load(
+                jl_op_sharding.tile_assignment_dimensions, j
+            )
+        end
+
+        tile_assignment_devices = Vector{Int64}(
+            undef, jl_op_sharding.n_tile_assignment_devices
+        )
+        for j in 1:(jl_op_sharding.n_tile_assignment_devices)
+            tile_assignment_devices[j] = unsafe_load(
+                jl_op_sharding.tile_assignment_devices, j
+            )
+        end
+
+        iota_reshape_dims = Vector{Int64}(undef, jl_op_sharding.n_iota_reshape_dims)
+        for j in 1:(jl_op_sharding.n_iota_reshape_dims)
+            iota_reshape_dims[j] = unsafe_load(jl_op_sharding.iota_reshape_dims, j)
+        end
+
+        iota_transpose_perm = Vector{Int32}(undef, jl_op_sharding.n_iota_transpose_perm)
+        for j in 1:(jl_op_sharding.n_iota_transpose_perm)
+            iota_transpose_perm[j] = unsafe_load(jl_op_sharding.iota_transpose_perm, j)
+        end
+
+        op_shardings[i] = OpSharding(
+            int_to_op_sharding_type(jl_op_sharding.type),
+            jl_op_sharding.replicate_on_last_tile_dim,
+            last_tile_dims,
+            tile_assignment_dimensions,
+            tile_assignment_devices,
+            iota_reshape_dims,
+            iota_transpose_perm,
+            jl_op_sharding.is_shard_group,
+            jl_op_sharding.shard_group_id,
+            int_to_shard_group_type(jl_op_sharding.shard_group_type),
+        )
+    end
+
+    return op_shardings
 end
