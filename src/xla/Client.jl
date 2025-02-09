d04da8de86a67bf5a8ddee671b4f4cadf39a8680
Avik Pal <avikpal@mit.edu>
2025-02-09 00:15:26 -0500

    feat: enable async on CPU

--- src/xla/Client.jl
+++ src/xla/Client.jl
@@ -86,9 +86,7 @@ end
 # Clients for Different Backends
 const cpuclientcount = Ref(0)
 
-# TODO synchronization when async is not working because `future` in `ConcreteRArray` is
-#      always `nothing`
-function CPUClient(node_id=0, num_nodes=1; checkcount=true, asynchronous=false)
+function CPUClient(node_id=0, num_nodes=1; checkcount=true, asynchronous=true)
     if checkcount
         @assert cpuclientcount[] == 0
         cpuclientcount[] += 1
