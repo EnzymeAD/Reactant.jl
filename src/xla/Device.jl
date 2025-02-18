2884cce80a140c93020e547e435a1a224ee0f6e0
Avik Pal <avikpal@mit.edu>
2025-02-18 08:34:24 -0600

    fix: old dispatch

--- src/xla/Device.jl
+++ src/xla/Device.jl
@@ -24,5 +24,5 @@ end
 function Base.string(device::AbstractDevice)
     client = XLA.client(device)
     pname = XLA.platform_name(client)
-    return "$(uppercase(pname)):$(device_ordinal(client, device)) $(device_kind(device))"
+    return "$(uppercase(pname)):$(device_ordinal(device)) $(device_kind(device))"
 end
