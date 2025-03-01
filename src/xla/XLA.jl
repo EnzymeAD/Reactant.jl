7c462f7750a060a960cebf246d745aa0e4380355
Avik Pal <avikpal@mit.edu>
2025-03-01 11:36:19 -0600

    feat: env var to control runtime

--- src/xla/XLA.jl
+++ src/xla/XLA.jl
@@ -8,8 +8,8 @@ using EnumX: @enumx
 
 const XLA_REACTANT_GPU_MEM_FRACTION = Ref{Float64}(0.75)
 const XLA_REACTANT_GPU_PREALLOCATE = Ref{Bool}(true)
+const REACTANT_RUNTIME_BACKEND = Ref{String}("PJRT")
 
-using Reactant_jll
 const CUDA_DATA_DIR = Ref(
     isdefined(Reactant_jll, :ptxas_path) ? dirname(dirname(Reactant_jll.ptxas_path)) : ""
 )
@@ -37,38 +37,45 @@ include("PJRT/PJRT.jl")
 
 include("IFRT/IFRT.jl")
 
-@kwdef mutable struct PJRTBackendState
-    initialized::Bool = false
-    clients::Dict{String,PJRT.Client} = Dict{String,PJRT.Client}()
-    default_client::PJRT.Client = PJRT.Client(C_NULL; skip_check=true)
+for runtime in (:PJRT, :IFRT)
+    backend_state = Symbol(runtime, :BackendState)
+
+    @eval begin
+        @kwdef mutable struct $(backend_state)
+            initialized::Bool = false
+            clients::Dict{String,$(runtime).Client} = Dict{String,$(runtime).Client}()
+            default_client::$(runtime).Client = $(runtime).Client(C_NULL; skip_check=true)
+        end
+
+        function Base.getproperty(bs::$(backend_state), sym::Symbol)
+            (sym === :initialized || bs.initialized) && return getfield(bs, sym)
+            initialize_default_clients!(bs)
+            return getfield(bs, sym)
+        end
+
+        function Base.setproperty!(bs::$(backend_state), sym::Symbol, val)
+            (sym === :initialized || bs.initialized) && return setfield!(bs, sym, val)
+            initialize_default_clients!(bs)
+            return setfield!(bs, sym, val)
+        end
+    end
 end
 
-function Base.getproperty(bs::PJRTBackendState, sym::Symbol)
-    (sym === :initialized || bs.initialized) && return getfield(bs, sym)
-    initialize_default_pjrt_clients!(bs)
-    return getfield(bs, sym)
-end
-
-function Base.setproperty!(bs::PJRTBackendState, sym::Symbol, val)
-    (sym === :initialized || bs.initialized) && return setfield!(bs, sym, val)
-    initialize_default_pjrt_clients!(bs)
-    return setfield!(bs, sym, val)
-end
-
-const global_backend_state = PJRTBackendState()
+const global_backend_state = Ref{Union{Nothing,PJRTBackendState,IFRTBackendState}}(nothing)
 const global_state = State()
 
-client(backend::String) = global_backend_state.clients[backend]
-default_backend() = global_backend_state.default_client
+client(backend::String) = global_backend_state[].clients[backend]
+default_backend() = global_backend_state[].default_client
+default_device() = default_device(default_backend())
 process_index() = process_index(default_backend())
 
 function set_default_backend(backend::AbstractClient)
-    global_backend_state.default_client = backend
+    global_backend_state[].default_client = backend
     return nothing
 end
 
 function set_default_backend(backend::String)
-    global_backend_state.default_client = client(backend)
+    global_backend_state[].default_client = client(backend)
     return nothing
 end
 
@@ -77,9 +84,9 @@ function update_global_state!(args...; kwargs...)
     # We conditionally initialize for now, since a lot of options that are set are not
     # necessarily supported by PJRT. This makes testing for IFRT quite hard.
     # Once we move to IFRT completely, we can remove this.
-    if global_backend_state.initialized
+    if global_backend_state[].initialized
         # We need to update the clients based on the new state
-        initialize_default_pjrt_clients!(global_backend_state)
+        initialize_default_clients!(global_backend_state[])
     end
     return nothing
 end
@@ -99,6 +106,9 @@ function __init__()
             Float64, ENV["XLA_REACTANT_GPU_MEM_FRACTION"]
         )
         @debug "XLA_REACTANT_GPU_MEM_FRACTION: " XLA_REACTANT_GPU_MEM_FRACTION[]
+        if XLA_REACTANT_GPU_MEM_FRACTION[] > 1 || XLA_REACTANT_GPU_MEM_FRACTION[] < 0
+            error("XLA_REACTANT_GPU_MEM_FRACTION must be between 0 and 1")
+        end
     end
 
     if haskey(ENV, "XLA_REACTANT_GPU_PREALLOCATE")
@@ -112,82 +122,101 @@ function __init__()
         @debug "REACTANT_VISIBLE_GPU_DEVICES: " global_state.local_gpu_device_ids
     end
 
+    if haskey(ENV, "REACTANT_RUNTIME_BACKEND")
+        REACTANT_RUNTIME_BACKEND[] = ENV["REACTANT_RUNTIME_BACKEND"]
+        @debug "REACTANT_RUNTIME_BACKEND: " REACTANT_RUNTIME_BACKEND[]
+        if REACTANT_RUNTIME_BACKEND[] == "PJRT"
+            global_backend_state[] = PJRTBackendState()
+        elseif REACTANT_RUNTIME_BACKEND[] == "IFRT"
+            global_backend_state[] = IFRTBackendState()
+        else
+            error("Unsupported REACTANT_RUNTIME_BACKEND: $(REACTANT_RUNTIME_BACKEND[])")
+        end
+    end
+
     @ccall MLIR.API.mlir_c.RegisterEnzymeXLACPUHandler()::Cvoid
     @ccall MLIR.API.mlir_c.RegisterEnzymeXLAGPUHandler()::Cvoid
     return nothing
 end
 
-function initialize_default_pjrt_clients!(state::PJRTBackendState)
-    was_initialized = state.initialized
-    state.initialized = true
-    distributed_runtime_client = if global_state.num_processes > 1
-        @assert global_state.client !== nothing
-        global_state.client
-    else
-        nothing
-    end
-    common_kwargs = (;
-        node_id=global_state.process_id,
-        num_nodes=global_state.num_processes,
-        distributed_runtime_client,
-    )
-
-    # CPU
-    if was_initialized && haskey(state.clients, "cpu")
-        XLA.free_client(state.clients["cpu"])
-        XLA.PJRT.cpu_client_count[] -= 1
-    end
-    cpu = PJRT.CPUClient(; common_kwargs..., asynchronous=true)
-    state.clients["cpu"] = cpu
-    state.default_client = cpu
-
-    # Try TPU if possible, then try GPU (CUDA)
-    @static if !Sys.isapple()
-        if Reactant.has_tpu()
-            dataset_dir = @get_scratch!("libtpu")
-            if !isfile(dataset_dir * "/libtpu.so")
-                Downloads.download(
-                    "https://storage.googleapis.com/cloud-tpu-tpuvm-artifacts/wheels/libtpu-nightly/libtpu_nightly-0.1.dev20240829-py3-none-any.whl",
-                    dataset_dir * "/tpu.zip",
-                )
-                run(`unzip -qq $(dataset_dir*"/tpu.zip") -d $(dataset_dir)/tmp`)
-                run(`mv $(dataset_dir)/tmp/libtpu/libtpu.so $(dataset_dir)/libtpu.so`)
-                rm(dataset_dir * "/tmp"; recursive=true)
-                rm(dataset_dir * "/tpu.zip"; recursive=true)
-            end
-            try
-                if was_initialized && haskey(state.clients, "tpu")
-                    XLA.free_client(state.clients["tpu"])
-                    XLA.PJRT.tpu_client_count[] -= 1
-                end
-                tpu = PJRT.TPUClient(;
-                    tpu_path=dataset_dir * "/libtpu.so", common_kwargs...
-                )
-                state.clients["tpu"] = tpu
-                state.default_client = tpu
-            catch e
-                println(stdout, e)
-            end
+for runtime in (:PJRT, :IFRT)
+    @eval function initialize_default_clients!(state::$(Symbol(runtime, :BackendState)))
+        was_initialized = state.initialized
+        state.initialized = true
+        distributed_runtime_client = if global_state.num_processes > 1
+            @assert global_state.client !== nothing
+            global_state.client
         else
-            if !Reactant.precompiling()
+            nothing
+        end
+        common_kwargs = (;
+            node_id=global_state.process_id,
+            num_nodes=global_state.num_processes,
+            distributed_runtime_client,
+        )
+
+        # CPU
+        if was_initialized && haskey(state.clients, "cpu")
+            XLA.free_client(state.clients["cpu"])
+            XLA.$(runtime).cpu_client_count[] -= 1
+        end
+        cpu = $(runtime).CPUClient(; common_kwargs..., asynchronous=true)
+        state.clients["cpu"] = cpu
+        state.default_client = cpu
+
+        # Try TPU if possible, then try GPU (CUDA)
+        @static if !Sys.isapple()
+            if Reactant.has_tpu()
+                dataset_dir = @get_scratch!("libtpu")
+                download_tpu(dataset_dir)
                 try
-                    if was_initialized && haskey(state.clients, "gpu")
-                        XLA.free_client(state.clients["gpu"])
-                        XLA.PJRT.gpu_client_count[] -= 1
+                    if was_initialized && haskey(state.clients, "tpu")
+                        XLA.free_client(state.clients["tpu"])
+                        XLA.$(runtime).tpu_client_count[] -= 1
                     end
-                    gpu = PJRT.GPUClient(;
-                        common_kwargs..., allowed_devices=global_state.local_gpu_device_ids
+                    tpu = $(runtime).TPUClient(;
+                        tpu_path=dataset_dir * "/libtpu.so", common_kwargs...
                     )
-                    state.clients["gpu"] = gpu
-                    state.default_client = gpu
+                    state.clients["tpu"] = tpu
+                    state.default_client = tpu
                 catch e
                     println(stdout, e)
                 end
+            else
+                if !Reactant.precompiling()
+                    try
+                        if was_initialized && haskey(state.clients, "gpu")
+                            XLA.free_client(state.clients["gpu"])
+                            XLA.$(runtime).gpu_client_count[] -= 1
+                        end
+                        gpu = $(runtime).GPUClient(;
+                            common_kwargs...,
+                            allowed_devices=global_state.local_gpu_device_ids,
+                        )
+                        state.clients["gpu"] = gpu
+                        state.default_client = gpu
+                    catch e
+                        println(stdout, e)
+                    end
+                end
             end
         end
-    end
 
-    return nothing
+        return nothing
+    end
+end
+
+function download_tpu(dataset_dir::String)
+    if !isfile(dataset_dir * "/libtpu.so")
+        Downloads.download(
+            "https://storage.googleapis.com/cloud-tpu-tpuvm-artifacts/wheels/libtpu-nightly/libtpu_nightly-0.1.dev20240829-py3-none-any.whl",
+            dataset_dir * "/tpu.zip",
+        )
+        run(`unzip -qq $(dataset_dir*"/tpu.zip") -d $(dataset_dir)/tmp`)
+        run(`mv $(dataset_dir)/tmp/libtpu/libtpu.so $(dataset_dir)/libtpu.so`)
+        rm(dataset_dir * "/tmp"; recursive=true)
+        rm(dataset_dir * "/tpu.zip"; recursive=true)
+    end
 end
 
 end
