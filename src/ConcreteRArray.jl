19d731a75846019d673f022aa36f105a0a6581df
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-03-11 19:50:50 +0100

    fix for views and add test

--- src/ConcreteRArray.jl
+++ src/ConcreteRArray.jl
@@ -449,8 +449,13 @@ function Base.fill!(x::Union{AnyConcreteIFRTArray,AnyConcretePJRTArray}, val)
     return x
 end
 
+function mymapreducedim!(f, op, R, A)
+    Base.mapreducedim!(f, op, R, A)
+    return nothing
+end
+
 function Base.mapreducedim!(f, op, R::Union{AnyConcreteIFRTArray,AnyConcretePJRTArray}, A::Union{Base.AbstractBroadcasted, AbstractArray})
-    fn = compile(Base.mapreducedim!, (f, op, R, A))
+    fn = compile(mymapreducedim!, (f, op, R, A))
     fn(f, op, R, A)
     return R
 end
