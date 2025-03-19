4fb38599f7c2876945165b2cb5aa19f572ff16e1
Avik Pal <avikpal@mit.edu>
2025-03-19 17:21:20 -0400

    fix: hijack OffsetArrays indexing

--- ext/ReactantOffsetArraysExt.jl
+++ ext/ReactantOffsetArraysExt.jl
@@ -2,7 +2,7 @@ module ReactantOffsetArraysExt
 
 using OffsetArrays
 using OffsetArrays: OffsetArray
-using Reactant: Reactant, MLIR, Ops, TracedRArray
+using Reactant: Reactant, MLIR, Ops, TracedRArray, AbstractConcreteArray
 
 Base.@nospecializeinfer function Reactant.traced_type_inner(
     @nospecialize(OA::Type{<:OffsetArray}),
@@ -16,6 +16,21 @@ Base.@nospecializeinfer function Reactant.traced_type_inner(
     T = OffsetArrays.parenttype(OA)
     T2 = Reactant.traced_type_inner(T, seen, mode, track_numbers, sharding, runtime)
     return OffsetArray{eltype(T2),N,T2}
+end
+
+# Because why wouldn't offset arrays assume that scalar indexing is the faster way
+function Base.getindex(
+    x::OffsetArray{T,N,<:AbstractConcreteArray}, args::Vararg{Any,N}
+) where {T,N}
+    args = [arg isa Colon ? Base.OneTo(size(x, i)) : arg for (i, arg) in enumerate(args)]
+    offset_indices = [arg .- x.offsets[i] for (i, arg) in enumerate(args)]
+    return getindex(parent(x), offset_indices...)
+end
+function Base.getindex(
+    x::OffsetArray{T,N,<:AbstractConcreteArray}, args::Vararg{Int,N}
+) where {T,N}
+    offset_indices = [arg .- x.offsets[i] for (i, arg) in enumerate(args)]
+    return getindex(parent(x), offset_indices...)
 end
 
 end
