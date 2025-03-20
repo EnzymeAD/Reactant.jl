e04dfe6953cf1a71ba059a344006adb4ee56e756
Avik Pal <avikpal@mit.edu>
2025-03-20 09:38:06 -0500

    Revert "fix: apply transpose in sharding_constraint"
    This reverts commit 4fae3ab625d3a1cab1ff81fcdbed35d7d9fd94b8.

--- test/sharding.jl
+++ test/sharding.jl
@@ -209,13 +209,12 @@ end
         @test contains(repr(hlo), "sharding_constraint")
         hlo = @code_hlo shardy_passes = :to_mhlo_shardings fn_with_constraint(x_ra)
         @test !contains(repr(hlo), "sharding_constraint")
-        @test length(collect(eachmatch(r"mhlo.sharding", repr(hlo)))) == 5
+        @test length(collect(eachmatch(r"mhlo.sharding", repr(hlo)))) == 3
 
         z = Reactant.to_rarray(x; sharding=constraint)
         res = @jit fn_with_constraint(x_ra)
 
         @test x .+ x ≈ Array(res)
-
         @test string(z.sharding.sharding.hlo_sharding) ==
             string(res.sharding.sharding.hlo_sharding)
         @test string(res.sharding.sharding.hlo_sharding) !=
@@ -230,7 +229,7 @@ end
             x_ra_no_sharding
         )
         @test !contains(repr(hlo), "sharding_constraint")
-        @test length(collect(eachmatch(r"mhlo.sharding", repr(hlo)))) == 5
+        @test length(collect(eachmatch(r"mhlo.sharding", repr(hlo)))) == 3
 
         res = @jit fn_with_constraint(x_ra_no_sharding)
         @test x .+ x ≈ Array(res)
