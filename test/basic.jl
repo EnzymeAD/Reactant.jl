6c77ca86dabed0828f8b6c919e969c0a5e1c2b6c
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-28 11:37:18 +0000

    Add an option to always dump MLIR module to be compiled (#1061)
    * Add an option to always dump MLIR module to be compiled
    
    * Fix test: read all the modules

--- test/basic.jl
+++ test/basic.jl
@@ -1082,3 +1082,21 @@ end
         @test res ≈ log.(v)
     end
 end
+
+@testset "Dump MLIR modules" begin
+    always_old = Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[]
+    dir_old = Reactant.MLIR.IR.DUMP_MLIR_DIR[]
+
+    Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = true
+
+    mktempdir() do dir
+        Reactant.MLIR.IR.DUMP_MLIR_DIR[] = dir
+        @compile sin.(Reactant.to_rarray(Float32[1.0]))
+        for mod in readdir(dir; join=true)
+            @test contains(read(mod, String), "hlo.sine")
+        end
+    end
+
+    Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = always_old
+    Reactant.MLIR.IR.DUMP_MLIR_DIR[] = dir_old
+end
