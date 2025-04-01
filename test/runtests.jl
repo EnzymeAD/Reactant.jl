af50c659d06e1c2edce9b9df5267d9301f0e31bb
Avik Pal <avikpal@mit.edu>
2025-03-31 21:19:32 -0400

    Apply suggestions from code review
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Reactant, SafeTestsets, Test
 # If we are in GitHub Actions, make `TMPDIR` be a local directory from which we
 # can upload artifacts at the end.
 if get(ENV, "GITHUB_ACTIONS", "false") == "true"
-    ENV["TMPDIR"] = mkpath(joinpath(@__DIR__, "..", "tmp"))
+    Reactant.MLIR.IR.DUMP_MLIR_DIR[] = mkpath(joinpath(@__DIR__, "..", "mlir_modules"))
 end
 
 # parse some command-line arguments
