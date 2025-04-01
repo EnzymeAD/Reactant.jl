bbe1a05065dd05e2ee5a9c9d240fa272bdbafe29
William Moses <wmoses@google.com>
2025-04-01 01:39:17 -0500

    Add missing optimizations

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -582,6 +582,13 @@ function optimization_passes(; no_nan::Bool=false, sroa::Bool=false, inline::Boo
         "while_simplify<1>",
         "scatter_update_computation_const_prop",
         "if_remove_unused",
+        "dus_dus<1>",
+        "dus_dus_concat<1>",
+        "select_comp_iota_const_simplify<1>",
+        "sign_abs_simplify<1>",
+        "abs_positive_simplify<1>",
+        "broadcastindim_is_reshape<1>",
+        "slice_reduce_window<1>"
     ]
     if no_nan
         append!(
