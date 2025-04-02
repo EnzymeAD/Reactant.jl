f30526ba839a0dab2a1c810729976f82bc9a1c59
Avik Pal <avikpal@mit.edu>
2025-04-02 02:23:49 -0400

    fix: codegen to fix aliasing issues (#1115)
    * fix: codegen to fix aliasing issues
    
    * fix: incorrect logic
    
    * test: more tests
    
    * fix: overload deepcopy_internal

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -1262,8 +1262,10 @@ function compile_mlir!(
     nresults = MLIR.IR.Value[]
     linear_results2 = TracedType[]
     results_mask = falses(length(results))
+
     for (i, op) in enumerate(results)
-        if !MLIR.IR.is_block_arg(op)
+        if !MLIR.IR.is_block_arg(op) ||
+            !Reactant.TracedUtils.has_idx(linear_results[i], :args) # new buffer
             push!(nresults, op)
             push!(linear_results2, linear_results[i])
             results_mask[i] = true
