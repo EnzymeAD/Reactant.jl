3803b19505def0f8577d2d5e934f5dc74c7deaba
Mosè Giordano <mose@gnu.org>
2025-04-09 00:00:30 +0100

    [deps] Define `using_clang` option when building with Clang

--- deps/build_local.jl
+++ deps/build_local.jl
@@ -160,7 +160,15 @@ if cc_is_gcc
         if gcc_version < v"12"
             push!(build_cmd_list, "--define=xnn_enable_avx512fp16=false")
         end
+        if gcc_version < v"11"
+            # TODO: this is not sufficient to complete a build with GCC 10.
+            push!(build_cmd_list, "--define=xnn_enable_avxvnni=false")
+        end
     end
+else
+    # Assume the compiler is clang if not GCC. `using_clang` is an option
+    # introduced by Enzyme-JAX.
+    push!(build_cmd_list, "--define=using_clang=true")
 end
 push!(build_cmd_list, "--color=$(parsed_args["color"])")
 push!(build_cmd_list, ":libReactantExtra.so")
