a344e25648330925f1903958b5e5d697feab6955
Avik Pal <avikpal@mit.edu>
2025-04-17 23:40:22 -0400

    fix: aliasing of results with buffers set later

--- test/integration/optimisers.jl
+++ test/integration/optimisers.jl
@@ -12,3 +12,12 @@ is_empty_buffer(x::ConcretePJRTNumber) = any(x === C_NULL for x in x.data)
     @test !is_empty_buffer(opt_state[2].rule.eta)
     @test !is_empty_buffer(opt_state[3].rule.eta)
 end
+
+@testset "Correct Aliasing" begin
+    ps = (a = rand(4), b = rand(2)) |> Reactant.to_rarray
+    opt = Reactant.to_rarray(Descent(0.001f0); track_numbers = true)
+
+    st_opt = @jit Optimisers.setup(opt, ps)
+
+    @test st_opt.a.rule.eta.data === st_opt.b.rule.eta.data
+end
