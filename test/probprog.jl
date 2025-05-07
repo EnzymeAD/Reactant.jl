3672d83caa1b53d66bb640cfee6901ece906b89a
sbrantq <scharfrichterq@gmail.com>
2025-05-06 20:14:28 -0500

    improve test

--- test/probprog.jl
+++ test/probprog.jl
@@ -3,29 +3,39 @@ using Reactant: ProbProg
 
 normal(rng, mean, stddev) = mean .+ stddev .* randn(rng, 10000)
 
-function model(mean, stddev)
-    s = ProbProg.sample(normal, StableRNG(0), mean, stddev)
-    t = ProbProg.sample(normal, StableRNG(0), s, stddev)
+function model(rng, mean, stddev)
+    s = ProbProg.sample(normal, rng, mean, stddev)
+    t = ProbProg.sample(normal, rng, s, stddev)
     return t
 end
 
 @testset "ProbProg" begin
     @testset "normal_hlo" begin
-        hlo = @code_hlo ProbProg.generate(
-            model, Reactant.to_rarray(0.0), Reactant.to_rarray(1.0)
+        rng = StableRNG(0)
+        before = @code_hlo optimize = :none ProbProg.generate(
+            model, rng, Reactant.to_rarray(0.0), Reactant.to_rarray(1.0)
         )
-        @test contains(repr(hlo), "enzyme.generate")
-        @test contains(repr(hlo), "enzyme.sample")
-        # println(hlo)
+        @test contains(repr(before), "enzyme.generate")
+        @test contains(repr(before), "enzyme.sample")
 
-        lowered = Reactant.Compiler.run_pass_pipeline_on_source(repr(hlo), "probprog")
-        println(lowered)
+        # println("Before")
+        # println(repr(before))
+
+        after = @code_hlo optimize = :all ProbProg.generate(
+            model, rng, Reactant.to_rarray(0.0), Reactant.to_rarray(1.0)
+        )
+        @test !contains(repr(after), "enzyme.generate")
+        @test !contains(repr(after), "enzyme.sample")
+
+        # println("After")
+        # println(repr(after))
     end
 
     @testset "normal_generate" begin
+        rng = StableRNG(1)
         X = Array(
-            @jit optimize = :probprog ProbProg.generate(
-                model, Reactant.to_rarray(0.0), Reactant.to_rarray(1.0)
+            @jit optimize = :all ProbProg.generate(
+                model, rng, Reactant.to_rarray(0.0), Reactant.to_rarray(1.0)
             )
         )
         @test mean(X) ≈ 0.0 atol = 0.05 rtol = 0.05
