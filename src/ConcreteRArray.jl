afcb3a9d5b2a73f3827d45c158ecddc468a34f6a
William Moses <wmoses@google.com>
2025-05-13 12:50:17 -0500

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/ConcreteRArray.jl
+++ src/ConcreteRArray.jl
@@ -494,16 +494,12 @@ for aType in (:ConcretePJRTArray, :ConcreteIFRTArray)
         end
 
         function Base.copyto!(
-            dest::SubArray{<:Any,<:Any,$(aType)},
-            src::SubArray{<:Any,<:Any,<:Array},
+            dest::SubArray{<:Any,<:Any,$(aType)}, src::SubArray{<:Any,<:Any,<:Array}
         )
-            Base.copyto!(dest, convert(Array, copy(src)))
+            return Base.copyto!(dest, convert(Array, copy(src)))
         end
 
-        function Base.copyto!(
-            dest::SubArray{<:Any,<:Any,$(aType)},
-            src::Array,
-        )
+        function Base.copyto!(dest::SubArray{<:Any,<:Any,$(aType)}, src::Array)
             fn = compile(mycopyto!, (dest, src))
             fn(dest, src)
             return dest
