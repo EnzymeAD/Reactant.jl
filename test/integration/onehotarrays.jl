9622de1416509b35377ce0b49cbb50b1ef8359f2
Avik Pal <avikpal@mit.edu>
2025-05-19 13:25:22 -0400

    chore: run fmt

--- test/integration/onehotarrays.jl
+++ test/integration/onehotarrays.jl
@@ -4,15 +4,15 @@ using OneHotArrays
 using Random
 
 function oh_mul(a, b)
-  return a * b
+    return a * b
 end
 
 @testset "OneHotArrays" begin
-	m = onehotbatch([10, 20, 30, 10, 10], 10:10:40)
-	r_m = Reactant.to_rarray(m)
-	a = rand(100,4)
-	r_a = Reactant.to_rarray(a)
-	r_res = @jit oh_mul(r_a, r_m)
-	res = oh_mul(a, m)
-	@test convert(Array, r_res) ≈ res 
+    m = onehotbatch([10, 20, 30, 10, 10], 10:10:40)
+    r_m = Reactant.to_rarray(m)
+    a = rand(100, 4)
+    r_a = Reactant.to_rarray(a)
+    r_res = @jit oh_mul(r_a, r_m)
+    res = oh_mul(a, m)
+    @test convert(Array, r_res) ≈ res
 end
