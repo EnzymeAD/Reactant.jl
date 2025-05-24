12ca833b650d5f6d1602c17826d8460eb63e184b
William Moses <wmoses@google.com>
2025-05-23 21:30:58 -0500

    Activity tests (#1316)
    * Add copyto override from anytraced rarray
    
    * assert size
    
    * more info
    
    * fix
    
    * Activity tests
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update Project.toml
    
    * fix
    
    * bump
    
    * Update test/autodiff.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/TracedUtils.jl
+++ src/TracedUtils.jl
@@ -42,6 +42,13 @@ function ReactantCore.materialize_traced_array(x::SubArray)
 end
 
 function ReactantCore.materialize_traced_array(x::Base.ReshapedArray)
+    if Base.prod(size(parent(x))) != Base.prod(size(x))
+        throw(
+            AssertionError(
+                "Invalid reshape array, original size $(size(parent(x))) not compatible with new size $(size(x))",
+            ),
+        )
+    end
     return Ops.reshape(materialize_traced_array(parent(x)), size(x)...)
 end
 
