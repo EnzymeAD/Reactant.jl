093e54eaeb652cacd1dec65499af31b4bd32a40b
William Moses <wmoses@google.com>
2025-05-25 13:08:06 -0500

    Update src/xla/PJRT/AsyncBuffer.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/xla/PJRT/AsyncBuffer.jl
+++ src/xla/PJRT/AsyncBuffer.jl
@@ -8,6 +8,6 @@ const AsyncEmptyBuffer = AsyncBuffer(Buffer(C_NULL), nothing)
 AsyncBuffer(args...; kwargs...) = AsyncBuffer(Buffer(args...; kwargs...), nothing)
 
 function Base.copy(b::AsyncBuffer)
-     Base.wait(b)
-     AsyncBuffer(Base.copy(b.buffer), nothing)
+    Base.wait(b)
+    return AsyncBuffer(Base.copy(b.buffer), nothing)
 end
