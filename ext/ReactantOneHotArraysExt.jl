e5e4feac1e48f5ae5eaa37a9f544d93cd064ad06
Avik Pal <avikpal@mit.edu>
2025-05-29 18:00:54 -0400

    fix: broadcasting with OHA (#1353)
    * fix: broadcasting with OHA
    
    * Update ext/ReactantOneHotArraysExt.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix: convert to Array
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/ReactantOneHotArraysExt.jl
+++ ext/ReactantOneHotArraysExt.jl
@@ -20,4 +20,23 @@ function Reactant.traced_type_inner(
     return OneHotArrays.OneHotArray{T2,N,Np1,I2}
 end
 
+# OneHotArray is a <: AbstractArray{Bool, M} so our usual dispatches don't work
+function Reactant.TracedUtils.broadcast_to_size(
+    r::OneHotArrays.OneHotArray{T,N,Np1,<:Reactant.TracedRArray}, rsize
+) where {T,N,Np1}
+    return Reactant.TracedUtils.broadcast_to_size(
+        Reactant.TracedUtils.materialize_traced_array(r), rsize
+    )
+end
+
+function Reactant.TracedUtils.materialize_traced_array(r::OneHotArrays.OneHotArray)
+    return reshape(r.indices, 1, size(r.indices)...) .== 1:(r.nlabels)
+end
+
+function Base.Array(
+    r::OneHotArrays.OneHotArray{T,N,Np1,<:Reactant.AbstractConcreteArray}
+) where {T,N,Np1}
+    return Array(reshape(Array(r.indices), 1, size(r.indices)...) .== 1:(r.nlabels))
+end
+
 end
