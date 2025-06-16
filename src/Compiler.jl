e51dd261a00a36c954713fe5b9864f36fd4b3e07
Avik Pal <avikpal@mit.edu>
2025-06-15 21:11:45 -0400

    feat: missing transpose_scatter pass

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -1071,6 +1071,7 @@ function optimization_passes(;
                 "transpose_batch_norm_inference",
                 "transpose_batch_norm_grad",
                 "transpose_if",
+                "transpose_scatter",
             ],
         )
         if AGGRESSIVE_PROPAGATION[]
