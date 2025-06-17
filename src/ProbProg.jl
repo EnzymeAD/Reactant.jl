d707053c7f553828dea03f458584b1e5181fe356
sbrantq <scharfrichterq@gmail.com>
2025-06-17 16:01:00 -0500

    reorder

--- src/ProbProg.jl
+++ src/ProbProg.jl
@@ -52,70 +52,6 @@ function __init__()
     return nothing
 end
 
-@noinline function generate!(f::Function, args::Vararg{Any,Nargs}) where {Nargs}
-    argprefix::Symbol = gensym("generatearg")
-    resprefix::Symbol = gensym("generateresult")
-    resargprefix::Symbol = gensym("generateresarg")
-
-    mlir_fn_res = invokelatest(
-        TracedUtils.make_mlir_fn,
-        f,
-        args,
-        (),
-        string(f),
-        false;
-        do_transpose=false,
-        args_in_result=:all,
-        argprefix,
-        resprefix,
-        resargprefix,
-    )
-    (; result, linear_args, in_tys, linear_results) = mlir_fn_res
-    fnwrap = mlir_fn_res.fnwrapped
-    func2 = mlir_fn_res.f
-
-    out_tys = [MLIR.IR.type(TracedUtils.get_mlir_data(res)) for res in linear_results]
-    fname = TracedUtils.get_attribute_by_name(func2, "sym_name")
-    fname = MLIR.IR.FlatSymbolRefAttribute(Base.String(fname))
-
-    batch_inputs = MLIR.IR.Value[]
-    for a in linear_args
-        idx, path = TracedUtils.get_argidx(a, argprefix)
-        if idx == 1 && fnwrap
-            TracedUtils.push_val!(batch_inputs, f, path[3:end])
-        else
-            if fnwrap
-                idx -= 1
-            end
-            TracedUtils.push_val!(batch_inputs, args[idx], path[3:end])
-        end
-    end
-
-    gen_op = MLIR.Dialects.enzyme.generate(batch_inputs; outputs=out_tys, fn=fname)
-
-    for (i, res) in enumerate(linear_results)
-        resv = MLIR.IR.result(gen_op, i)
-        if TracedUtils.has_idx(res, resprefix)
-            path = TracedUtils.get_idx(res, resprefix)
-            TracedUtils.set!(result, path[2:end], resv)
-        elseif TracedUtils.has_idx(res, argprefix)
-            idx, path = TracedUtils.get_argidx(res, argprefix)
-            if idx == 1 && fnwrap
-                TracedUtils.set!(f, path[3:end], resv)
-            else
-                if fnwrap
-                    idx -= 1
-                end
-                TracedUtils.set!(args[idx], path[3:end], resv)
-            end
-        else
-            TracedUtils.set!(res, (), resv)
-        end
-    end
-
-    return result
-end
-
 @noinline function sample!(
     f::Function, args::Vararg{Any,Nargs}; symbol::Symbol=gensym("sample")
 ) where {Nargs}
@@ -175,6 +111,70 @@ end
 
     for (i, res) in enumerate(linear_results)
         resv = MLIR.IR.result(sample_op, i)
+        if TracedUtils.has_idx(res, resprefix)
+            path = TracedUtils.get_idx(res, resprefix)
+            TracedUtils.set!(result, path[2:end], resv)
+        elseif TracedUtils.has_idx(res, argprefix)
+            idx, path = TracedUtils.get_argidx(res, argprefix)
+            if idx == 1 && fnwrap
+                TracedUtils.set!(f, path[3:end], resv)
+            else
+                if fnwrap
+                    idx -= 1
+                end
+                TracedUtils.set!(args[idx], path[3:end], resv)
+            end
+        else
+            TracedUtils.set!(res, (), resv)
+        end
+    end
+
+    return result
+end
+
+@noinline function generate!(f::Function, args::Vararg{Any,Nargs}) where {Nargs}
+    argprefix::Symbol = gensym("generatearg")
+    resprefix::Symbol = gensym("generateresult")
+    resargprefix::Symbol = gensym("generateresarg")
+
+    mlir_fn_res = invokelatest(
+        TracedUtils.make_mlir_fn,
+        f,
+        args,
+        (),
+        string(f),
+        false;
+        do_transpose=false,
+        args_in_result=:all,
+        argprefix,
+        resprefix,
+        resargprefix,
+    )
+    (; result, linear_args, in_tys, linear_results) = mlir_fn_res
+    fnwrap = mlir_fn_res.fnwrapped
+    func2 = mlir_fn_res.f
+
+    out_tys = [MLIR.IR.type(TracedUtils.get_mlir_data(res)) for res in linear_results]
+    fname = TracedUtils.get_attribute_by_name(func2, "sym_name")
+    fname = MLIR.IR.FlatSymbolRefAttribute(Base.String(fname))
+
+    batch_inputs = MLIR.IR.Value[]
+    for a in linear_args
+        idx, path = TracedUtils.get_argidx(a, argprefix)
+        if idx == 1 && fnwrap
+            TracedUtils.push_val!(batch_inputs, f, path[3:end])
+        else
+            if fnwrap
+                idx -= 1
+            end
+            TracedUtils.push_val!(batch_inputs, args[idx], path[3:end])
+        end
+    end
+
+    gen_op = MLIR.Dialects.enzyme.generate(batch_inputs; outputs=out_tys, fn=fname)
+
+    for (i, res) in enumerate(linear_results)
+        resv = MLIR.IR.result(gen_op, i)
         if TracedUtils.has_idx(res, resprefix)
             path = TracedUtils.get_idx(res, resprefix)
             TracedUtils.set!(result, path[2:end], resv)
@@ -278,5 +278,4 @@ function print_trace(trace::Dict{Symbol,Any})
     end
     return println("### End of Trace ###")
 end
-
 end
