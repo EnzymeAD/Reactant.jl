c744ff80e6f4bbfeab4a58e86b980b9416e5ef01
William S. Moses <gh@wsmoses.com>
2025-06-24 15:57:34 -0500

    dev

--- deps/build_local.jl
+++ deps/build_local.jl
@@ -138,6 +138,7 @@ push!(build_cmd_list, "--check_visibility=false")
 push!(build_cmd_list, "--verbose_failures")
 push!(build_cmd_list, "--jobs=$(parsed_args["jobs"])")
 push!(build_cmd_list, "--experimental_ui_max_stdouterr_bytes=-1")
+push!(build_cmd_list, "-s")
 for opt in parsed_args["copt"]
     push!(build_cmd_list, "--copt=$(opt)")
 end
