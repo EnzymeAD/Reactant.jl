7c5bfca26d9f0239a48f4b315d0206e247e34a40
Avik Pal <avikpal@mit.edu>
2025-06-28 12:54:56 -0400

    fix: don't initialize jax arrays for tracing

---
+++ ext/ReactantPythonCallExt/pycall.jl
@@ -0,0 +1,17 @@
+function PythonCall.pycall(f::Py, arg0::TracedRArray, argNs::TracedRArray...; kwargs...)
+    JAX_TRACING_SUPPORTED[] || throw("jax could not be loaded.")
+
+    jax = jaxptr[]
+    jnp = jnpptr[]
+
+    inputs = map((arg0, argNs...)) do arg
+        jax.ShapeDtypeStruct(
+            size(arg), jnp.dtype(NUMPY_SIMPLE_TYPES[Reactant.unwrapped_eltype(arg)])
+        )
+    end
+
+    lowered = jax.jit(f).lower(inputs...)
+    res = Reactant.Ops.hlo_call(pyconvert(String, lowered.as_text()), arg0, argNs...)
+
+    return length(res) == 0 ? nothing : res[1]
+end
