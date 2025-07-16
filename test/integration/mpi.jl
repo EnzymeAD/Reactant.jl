3391df8a6aa5d6980b9befd324c01ca28a6c85b1
romanlee <romanlee@lbl.gov>
2025-07-16 14:06:43 -0700

    Add tests for Send and Recv!

--- test/integration/mpi.jl
+++ test/integration/mpi.jl
@@ -21,4 +21,64 @@ end
     @test nranks == @jit MPI.Allreduce(x, MPI.SUM, MPI.COMM_WORLD)
 end
 
+@testset "Send, Recv!" begin
+    comm = MPI.COMM_WORLD
+    rank = MPI.Comm_rank(comm)
+    nranks = MPI.Comm_size(comm)
+
+    if nranks < 2
+        @warn "Need at least 2 MPI processes for send tests. Skipping."
+        return
+    end
+
+    # test MPI.jl Send / Reactant Recv
+    @testset "MPI.jl Send / Reactant Recv!" begin
+        send_buf = fill(1)
+        tag = 43
+        if rank == 0
+            MPI.Send(send_buf, comm; dest=1, tag=tag)
+            @test true
+        elseif rank == 1
+            recv_buf = ConcreteRArray(fill(12))
+            source = 0
+            @jit MPI.Recv!(recv_buf, source, tag, comm)
+            @test recv_buf == send_buf
+        end
+    end
+
+    # test Reactant Send / MPI.jl Recv
+    @testset "Reactant Send / MPI.jl Recv!" begin
+        send_buf = ConcreteRArray(fill(1))
+        tag = 43
+        if rank == 0
+            dest = 1
+            @jit MPI.Send(send_buf, dest, tag, comm)
+            @test true
+        elseif rank == 1
+            recv_buf = fill(12)
+            MPI.Recv!(recv_buf, comm; source=0, tag=tag)
+            @test recv_buf == send_buf
+        end
+    end
+
+    # test Reactant Send/Recv
+    @testset "Reactant Send / Recv!" begin
+        send_buf = ConcreteRArray(fill(1))
+        tag = 43
+        if rank == 0
+            # Send: pass on cpu, pass on gpu
+            dest = 1
+            @jit MPI.Send(send_buf, dest, tag, comm)
+            @test true  # Send completed
+        elseif rank == 1
+            # hang on cpu
+            # segfault on gpu upon trying to reference res
+            recv_buf = ConcreteRArray(fill(12))
+            src = 0
+            @jit MPI.Recv!(recv_buf, src, tag, comm)
+            @test recv_buf == send_buf
+        end
+    end
+end
+
 MPI.Finalize()
