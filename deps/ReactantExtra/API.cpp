66ad028074e51316dd93936b723175394ca36287
William S. Moses <gh@wsmoses.com>
2025-08-01 01:10:45 -0500

    more rebase

--- deps/ReactantExtra/API.cpp
+++ deps/ReactantExtra/API.cpp
@@ -1435,7 +1435,7 @@ extern "C" HeldIfrtArray *ifrt_client_make_array_from_host_buffer(
       std::nullopt, // byte_strides
       sharding->obj(),
       static_cast<ifrt::Client::HostBufferSemantics>(c_semantics),
-      [] {}, // on_done_with_host_buffer)));
+      [] {})));
 }
 
 extern "C" HeldIfrtArray *ifrt_client_make_single_shard_array_from_host_buffer(
@@ -2659,8 +2659,7 @@ extern "C" HeldIfrtArray *ifrt_make_array_from_host_buffer_shards(
       sharding);
   auto arrays = MyValueOrThrow(client->MakeArraysFromHostBufferShards(
       absl::MakeSpan(&spec, 1),
-      static_cast<ifrt::Client::HostBufferSemantics>(c_host_buffer_semantics),
-      client->CreateUserContext()));
+      static_cast<ifrt::Client::HostBufferSemantics>(c_host_buffer_semantics)));
   return reactant::capture(arrays[0]);
 }
 
