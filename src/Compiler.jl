02d2fe64bdf096e5a27b2daf7d60de6794cdbd1c
William Moses <wmoses@google.com>
2025-08-03 18:48:12 -0500

    Prepare for new jll passes

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -898,6 +898,9 @@ function optimization_passes(
         "select_simplify",
         "concatenate_subtract_to_subtract_pad",
         "concatenate_broadcast_in_dim",
+        "compare_abs",
+        "compare_mul",
+        "compare_convert",
     ]
 
     if !compile_options.disable_scatter_gather_optimization_passes
