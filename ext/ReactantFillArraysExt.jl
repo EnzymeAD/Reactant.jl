e0e7bac5421f61e8f964f810c3bbea97e8b6bee1
Avik Pal <avikpal@mit.edu>
2025-08-19 18:03:16 -0400

    feat: FillArrays support

---
+++ ext/ReactantFillArraysExt.jl
@@ -0,0 +1,87 @@
+module ReactantFillArraysExt
+
+using Reactant: Reactant, TracedUtils, TracedRNumber, Ops, Sharding, unwrapped_eltype
+using ReactantCore: ReactantCore
+using FillArrays: FillArrays, AbstractFill, Fill, Ones, Zeros, OneElement
+using GPUArraysCore: @allowscalar
+
+# Tracing
+Reactant._parent_type(T::Type{<:AbstractFill}) = T
+Reactant._parent_type(T::Type{<:OneElement}) = T
+
+Base.@nospecializeinfer function Reactant.traced_type_inner(
+    @nospecialize(FA::Type{Fill{T,N,Axes}}),
+    seen,
+    mode::Reactant.TraceMode,
+    @nospecialize(track_numbers::Type),
+    @nospecialize(sharding),
+    @nospecialize(runtime)
+) where {T,N,Axes}
+    # T will be a number so we need to trace it
+    return Fill{Reactant.traced_type_inner(T, seen, mode, Number, sharding, runtime),N,Axes}
+end
+
+Base.@nospecializeinfer function Reactant.make_tracer(
+    seen, @nospecialize(prev::Fill{T,N,Axes}), @nospecialize(path), mode; kwargs...
+) where {T,N,Axes}
+    return Fill(
+        Reactant.make_tracer(
+            seen, prev.value, (path..., 1), mode; kwargs..., track_numbers=Number
+        ),
+        prev.axes,
+    )
+end
+
+Base.@nospecializeinfer function Reactant.traced_type_inner(
+    @nospecialize(FA::Type{OneElement{T,N,I,A}}),
+    seen,
+    mode::Reactant.TraceMode,
+    @nospecialize(track_numbers::Type),
+    @nospecialize(sharding),
+    @nospecialize(runtime)
+) where {T,N,I,A}
+    # T will be a number so we need to trace it
+    return OneElement{
+        Reactant.traced_type_inner(T, seen, mode, Number, sharding, runtime),N,I,A
+    }
+end
+
+Base.@nospecializeinfer function Reactant.make_tracer(
+    seen, @nospecialize(prev::OneElement{T,N,I,A}), @nospecialize(path), mode; kwargs...
+) where {T,N,I,A}
+    return OneElement(
+        Reactant.make_tracer(
+            seen, prev.val, (path..., 1), mode; kwargs..., track_numbers=Number
+        ),
+        prev.ind,
+        prev.axes,
+    )
+end
+
+# Materialize into a dense array
+function ReactantCore.materialize_traced_array(x::Fill{T}) where {T}
+    return TracedUtils.broadcast_to_size(
+        TracedUtils.promote_to(TracedRNumber{unwrapped_eltype(T)}, x.value), size(x)
+    )
+end
+
+function ReactantCore.materialize_traced_array(x::Ones{T}) where {T}
+    return TracedUtils.broadcast_to_size(unwrapped_eltype(T)(1), size(x))
+end
+
+function ReactantCore.materialize_traced_array(x::Zeros{T}) where {T}
+    return TracedUtils.broadcast_to_size(unwrapped_eltype(T)(0), size(x))
+end
+
+function ReactantCore.materialize_traced_array(x::OneElement{T}) where {T}
+    y = TracedUtils.broadcast_to_size(unwrapped_eltype(T)(0), size(x))
+    @allowscalar setindex!(y, x.val, x.ind...)
+    return y
+end
+
+# some functions to avoid bad performance
+function Base.similar(::OneElement{<:TracedRNumber}, ::Type{T}, dims::Dims) where {T}
+    return TracedUtils.broadcast_to_size(unwrapped_eltype(T)(0), dims)
+end
+
+end
