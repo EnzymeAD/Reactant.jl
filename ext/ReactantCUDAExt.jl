e260219f0ceca5a39ded116a4545aee3f9b62cdf
William Moses <wmoses@google.com>
2025-08-22 17:32:49 +0200

    override ifelse (#1577)
    * override ifelse
    
    * fix

--- ext/ReactantCUDAExt.jl
+++ ext/ReactantCUDAExt.jl
@@ -85,6 +85,14 @@ for jlop in (
     end
 end
 
+@inline Base.ifelse(cond::Bool, a, b::CuTracedRNumber) = Base.ifelse(cond, a, b[])
+@inline Base.ifelse(cond::Bool, a::CuTracedRNumber, b) = Base.ifelse(cond, a[], b)
+@inline Base.ifelse(cond::Bool, a::CuTracedRNumber, b::CuTracedRNumber) = Base.ifelse(cond, a[], b[])
+@inline Base.ifelse(cond::CuTracedRNumber, a, b) = Base.ifelse(cond[], a, b)
+@inline Base.ifelse(cond::CuTracedRNumber, a::CuTracedRNumber, b) = Base.ifelse(cond[], a[], b)
+@inline Base.ifelse(cond::CuTracedRNumber, a, b::CuTracedRNumber) = Base.ifelse(cond[], a, b[])
+@inline Base.ifelse(cond::CuTracedRNumber, a::CuTracedRNumber, b::CuTracedRNumber) = Base.ifelse(cond[], a[], b[])
+
 Base.@constprop :aggressive @inline Base.:^(
     a::CuTracedRNumber{T,A}, b::Integer
 ) where {T,A} = ^(a[], b)
