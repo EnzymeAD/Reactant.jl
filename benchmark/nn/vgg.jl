170bb05a4159ef734bdb75f0018b0e5c0770e0ea
Avik Pal <avikpal@mit.edu>
2025-09-06 00:01:03 -0400

    ci: test limited options for now

--- benchmark/nn/vgg.jl
+++ benchmark/nn/vgg.jl
@@ -2,7 +2,7 @@ function setup_vgg_benchmark!(suite::BenchmarkGroup, backend)
     for depth in (16,), bsize in (32,)
         benchmark_name = "VGG$(depth) bn=true (224 x 224 x 3 x $(bsize))"
 
-        setup_lux_forward_pass_benchmark!(
+        setup_lux_benchmark!(
             suite,
             benchmark_name,
             backend,
