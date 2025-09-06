33228ac338b4d26954f8f175aef4aad1d782687e
Avik Pal <avikpal@mit.edu>
2025-09-06 10:26:21 -0400

    perf: move Neural Operators benchmarks to benchmark/

--- benchmark/nn/vision.jl
+++ benchmark/nn/vision.jl
@@ -1,3 +1,13 @@
+function setup_vit_benchmark!(suite::BenchmarkGroup, backend)
+    for mode in (:tiny, :small), bsize in (32,)
+        benchmark_name = "ViT $(mode) (256 x 256 x 3 x $(bsize))"
+
+        setup_lux_benchmark!(
+            suite, benchmark_name, backend, Vision.ViT(mode), (256, 256, 3, bsize)
+        )
+    end
+end
+
 function setup_vgg_benchmark!(suite::BenchmarkGroup, backend)
     for depth in (16,), bsize in (32,)
         benchmark_name = "VGG$(depth) bn=true (224 x 224 x 3 x $(bsize))"
--- benchmark/nn/vit.jl
+++
@@ -1,9 +0,0 @@
-function setup_vit_benchmark!(suite::BenchmarkGroup, backend)
-    for mode in (:tiny, :small), bsize in (32,)
-        benchmark_name = "ViT $(mode) (256 x 256 x 3 x $(bsize))"
-
-        setup_lux_benchmark!(
-            suite, benchmark_name, backend, Vision.ViT(mode), (256, 256, 3, bsize)
-        )
-    end
-end
