43df184d6596ff79b4b268715cefad62a1b67891
romanlee <romanlee@lbl.gov>
2025-09-10 11:14:21 -0700

    Cleanup testing mess

--- roman-temp-debug/2025.09.mpi/test-isend-irecv.jl
+++ roman-temp-debug/2025.09.mpi/test-isend-irecv.jl
@@ -10,91 +10,101 @@ rank = MPI.Comm_rank(comm)
 nranks = MPI.Comm_size(comm)
 
 
-# ----------------
-# Send/Recv! in one func
-# ----------------
-send_buf = ConcreteRArray([1, 2, 3, 4, 5])
-recv_buf = ConcreteRArray([-1, -2, -3, -4, -5])
-tag = 43
-function aaa(comm, rank, send_buf, recv_buf, tag)
-    if rank == 0
-        dest = 1
-        # ccall(:jl_breakpoint, Cvoid, (Any,), dest)
-        return MPI.Send(send_buf, dest, tag, comm) # kinda hacky, but have to return this otherwise julia optimizes this out
-    elseif rank == 1
-        src = 0
-        # return MPI.Recv!(recv_buf, src, tag, comm)
-        MPI.Recv!(recv_buf, src, tag, comm)
-        return recv_buf
-    end
-end
-
-result = @jit aaa(comm, rank, send_buf, recv_buf, tag)
-
-if rank==0
-    println("Rank $rank: $result")
-elseif rank==1
-    println("Rank $rank: $(result[2])")
-    println( recv_buf == send_buf )
-end
-
-# # rank==1 && sleep(5)
-# # # println("\nRank $rank:\n", @code_hlo optimize=false aaa(comm, rank, send_buf, recv_buf, tag))
-# # println("\nRank $rank:\n", @code_xla aaa(comm, rank, send_buf, recv_buf, tag))
-
-# if rank==0
-#     println("\ncode_hlo optimize=false:\n", @code_hlo optimize=false aaa(comm, rank, send_buf, recv_buf, tag))
-#     println("\ncode_hlo:\n", @code_hlo aaa(comm, rank, send_buf, recv_buf, tag))
-#     println("\ncode_xla:\n", @code_xla aaa(comm, rank, send_buf, recv_buf, tag))
-
-#     bbb = @compile aaa(comm, rank, send_buf, recv_buf, tag)
-#     println("\nlowered:\n", @code_lowered bbb(comm, rank, send_buf, recv_buf, tag))
-#     println("\ntyped:\n", @code_typed bbb(comm, rank, send_buf, recv_buf, tag))
-#     println("\nllvm:\n", @code_llvm bbb(comm, rank, send_buf, recv_buf, tag))
+# # --------------------------
+# # test MPI.jl Isend / Irecv!
+# # --------------------------
+# # Skip test if not enough processes
+# if nranks < 2
+#     @error "Need at least 2 MPI processes for Isend/Irecv test"
 # end
 
-
-
-# ----------------
-# Send/Recv! compiled separately
-# ----------------
-# # test: works
-# send_buf = ConcreteRArray([1, 2, 3, 4, 5])
-# recv_buf = ConcreteRArray([-1, -2, -3, -4, -5])
-# tag = 43
+# send_buf = [1, 2, 3, 4, 5]
+# tag = 42
 # if rank == 0
 #     dest = 1
-#     @jit MPI.Send(send_buf, dest, tag, comm)
+
+#     req_send = MPI.Isend(send_buf, dest, tag, comm)
+
+#     println("Rank 0: Waiting...")
+
+#     MPI.Wait(req_send)
+
+#     println("Rank 0: Sent")
+
 # elseif rank == 1
-#     src = 0
-#     @jit MPI.Recv!(recv_buf, src, tag, comm)
-#     println( recv_buf == send_buf )
+#     recv_buf = Vector{Int}(undef, 5)
+#     source = 0
+
+#     req_recv = MPI.Irecv!(recv_buf, source, tag, comm)
+
+#     println("Rank 1: Waiting...")
+
+#     status = MPI.Wait(req_recv)
+
+#     println( "Rank 1: Received: $(recv_buf == send_buf)" )
+#     # @test MPI.Get_source(status) == 0
+#     # @test MPI.Get_tag(status) == 42
+
 # end
+# # --------------------------
 
 
-# debug
+
+send_buf = ConcreteRArray([1, 2, 3, 4, 5])
+tag = 42
+dest = 1
+function aaa(send_buf, dest, tag, comm)
+    req = MPI.Isend(send_buf, dest, tag, comm)
+    errcode = MPI.Wait(req)
+    return errcode
+end
+@jit aaa(send_buf, dest, tag, comm)
+
+
+
+# recv_buf = ConcreteRArray([1, 2, 3, 4, 5])
+# tag = 42
+# src = 1
+# function aaa(recv_buf, src, tag, comm)
+#     req = MPI.Irecv!(recv_buf, src, tag, comm)
+#     MPI.Wait(req)
+#     return nothing
+# end
+# # @jit Irecv!_Wait(recv_buf, src, tag, comm)
+
+# # if rank==0
+#     println("\ncode_hlo optimize=false:\n", @code_hlo optimize=false aaa(recv_buf, src, tag, comm))
+#     println("\ncode_hlo:\n", @code_hlo aaa(recv_buf, src, tag, comm))
+#     println("\ncode_xla:\n", @code_xla aaa(recv_buf, src, tag, comm))
+
+#     bbb = @compile aaa(recv_buf, src, tag, comm)
+#     println("\nlowered:\n", @code_lowered bbb(recv_buf, src, tag, comm))
+#     println("\ntyped:\n", @code_typed bbb(recv_buf, src, tag, comm))
+#     println("\nllvm:\n", @code_llvm bbb(recv_buf, src, tag, comm))
+# # end
+
+
 # send_buf = ConcreteRArray([1, 2, 3, 4, 5])
 # recv_buf = ConcreteRArray([-1, -2, -3, -4, -5])
-# tag = 43
-# if rank == 0
-#     dest = 1
-
-#     # @jit MPI.Send(send_buf, dest, tag, comm)
-    
-#     println("\ncode_hlo optimize=false:\n", @code_hlo optimize=false MPI.Send(send_buf, dest, tag, comm))
-#     println("\ncode_hlo:\n", @code_hlo MPI.Send(send_buf, dest, tag, comm))
-#     println("\ncode_xla:\n", @code_xla MPI.Send(send_buf, dest, tag, comm))
-    
-#     sss = @compile MPI.Send(send_buf, dest, tag, comm)
-#     println("\nlowered:\n", @code_lowered sss(send_buf, dest, tag, comm))
-#     println("\ntyped:\n", @code_typed sss(send_buf, dest, tag, comm))
-#     println("\nllvm:\n", @code_llvm sss(send_buf, dest, tag, comm))
-
-# # elseif rank == 1
-# #     src = 0
-# #     @jit MPI.Recv!(recv_buf, src, tag, comm)
-# #     println( recv_buf == send_buf )
+# function Isend_Irecv!(comm, rank, send_buf, recv_buf)
+#     if rank==0
+#         dest = 1
+#         tag = 42
+#         req = MPI.Isend(send_buf, dest, tag, comm)
+#         err = MPI.Wait(req)
+#         return err 
+#     elseif rank==1
+#         src = 0 
+#         tag = 42
+#         req = MPI.Irecv!(recv_buf, src, tag, comm)
+#         MPI.Wait(req)
+#         return recv_buf
+#     end
 # end
+# result = @jit Isend_Irecv!(comm, rank, send_buf, recv_buf)
+# println("Rank $rank: $result")
+
+
 
 
 MPI.Finalize()
