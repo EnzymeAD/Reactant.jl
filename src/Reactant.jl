9cd90127055fa5f15f39954b937bafda20eb528f
Avik Pal <avikpal@mit.edu>
2025-09-12 16:26:45 -0400

    fix: try removing nospecialize

--- src/Reactant.jl
+++ src/Reactant.jl
@@ -178,27 +178,28 @@ include("TracedRArray.jl")
 
 include("ConcreteRArray.jl")
 
-Base.@nospecializeinfer use_overlayed_version(x) = false
-Base.@nospecializeinfer function use_overlayed_version(
-    @nospecialize(x::F)
-) where {F<:Function}
+use_overlayed_version(x) = false
+function use_overlayed_version(x::F) where {F<:Function}
     return Base.inferencebarrier(any)(
         use_overlayed_version, getfield.(Ref(x), fieldnames(F))
     )
 end
-Base.@nospecializeinfer function use_overlayed_version(@nospecialize(x::Base.Generator))
+function use_overlayed_version(x::Base.Generator)
     return use_overlayed_version(x.f) || use_overlayed_version(x.iter)
 end
-Base.@nospecializeinfer function use_overlayed_version(@nospecialize(x::Base.Iterators.Zip))
+function use_overlayed_version(x::Base.Iterators.Zip)
     return Base.inferencebarrier(any)(use_overlayed_version, x.is)
 end
-Base.@nospecializeinfer use_overlayed_version(@nospecialize(x::Base.Iterators.Enumerate)) =
-    use_overlayed_version(x.itr)
-Base.@nospecializeinfer use_overlayed_version(@nospecialize(x::Vector)) =
-    Base.inferencebarrier(any)(use_overlayed_version, x)
-Base.@nospecializeinfer use_overlayed_version(@nospecialize(iter::Tuple)) =
-    Base.inferencebarrier(any)(use_overlayed_version, iter)
-Base.@nospecializeinfer function use_overlayed_version(@nospecialize(iter::NamedTuple))
+function use_overlayed_version(x::Base.Iterators.Enumerate)
+    return use_overlayed_version(x.itr)
+end
+function use_overlayed_version(x::Vector)
+    return Base.inferencebarrier(any)(use_overlayed_version, x)
+end
+function use_overlayed_version(iter::Tuple)
+    return Base.inferencebarrier(any)(use_overlayed_version, iter)
+end
+function use_overlayed_version(iter::NamedTuple)
     return Base.inferencebarrier(any)(use_overlayed_version, values(iter))
 end
 use_overlayed_version(::TracedRArray) = true
