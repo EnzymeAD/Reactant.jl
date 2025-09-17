31643bd27be280ffa53457a5172dfd993936eab5
qfl3x <qfl3x@protonmail.com>
2025-09-17 12:48:52 +0200

    Formatter

--- ext/ReactantFixedSizeArraysExt.jl
+++ ext/ReactantFixedSizeArraysExt.jl
@@ -6,26 +6,27 @@ using Reactant: TracedRArray, TracedRNumber, Ops
 using ReactantCore: ReactantCore
 
 function Reactant.traced_type_inner(
-    @nospecialize(_::Type{FixedSizeArrays.FixedSizeArrayDefault{T, N}}),
+    @nospecialize(_::Type{FixedSizeArrays.FixedSizeArrayDefault{T,N}}),
     seen,
     @nospecialize(mode::Reactant.TraceMode),
     @nospecialize(track_numbers::Type),
     @nospecialize(sharding),
     @nospecialize(runtime)
-) where {T, N}
+) where {T,N}
     T2 = Reactant.TracedRNumber{T}
-    return FixedSizeArrays.FixedSizeArrayDefault{T2, N}
+    return FixedSizeArrays.FixedSizeArrayDefault{T2,N}
 end
 
 Base.@nospecializeinfer function Reactant.make_tracer(
     seen,
-    @nospecialize(prev::FixedSizeArrays.FixedSizeArrayDefault{T, N}),
+    @nospecialize(prev::FixedSizeArrays.FixedSizeArrayDefault{T,N}),
     @nospecialize(path),
-    mode; kwargs...
-) where {T, N}
+    mode;
+    kwargs...,
+) where {T,N}
     return Reactant.make_tracer(
         seen, parent(prev), (path..., 1), mode; kwargs..., track_numbers=Number
     )
 end
-    
+
 end
