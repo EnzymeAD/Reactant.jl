730eb1b926b69b78e43b444376237c8334bd201f
Avik Pal <avikpal@mit.edu>
2025-09-21 13:13:48 -0400

    test: remove test groups

--- test/integration/Flux/runtests.jl
+++ test/integration/Flux/runtests.jl
@@ -1,24 +1,11 @@
-using Reactant, Flux, Test
+using Test
 
 @testset "Flux.jl Integration" begin
-    noisy = rand(Float32, 2, 1000)
-    truth = [xor(col[1] > 0.5, col[2] > 0.5) for col in eachcol(noisy)]
+    @testset "Zygote Integration" begin
+        include("zygote.jl")
+    end
 
-    model = Chain(
-        Dense(2 => 3, tanh),
-        BatchNorm(3),
-        Dense(3 => 2),
-        softmax,
-    )
-
-    origout = model(noisy)
-
-    cmodel = Reactant.to_rarray(model)
-    cnoisy = Reactant.to_rarray(noisy)
-
-    fn = (a, b) -> a(b)
-    f = @compile fn(cmodel, cnoisy)
-
-    comp = f(cmodel, cnoisy)
-    @test origout ≈ comp atol = 1e-3 rtol = 1e-2
+    @testset "Flux Integration" begin
+        include("flux.jl")
+    end
 end
