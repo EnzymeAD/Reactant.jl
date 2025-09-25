bbccf0ad4ca1bff38fb5d1c08e255e7ed6d7ab28
Avik Pal <avikpal@mit.edu>
2025-09-24 22:16:04 -0500

    feat: initial triton setup [skip ci]

--- ext/ReactantPythonCallExt/overlays.jl
+++ ext/ReactantPythonCallExt/overlays.jl
@@ -1,6 +1,6 @@
 @reactant_overlay function PythonCall.pycall(f::Py, args...)
     if Reactant.looped_any(Reactant.use_overlayed_version, args)
-        return pycall_with_jax_tracing(f, args...)
+        return overlayed_pycall(f, args...)
     else
         return Base.inferencebarrier(PythonCall.pycall)(f, args...)
     end
