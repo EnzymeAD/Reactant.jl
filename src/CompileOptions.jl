0554bc9bf1e1d7636f29ad0eb1b6300dc998ac00
William Moses <wmoses@google.com>
2025-09-30 10:49:50 -0500

    no dus to pad

--- src/CompileOptions.jl
+++ src/CompileOptions.jl
@@ -12,9 +12,9 @@ communication.
     wrap_comm::Int = 0
     extend_comm::Int = 0
     dus_to_pad_manual_comp_comm::Int = 0 # 2
-    dus_to_pad_comm::Int = 1
+    dus_to_pad_comm::Int = 0
     concat_two_operands_comm::Int = 0
-    concat_to_pad_comm::Int = 1
+    concat_to_pad_comm::Int = 0
     extend_to_pad_comm::Int = 1
     wrap_to_pad_comm::Int = 1
 end
