46222d929100520b19807e24931d9a54a4a74727
sbrantq <scharfrichterq@gmail.com>
2025-10-08 00:11:02 -0500

    test

--- test/probprog/mh.jl
+++ test/probprog/mh.jl
@@ -66,23 +66,29 @@ end
     end
 
     @testset "inference" begin
-        seed = Reactant.to_rarray(UInt64[1, 4])
+        seed = Reactant.to_rarray(UInt64[1, 5])
         rng = ReactantRNG(seed)
 
         xs = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
         ys = [8.23, 5.87, 3.99, 2.59, 0.23, -0.66, -3.53, -6.91, -7.24, -9.90]
         obs = ProbProg.Constraint(:ys => (ys,))
+        num_iters = ConcreteRNumber(10000)
         init_trace, _ = ProbProg.generate_(rng, model, xs; constraint=obs)
 
-        code = @code_hlo optimize = false mh_program(rng, init_trace, model, xs, 10000)
-        println(code)
+        code = @code_hlo optimize = :probprog mh_program(rng, init_trace, model, xs, 10000)
+        @test contains(repr(code), "enzyme_probprog_get_sample_from_trace")
+        @test contains(repr(code), "enzyme_probprog_get_weight_from_trace")
+        @test !contains(repr(code), "enzyme.mh")
 
+        compiled_fn = @compile optimize = :probprog mh_program(
+            rng, init_trace, model, xs, num_iters
+        )
+
+        trace = nothing
         seed_buffer = only(rng.seed.data).buffer
+        num_iters = ConcreteRNumber(1000)
         GC.@preserve seed_buffer init_trace begin
-            trace_ptr = @compile optimize = :probprog mh_program(
-                rng, init_trace, model, xs, 10000
-            )
-
+            trace_ptr = compiled_fn(rng, init_trace, model, xs, num_iters)
             while !isready(trace_ptr)
                 yield()
             end
@@ -90,11 +96,11 @@ end
             trace = unsafe_pointer_to_objref(Ptr{Any}(Array(trace_ptr)[1]))
         end
 
-        slope = trace.choices[:slope][1]
-        intercept = trace.choices[:intercept][1]
+        slope = only(trace.choices[:slope])[1]
+        intercept = only(trace.choices[:intercept])[1]
         @show slope, intercept
 
-        @test slope ≈ -2.0 rtol = 0.05
-        @test intercept ≈ 10.0 rtol = 0.05
+        @test slope ≈ -2.0 rtol = 0.1
+        @test intercept ≈ 10.0 rtol = 0.1
     end
 end
