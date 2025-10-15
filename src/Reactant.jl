f1e62f41469cb2402beb5a28b9182d88455db432
Avik Pal <avikpal@mit.edu>
2025-10-15 11:58:52 -0400

    fix: flip recommendation order

--- src/Reactant.jl
+++ src/Reactant.jl
@@ -335,7 +335,7 @@ function __init__()
             @warn """
             Reactant.jl currently doesn't support versions of Julia 1.12 or newer. We are
             actively working on adding support for newer versions of Julia. For the time
-            being we recommend using 1.10 (current Julia LTS) or 1.11.
+            being we recommend using 1.11 or LTS.
 
             For latest updates, check the status of support for Julia 1.12+ at
             https://github.com/EnzymeAD/Reactant.jl/issues/1736.
