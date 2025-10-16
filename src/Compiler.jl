bea3b16e6731008f5017075c237e566e84826965
Avik Pal <avikpal@mit.edu>
2025-10-16 11:16:56 -0500

    fix: extra export + naming

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -1914,7 +1914,7 @@ function compile_mlir!(
                 end,
                 ',',
             ),
-            "before_kernel",
+            "no_triton",
         )
     elseif compile_options.optimization_passes === :before_kernel
         run_pass_pipeline!(
