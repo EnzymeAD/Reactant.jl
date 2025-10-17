de404f08ef30a3374b17f0101ac4cd309b6033e1
sbrantq <scharfrichterq@gmail.com>
2025-10-16 21:31:40 -0500

    static hmc correctness test

--- test/runtests.jl
+++ test/runtests.jl
@@ -74,5 +74,6 @@ const REACTANT_TEST_GROUP = lowercase(get(ENV, "REACTANT_TEST_GROUP", "all"))
         @safetestset "ProbProg Sample" include("probprog/sample.jl")
         @safetestset "ProbProg Simulate" include("probprog/simulate.jl")
         @safetestset "ProbProg Generate" include("probprog/generate.jl")
+        @safetestset "ProbProg HMC" include("probprog/hmc.jl")
     end
 end
