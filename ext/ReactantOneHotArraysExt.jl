7ba923a57b9e00014c7eed6de8b80770ca9f399a
Avik Pal <avikpal@mit.edu>
2025-10-27 19:23:49 -0500

    feat: support onehotbatch/onecold

--- ext/ReactantOneHotArraysExt.jl
+++ ext/ReactantOneHotArraysExt.jl
@@ -1,9 +1,17 @@
 module ReactantOneHotArraysExt
 
-using OneHotArrays: OneHotArray
-using Reactant: Reactant, TracedRArray, TracedRNumber, Ops
+using GPUArraysCore: @allowscalar
+using OneHotArrays: OneHotArrays, OneHotArray
+using Reactant: Reactant, AnyTracedRArray, TracedRArray, TracedRNumber
 using ReactantCore: ReactantCore
 using Reactant.Ops: @opcall
+
+__compatible_eltype(::Type{T}, ::Type{U}) where {T,U} = T
+function __compatible_eltype(::Type{TracedRNumber{T}}, ::Type{TracedRNumber{U}}) where {T,U}
+    return TracedRNumber{T}
+end
+__compatible_eltype(::Type{TracedRNumber{T}}, ::Type{U}) where {T,U} = T
+__compatible_eltype(::Type{T}, ::Type{TracedRNumber{U}}) where {T,U} = TracedRNumber{T}
 
 function Reactant.traced_type_inner(
     @nospecialize(_::Type{OneHotArray{T,N,Np1,I}}),
@@ -14,12 +22,7 @@ function Reactant.traced_type_inner(
     @nospecialize(runtime)
 ) where {T,N,Np1,I}
     I2 = Reactant.traced_type_inner(I, seen, mode, track_numbers, sharding, runtime)
-    T2 = if eltype(I2) <: Reactant.TracedRNumber && !(T <: Reactant.TracedRNumber)
-        Reactant.TracedRNumber{T}
-    else
-        T
-    end
-    return OneHotArray{T2,N,Np1,I2}
+    return OneHotArray{__compatible_eltype(T, eltype(I2)),N,Np1,I2}
 end
 
 function ReactantCore.materialize_traced_array(r::OneHotArray)
@@ -43,6 +46,56 @@ function Base.Array(
     r::OneHotArray{T,N,Np1,<:Reactant.AbstractConcreteArray}
 ) where {T,N,Np1}
     return Array(reshape(Array(r.indices), 1, size(r.indices)...) .== 1:(r.nlabels))
+end
+
+function OneHotArrays.onehotbatch(
+    data::AnyTracedRArray{<:Integer,N}, labels::AbstractVector{<:Integer}
+) where {N}
+    # TODO: add checkbounds once we support that with TracedRNumber
+    indices =
+        UInt32.(
+            map(
+                Base.Fix2(+, 1 - first(labels)), ReactantCore.materialize_traced_array(data)
+            )
+        )
+    return indices
+    return OneHotArray{TracedRNumber{UInt32},N,N + 1,typeof(indices)}(
+        indices, length(labels)
+    )
+end
+
+function OneHotArrays.onecold(y::AnyTracedRArray{T,1}, labels=1:length(y)) where {T}
+    nl = length(labels)
+    ny = length(y)
+    nl == ny || throw(
+        DimensionMismatch(
+            "onecold got $nl labels for a vector of length $ny, these must agree"
+        ),
+    )
+    imax = argmax(y)
+    # TODO: error if ymax is nan
+    labels_arr = Reactant.promote_to(
+        TracedRArray{Reactant.unwrapped_eltype(labels),1}, labels
+    )
+    return @allowscalar labels_arr[imax]
+end
+function OneHotArrays.onecold(y::AnyTracedRArray{T}, labels=1:size(y, 1)) where {T}
+    nl = length(labels)
+    ny = size(y, 1)
+    nl == ny || throw(
+        DimensionMismatch(
+            "onecold got $nl labels for an array with first dimension of size $ny, these must agree",
+        ),
+    )
+    labels_arr = Reactant.promote_to(
+        TracedRArray{Reactant.unwrapped_eltype(labels),1}, labels
+    )
+    labels_expanded = @opcall broadcast_in_dim(
+        labels_arr, Int64[1], Int64[nl, size(y)[2:end]...]
+    )
+    return ReactantCore.materialize_traced_array(
+        vec(getindex(labels_expanded, argmax(y; dims=1)))
+    )
 end
 
 end
--- test/integration/onehotarrays.jl
+++ test/integration/onehotarrays.jl
@@ -31,3 +31,22 @@ end
         @test res_ra ≈ res
     end
 end
+
+using Reactant, Test, OneHotArrays, Random
+
+@testset "onehotbatch/onecold" begin
+    x = Int32[10, 20, 30, 10, 10]
+    x_ra = Reactant.to_rarray(x)
+    labels = Int32.(10:10:40)
+
+    res_ra = @jit onehotbatch(x_ra, labels) # XXX: broken??
+    res = onehotbatch(x, labels)
+    @test Array(res_ra) ≈ res
+
+    vec_ra = Reactant.to_rarray(Float32[0.3, 0.2, 0.5])
+    @test @jit(onecold(vec_ra)) == 3
+
+    dense_ra = Reactant.to_rarray(Array(res))
+    oc_res = onecold(res)
+    @test @jit(onecold(dense_ra)) == oc_res
+end
