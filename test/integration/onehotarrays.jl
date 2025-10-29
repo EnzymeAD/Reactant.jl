2da6fe306e2c7af5173ce7a61b7cd11b96e4a0ac
Avik Pal <avikpal@mit.edu>
2025-10-29 10:04:04 -0400

    feat: support onehotbatch/onecold (#1794)
    * fix: try always downloading libtpu on ci
    
    * feat: version check for libtpu
    
    * feat: support onehotbatch/onecold
    
    * fix: onehotbatch
    
    * Apply suggestion from @github-actions[bot]
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix: use batched findfirst
    
    * Apply suggestion from @avik-pal
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/integration/onehotarrays.jl
+++ test/integration/onehotarrays.jl
@@ -31,3 +31,33 @@ end
         @test res_ra ≈ res
     end
 end
+
+@testset "onehotbatch/onecold" begin
+    x = Int32[10, 20, 30, 10, 10]
+    x_ra = Reactant.to_rarray(x)
+    labels = Int32(10):Int32(10):Int32(40)
+    res_ra = @jit onehotbatch(x_ra, labels)
+    res = onehotbatch(x, labels)
+    @test Array(res_ra) ≈ res
+
+    x = rand(10:10:40, 2, 3, 5)
+    x_ra = Reactant.to_rarray(x)
+    labels = reshape([10, 20, 30, 40], 2, 2)
+    res = onehotbatch(x, labels)
+    res_ra = @jit onehotbatch(x_ra, labels)
+    @test Array(res_ra) ≈ res
+
+    x = Int32[1, 2, 3, 1, 1]
+    x_ra = Reactant.to_rarray(x)
+    labels = Int32(1):Int32(4)
+    res_ra = @jit onehotbatch(x_ra, labels)
+    res = onehotbatch(x, labels)
+    @test Array(res_ra) ≈ res
+
+    vec_ra = Reactant.to_rarray(Float32[0.3, 0.2, 0.5])
+    @test @jit(onecold(vec_ra)) == 3
+
+    dense_ra = Reactant.to_rarray(Array(res))
+    oc_res = onecold(res)
+    @test @jit(onecold(dense_ra)) == oc_res
+end
