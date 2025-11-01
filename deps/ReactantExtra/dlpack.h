d7c9eb8da15fee30e24e0935e01cdeeb031e94d0
Avik Pal <avikpal@mit.edu>
2025-10-31 22:01:33 -0500

    feat: setup for dlpack

---
+++ deps/ReactantExtra/dlpack.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "mlir-c/IR.h"
+#include "mlir-c/Support.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define REACTANT_ABI extern "C" MLIR_CAPI_EXPORTED
+
+
+
+#ifdef __cplusplus
+}
+#endif
