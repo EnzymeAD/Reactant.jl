e715c44c70428eb5012da389213f49d8794a35cb
Roman Lee <rplee7@gmail.com>
2025-11-26 11:01:53 -0600

    Comment out stuff from last commit so reactant Precompiles

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -1798,7 +1798,7 @@ function compile_mlir!(
                                    lower-enzymexla-lapack{backend=$backend \
                                    blas_int_width=$blas_int_width}"
 
-    lower_enzymexla_mpi_pass = "lower-enzymexla-mpi{backend=$backend}"
+    # lower_enzymexla_mpi_pass = "lower-enzymexla-mpi{backend=$backend}"
 
     legalize_chlo_to_stablehlo =
         if legalize_stablehlo_to_mhlo || compile_options.legalize_chlo_to_stablehlo
@@ -1829,7 +1829,7 @@ function compile_mlir!(
                         legalize_chlo_to_stablehlo...,
                         opt_passes2,
                         lower_enzymexla_linalg_pass,
-                        lower_enzymexla_mpi_pass,
+                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 else
@@ -1848,7 +1848,7 @@ function compile_mlir!(
                         kern,
                         raise_passes,
                         lower_enzymexla_linalg_pass,
-                        lower_enzymexla_mpi_pass,
+                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 end,
@@ -2002,7 +2002,7 @@ function compile_mlir!(
                         legalize_chlo_to_stablehlo...,
                         opt_passes2,
                         lower_enzymexla_linalg_pass,
-                        lower_enzymexla_mpi_pass,
+                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 else
@@ -2018,7 +2018,7 @@ function compile_mlir!(
                         kern,
                         raise_passes,
                         lower_enzymexla_linalg_pass,
-                        lower_enzymexla_mpi_pass,
+                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 end,
@@ -2041,7 +2041,7 @@ function compile_mlir!(
                         enzyme_pass,
                         "canonicalize,remove-unnecessary-enzyme-ops,enzyme-simplify-math",
                         lower_enzymexla_linalg_pass,
-                        lower_enzymexla_mpi_pass,
+                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 else
@@ -2055,7 +2055,7 @@ function compile_mlir!(
                         kern,
                         raise_passes,
                         lower_enzymexla_linalg_pass,
-                        lower_enzymexla_mpi_pass,
+                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 end,
