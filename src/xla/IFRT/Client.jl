5762ebfd1051c09c5f2843566e4ed2d8debd937e
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-12-02 22:14:23 +0000

    Create TT client via plugin for TensTorrent devices (#1860)
    * Create TT client via plugin for TensTorrent devices
    
    * Automatically download wheel for TT PJRT plugin
    
    * Remove self-qualified accesses
    
    * [tt plugin] Implement logic to detect Tenstorrent devices
    
    * [tt plugin] More comments
    
    * Update URL of TT PJRT plugin
    
    * [docs] Mention Tenstorrent as experimental backend

--- src/xla/IFRT/Client.jl
+++ src/xla/IFRT/Client.jl
@@ -115,12 +115,14 @@ const cpu_client_count = Ref(0)
 const cuda_client_count = Ref(0)
 const tpu_client_count = Ref(0)
 const metal_client_count = Ref(0)
+const tt_client_count = Ref(0)
 
 for (backend, counter) in (
     (:CPUClient, :cpu_client_count),
     (:CUDAClient, :cuda_client_count),
     (:TPUClient, :tpu_client_count),
     (:MetalClient, :metal_client_count),
+    (:TTClient, :tt_client_count),
 )
     main_fn = Symbol(:MakeIFRTPJRT, backend)
     @eval function $(backend)(args...; checkcount::Bool=true, kwargs...)
@@ -216,6 +218,17 @@ function MakeIFRTPJRTMetalClient(;
         node_id,
         num_nodes,
         distributed_runtime_client,
+    )
+end
+
+function MakeIFRTPJRTTTClient(;
+    tt_pjrt_plugin_path::String,
+    node_id::Integer=0,
+    num_nodes::Integer=1,
+    distributed_runtime_client::Union{Nothing,XLA.DistributedRuntimeClient}=nothing,
+)
+    return MakeIFRTPJRTClientViaPluginAPI(
+        tt_pjrt_plugin_path, "tt", "TT"; node_id, num_nodes, distributed_runtime_client
     )
 end
 
