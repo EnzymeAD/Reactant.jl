c66c2d19067da7a721d1a08f6972300d2f9b09b8
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-12-09 22:37:58 -0600

    test structarray to_rarray and make_tracer

---
+++ test/integration/structarrays.jl
@@ -0,0 +1,41 @@
+using StructArrays, Reactant, Test
+
+@testset "StructArray to_rarray and make_tracer" begin
+    x = StructArray(;
+        a=rand(10, 2), b=fill("some strings", (10, 2)), c=rand(Float32, 10, 2)
+    )
+    x_ra = Reactant.to_rarray(x)
+
+    # Note that the element type (the NamedTuple) contains ConcreteRNumbers even though track_numbers is not enabled.
+    # This is because when the backing arrays are converted to TracedRArrays, their elements will contain TracedRNumbers.
+    # In order for the element type to match the backing arrays, we need to use ConcreteRNumbers here as well:
+    @test typeof(x_ra) == StructArray{
+        @NamedTuple{
+            a::ConcretePJRTNumber{Float64,1}, b::String, c::ConcretePJRTNumber{Float32,1}
+        },
+        2,
+        @NamedTuple{
+            a::ConcretePJRTArray{Float64,2,1},
+            b::Matrix{String},
+            c::ConcretePJRTArray{Float32,2,1},
+        },
+        CartesianIndex{2},
+    }
+
+    @test typeof(
+        make_tracer(Reactant.OrderedIdDict(), x_ra, (), Reactant.ConcreteToTraced)
+    ) == StructArray{
+        @NamedTuple{
+            a::Reactant.TracedRNumber{Float64},
+            b::String,
+            c::Reactant.TracedRNumber{Float32},
+        },
+        2,
+        @NamedTuple{
+            a::Reactant.TracedRArray{Float64,2},
+            b::Matrix{String},
+            c::Reactant.TracedRArray{Float32,2},
+        },
+        Int64,
+    }
+end
