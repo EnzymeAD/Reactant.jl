978aeb7d5b2bf668e37583cff7133a9aa14d7356
Roman Lee <rplee7@gmail.com>
2025-12-10 19:00:44 -0600

    Temporarily move lower_enzymexla_mpi_pass to the front

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -1798,7 +1798,7 @@ function compile_mlir!(
                                    lower-enzymexla-lapack{backend=$backend \
                                    blas_int_width=$blas_int_width}"
 
-    # lower_enzymexla_mpi_pass = "lower-enzymexla-mpi{backend=$backend}"
+    lower_enzymexla_mpi_pass = "lower-enzymexla-mpi{backend=$backend}"
 
     legalize_chlo_to_stablehlo =
         if legalize_stablehlo_to_mhlo || compile_options.legalize_chlo_to_stablehlo
@@ -1816,6 +1816,7 @@ function compile_mlir!(
                 if compile_options.raise_first
                     [
                         "mark-func-memory-effects",
+                        lower_enzymexla_mpi_pass,
                         opt_passes,
                         kern,
                         raise_passes,
@@ -1829,12 +1830,12 @@ function compile_mlir!(
                         legalize_chlo_to_stablehlo...,
                         opt_passes2,
                         lower_enzymexla_linalg_pass,
-                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 else
                     [
                         "mark-func-memory-effects",
+                        lower_enzymexla_mpi_pass,
                         opt_passes,
                         "enzyme-batch",
                         opt_passes2,
@@ -1848,7 +1849,6 @@ function compile_mlir!(
                         kern,
                         raise_passes,
                         lower_enzymexla_linalg_pass,
-                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 end,
@@ -2002,7 +2002,6 @@ function compile_mlir!(
                         legalize_chlo_to_stablehlo...,
                         opt_passes2,
                         lower_enzymexla_linalg_pass,
-                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 else
@@ -2018,7 +2017,6 @@ function compile_mlir!(
                         kern,
                         raise_passes,
                         lower_enzymexla_linalg_pass,
-                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 end,
@@ -2041,7 +2039,6 @@ function compile_mlir!(
                         enzyme_pass,
                         "canonicalize,remove-unnecessary-enzyme-ops,enzyme-simplify-math",
                         lower_enzymexla_linalg_pass,
-                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 else
@@ -2055,7 +2052,6 @@ function compile_mlir!(
                         kern,
                         raise_passes,
                         lower_enzymexla_linalg_pass,
-                        # lower_enzymexla_mpi_pass,
                         jit,
                     ]
                 end,
