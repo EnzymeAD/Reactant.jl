ff5647e5f322ea33f14683e0930a93bd61066475
Avik Pal <avikpal@mit.edu>
2025-12-11 23:00:48 -0500

    feat: bump jll (#1957)
    * feat: bump jll
    
    * fix: disable dot_general_broadcast_in_dim
    
    * fix: disable all the new passes
    
    * feat: bump jll

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -915,6 +915,9 @@ function optimization_passes(
         "fuse_mul_into_syrk",
         "fuse_add_into_syrk",
         "factor_scalars_in_dot_general",
+        "reduce_mul_to_dot_general",
+        "dot_general_broadcast_in_dim",
+        "dot_general_broadcast_in_dim_sort_dims",
     ]
 
     if !is_sharded
