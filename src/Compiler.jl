1b5e19ac44c43a6b3cf62941a37f37bfedb1e543
William Moses <gh@wsmoses.com>
2026-01-07 09:52:38 -0500

    Update Compiler.jl

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -835,6 +835,7 @@ function optimization_passes(
         "while_op_induction_replacement",
         "dus_concat",
         "slice_dus_to_concat",
+        "sink_dus",
         "while_induction_reduction",
         "slice_broadcast",
         "associative_common_mul_op_reordering",
