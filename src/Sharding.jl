19e327565af8f6ddc7e06360783c321ef381091c
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2026-01-10 05:24:06 -0600

    fix MLIR context on sharded array print

--- src/Sharding.jl
+++ src/Sharding.jl
@@ -834,25 +834,26 @@ function HloSharding(sharding::DimsSharding, size_x)
 end
 
 function Base.convert(::Type{HloSharding}, sharding::NamedSharding)
-    # MLIR.IR.with_context(; allow_use_existing=true) do
-    ctx = MLIR.IR.context()
-    MLIR.IR.@scope ctx begin
-        mesh_op = Reactant.Ops.mesh(
-            sharding.mesh; mod=MLIR.IR.Module(; location=MLIR.IR.Location(; context=ctx))
-        )
+    MLIR.IR.@dispose ctx = MLIR.IR.Context() begin
+        MLIR.IR.register_enzymexla_dialects(ctx)
+        MLIR.IR.@scope ctx begin
+            mesh_op = Reactant.Ops.mesh(
+                sharding.mesh; mod=MLIR.IR.Module()
+            )
 
-        tensor_sharding_attr, _ = get_tensor_sharding_attribute(
-            sharding, ctx, mesh_op.sym_name, mesh_op.mesh_attr, nothing; dialect=:sdy
-        )
+            tensor_sharding_attr, _ = get_tensor_sharding_attribute(
+                sharding, ctx, mesh_op.sym_name, mesh_op.mesh_attr, nothing; dialect=:sdy
+            )
 
-        return HloSharding(
-            hlo_sharding_from_sdy_tensor_sharding_attr(
-                tensor_sharding_attr, mesh_op.mesh_attr
-            ),
-            sharding.mesh,
-            sharding.is_closed,
-            sharding.priority,
-        )
+            return HloSharding(
+                hlo_sharding_from_sdy_tensor_sharding_attr(
+                    tensor_sharding_attr, mesh_op.mesh_attr
+                ),
+                sharding.mesh,
+                sharding.is_closed,
+                sharding.priority,
+            )
+        end
     end
 end
 
