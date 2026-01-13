930d8b2d14b90377f3356fca9650c1e241edfc8d
William S. Moses <gh@wsmoses.com>
2026-01-13 17:24:21 -0600

    fix

--- src/Reactant.jl
+++ src/Reactant.jl
@@ -120,7 +120,7 @@ unwrapped_eltype(::Type{<:AbstractArray{T,N}}) where {T,N} = unwrapped_eltype(T)
 unwrapped_eltype(::AbstractArray{T,N}) where {T,N} = unwrapped_eltype(T)
 
 include("Ops.jl")
-no_rewrite_ancestor_modules.push_back(Reactant.Ops)
+no_rewrite_ancestor_modules.push_back!(Reactant.Ops)
 
 using .Ops: @opcall
 
@@ -191,7 +191,7 @@ end
 
 include("TracedPromotion.jl")
 include("TracedUtils.jl")
-no_rewrite_ancestor_modules.push_back(Reactant.TracedUtils)
+no_rewrite_ancestor_modules.push_back!(Reactant.TracedUtils)
 
 include("TracedRNumber.jl")
 include("TracedRArray.jl")
