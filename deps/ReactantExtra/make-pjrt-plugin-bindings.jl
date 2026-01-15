6ad2776a4969612410b863bf2499f902bb1bc4a6
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2026-01-15 15:49:21 +0100

    format
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- deps/ReactantExtra/make-pjrt-plugin-bindings.jl
+++ deps/ReactantExtra/make-pjrt-plugin-bindings.jl
@@ -12,21 +12,20 @@ end
 function main(; output_dir::String=DEFAULT_OUTPUT_DIR, force::Bool=false)
     bazel_target = "julia_pjrt_capi_bindings"
 
-    run(
-        Cmd(
-            `$(bazel_cmd) build
-            --action_env=JULIA=$(Base.julia_cmd().exec[1])
-            --jobs=$(Threads.nthreads())
-            --action_env=JULIA_DEPOT_PATH=$(Base.DEPOT_PATH)
-            --repo_env HERMETIC_PYTHON_VERSION="3.10"
-            --check_visibility=false
-            --verbose_failures
-            //:$bazel_target
-            `; dir=@__DIR__,
-        ),
-    )
+    run(Cmd(
+        `$(bazel_cmd) build
+        --action_env=JULIA=$(Base.julia_cmd().exec[1])
+        --jobs=$(Threads.nthreads())
+        --action_env=JULIA_DEPOT_PATH=$(Base.DEPOT_PATH)
+        --repo_env HERMETIC_PYTHON_VERSION="3.10"
+        --check_visibility=false
+        --verbose_failures
+        //:$bazel_target
+        `;
+        dir=@__DIR__,
+    ))
 
-    Base.Filesystem.cp(
+    return Base.Filesystem.cp(
         joinpath(@__DIR__, "bazel-bin", "CAPI.jl"), joinpath(output_dir, "CAPI.jl"); force
     )
 end
