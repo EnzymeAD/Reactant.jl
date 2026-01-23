eac1c7ca0da6fb67f476c6f88dfad6da3e3c664f
Avik Pal <avikpal@mit.edu>
2026-01-23 12:43:05 -0600

    fix: finite differences for complex inputs

--- src/PrimitiveTypes.jl
+++ src/PrimitiveTypes.jl
@@ -71,6 +71,7 @@ const ReactantFloatInt = Union{
 
 const ReactantPrimitive = Union{
     Bool,
+    Complex{Bool},
     Base.uniontypes(ReactantFloatInt)...,
     Base.uniontypes(ReactantComplexInt)...,
     Base.uniontypes(ReactantComplexFloat)...,
