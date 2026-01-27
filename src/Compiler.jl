c1c81a42d430c91f09a3e4bc5eeea272d85fa470
Avik Pal <avikpal@mit.edu>
2026-01-27 06:53:36 -0600

    feat: new upstream passes

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -974,6 +974,7 @@ function optimization_passes(
                 "scatter_div_simplify",
                 "unary_elementwise_scatter_simplify",
                 "scatter_indices_are_unique",
+                "split_complex_scatter_setindex",
                 ## const prop patterns
                 "scatter_update_computation_const_prop",
                 # gather patterns
