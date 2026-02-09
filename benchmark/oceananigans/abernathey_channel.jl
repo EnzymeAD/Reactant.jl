020fa1f75379090b7c8bdcb92570d84fe54cd586
Avik Pal <avikpal@mit.edu>
2026-02-09 10:26:53 -0600

    fix: merge projects

--- benchmark/oceananigans/abernathey_channel.jl
+++ benchmark/oceananigans/abernathey_channel.jl
@@ -1,9 +1,5 @@
-#using Pkg
-# pkg"add Oceananigans CairoMakie"
 using Oceananigans
 ENV["GKSwstype"] = "100"
-
-pushfirst!(LOAD_PATH, @__DIR__)
 
 using Printf
 using Statistics
@@ -20,18 +16,12 @@ using CUDA
 
 using Reactant
 using Oceananigans.Architectures: ReactantState
-#Reactant.set_default_backend("cpu")
 
 using Enzyme
 
 Oceananigans.defaults.FloatType = Float64
 
 graph_directory = "run_abernathy_model_ad_spinup100_100steps/"
-#graph_directory = "run_abernathy_model_ad_spinup40000000_8100steps/"
-
-#
-# Model parameters to set first:
-#
 
 # number of grid points
 const Nx = 80  # LowRes: 48
@@ -49,9 +39,9 @@ const Ly = 2000kilometers # meridional domain length [m]
 const Lz = sum(Δz_center)
 
 z_faces = vcat([-Lz], -Lz .+ cumsum(Δz_center))
-z_faces[Nz+1] = 0
+z_faces[Nz + 1] = 0
 
-Δz = z_faces[2:end] - z_faces[1:end-1]
+Δz = z_faces[2:end] - z_faces[1:(end - 1)]
 
 Δz = reshape(Δz, 1, :)
 
@@ -68,47 +58,47 @@ const cᵖ = 3994.0   # [J/K]  heat capacity
 const ρ = 999.8    # [kg/m³] reference density
 
 parameters = (
-    Ly = Ly,
-    Lz = Lz,
-    Qᵇ = 10 / (ρ * cᵖ) * α * g,            # buoyancy flux magnitude [m² s⁻³]
-    Qᵀ = 10 / (ρ * cᵖ),                    # temperature flux magnitude
-    y_shutoff = 5 / 6 * Ly,                # shutoff location for buoyancy flux [m]
-    τ = 0.2 / ρ,                           # surface kinematic wind stress [m² s⁻²]
-    μ = 1 / 30days,                      # bottom drag damping time-scale [s⁻¹]
-    ΔB = 8 * α * g,                      # surface vertical buoyancy gradient [s⁻²]
-    ΔT = 8,                              # surface vertical temperature gradient
-    H = Lz,                              # domain depth [m]
-    h = 1000.0,                          # exponential decay scale of stable stratification [m]
-    y_sponge = 19 / 20 * Ly,               # southern boundary of sponge layer [m]
-    λt = 7.0days                         # relaxation time scale [s]
+    Ly=Ly,
+    Lz=Lz,
+    Qᵇ=10 / (ρ * cᵖ) * α * g,            # buoyancy flux magnitude [m² s⁻³]
+    Qᵀ=10 / (ρ * cᵖ),                    # temperature flux magnitude
+    y_shutoff=5 / 6 * Ly,                # shutoff location for buoyancy flux [m]
+    τ=0.2 / ρ,                           # surface kinematic wind stress [m² s⁻²]
+    μ=1 / 30days,                      # bottom drag damping time-scale [s⁻¹]
+    ΔB=8 * α * g,                      # surface vertical buoyancy gradient [s⁻²]
+    ΔT=8,                              # surface vertical temperature gradient
+    H=Lz,                              # domain depth [m]
+    h=1000.0,                          # exponential decay scale of stable stratification [m]
+    y_sponge=19 / 20 * Ly,               # southern boundary of sponge layer [m]
+    λt=7.0days,                         # relaxation time scale [s]
 )
 
 # full ridge function:
 function ridge_function(x, y)
-    zonal = (Lz+3000)exp(-(x - Lx/2)^2/(1e6kilometers))
-    gap   = 1 - 0.5(tanh((y - (Ly/6))/1e5) - tanh((y - (Ly/2))/1e5))
+    zonal = (Lz + 3000)exp(-(x - Lx / 2)^2 / (1e6kilometers))
+    gap = 1 - 0.5(tanh((y - (Ly / 6)) / 1e5) - tanh((y - (Ly / 2)) / 1e5))
     return zonal * gap - Lz
 end
 
 function wall_function(x, y)
     zonal = (x > 470kilometers) && (x < 530kilometers)
-    gap   = (y < 400kilometers) || (y > 1000kilometers)
-    return (Lz+1) * zonal * gap - Lz
+    gap = (y < 400kilometers) || (y > 1000kilometers)
+    return (Lz + 1) * zonal * gap - Lz
 end
 
-
 function make_grid(architecture, Nx, Ny, Nz, z_faces)
-
-    underlying_grid = RectilinearGrid(architecture,
-        topology = (Periodic, Bounded, Bounded),
-        size = (Nx, Ny, Nz),
-        halo = (halo_size, halo_size, halo_size),
-        x = (0, Lx),
-        y = (0, Ly),
-        z = z_faces)
+    underlying_grid = RectilinearGrid(
+        architecture;
+        topology=(Periodic, Bounded, Bounded),
+        size=(Nx, Ny, Nz),
+        halo=(halo_size, halo_size, halo_size),
+        x=(0, Lx),
+        y=(0, Ly),
+        z=z_faces,
+    )
 
     # Make into a ridge array:
-    ridge = Field{Center, Center, Nothing}(underlying_grid)
+    ridge = Field{Center,Center,Nothing}(underlying_grid)
     @allowscalar set!(ridge, wall_function)
 
     grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(ridge))
@@ -120,33 +110,35 @@ end
 #####
 
 function build_model(grid, Δt₀, parameters)
+    temperature_flux_bc = FluxBoundaryCondition(Field{Center,Center,Nothing}(grid))
 
-    temperature_flux_bc = FluxBoundaryCondition(Field{Center, Center, Nothing}(grid))
+    u_stress_bc = FluxBoundaryCondition(Field{Face,Center,Nothing}(grid))
+    v_stress_bc = FluxBoundaryCondition(Field{Center,Face,Nothing}(grid))
 
-    u_stress_bc = FluxBoundaryCondition(Field{Face, Center, Nothing}(grid))
-    v_stress_bc = FluxBoundaryCondition(Field{Center, Face, Nothing}(grid))
+    @inline u_drag(i, j, grid, clock, model_fields, p) =
+        @inbounds -p.μ * p.Lz * model_fields.u[i, j, 1]
+    @inline v_drag(i, j, grid, clock, model_fields, p) =
+        @inbounds -p.μ * p.Lz * model_fields.v[i, j, 1]
 
-    @inline u_drag(i, j, grid, clock, model_fields, p) = @inbounds -p.μ * p.Lz * model_fields.u[i, j, 1]
-    @inline v_drag(i, j, grid, clock, model_fields, p) = @inbounds -p.μ * p.Lz * model_fields.v[i, j, 1]
+    u_drag_bc = FluxBoundaryCondition(u_drag; discrete_form=true, parameters=parameters)
+    v_drag_bc = FluxBoundaryCondition(v_drag; discrete_form=true, parameters=parameters)
 
-    u_drag_bc = FluxBoundaryCondition(u_drag, discrete_form = true, parameters = parameters)
-    v_drag_bc = FluxBoundaryCondition(v_drag, discrete_form = true, parameters = parameters)
+    T_bcs = FieldBoundaryConditions(; top=temperature_flux_bc)
 
-    T_bcs = FieldBoundaryConditions(top = temperature_flux_bc)
-
-    u_bcs = FieldBoundaryConditions(top = u_stress_bc, bottom = u_drag_bc)
-    v_bcs = FieldBoundaryConditions(top = v_stress_bc, bottom = v_drag_bc)
+    u_bcs = FieldBoundaryConditions(; top=u_stress_bc, bottom=u_drag_bc)
+    v_bcs = FieldBoundaryConditions(; top=v_stress_bc, bottom=v_drag_bc)
 
     #####
     ##### Coriolis
     #####
-    coriolis = BetaPlane(f₀ = f, β = β)
+    coriolis = BetaPlane(; f₀=f, β=β)
 
     #####
     ##### Forcing and initial condition
     #####
-    @inline initial_temperature(z, p) = p.ΔT * (exp(z / p.h) - exp(-p.Lz / p.h)) / (1 - exp(-p.Lz / p.h))
-    @inline mask(y, p)                = max(0.0, y - p.y_sponge) / (Ly - p.y_sponge)
+    @inline initial_temperature(z, p) =
+        p.ΔT * (exp(z / p.h) - exp(-p.Lz / p.h)) / (1 - exp(-p.Lz / p.h))
+    @inline mask(y, p) = max(0.0, y - p.y_sponge) / (Ly - p.y_sponge)
 
     @inline function temperature_relaxation(i, j, k, grid, clock, model_fields, p)
         timescale = p.λt
@@ -154,11 +146,11 @@ function build_model(grid, Δt₀, parameters)
         z = znode(k, grid, Center())
         target_T = initial_temperature(z, p)
         T = @inbounds model_fields.T[i, j, k]
-    
+
         return -1 / timescale * mask(y, p) * (T - target_T)
     end
-    
-    FT = Forcing(temperature_relaxation, discrete_form = true, parameters = parameters)
+
+    FT = Forcing(temperature_relaxation; discrete_form=true, parameters=parameters)
 
     # closure (moderately elevating scalar visc/diff)
 
@@ -167,37 +159,41 @@ function build_model(grid, Δt₀, parameters)
     κz = 5e-5 # [m²/s] vertical diffusivity
     νz = 3e-3 # [m²/s] vertical viscocity
 
-    κz_field = Field{Center, Center, Center}(grid)
+    κz_field = Field{Center,Center,Center}(grid)
     κz_array = zeros(Nx, Ny, Nz)
 
     κz_add = 5e-5  # m² / s at surface
     decay_scale = 5   # layers
     for k in 1:Nz
-        taper = exp(- (k-1) / decay_scale)
-        κz_array[:,:,k] .= κz + κz_add * taper
+        taper = exp(-(k - 1) / decay_scale)
+        κz_array[:, :, k] .= κz + κz_add * taper
     end
-    @show κz_array[1:2,20,:]
+    @show κz_array[1:2, 20, :]
 
     set!(κz_field, κz_array)
 
-    horizontal_closure = HorizontalScalarDiffusivity(ν = νh, κ = κh)
-    vertical_closure = VerticalScalarDiffusivity(ν = νz, κ = κz_field)
+    horizontal_closure = HorizontalScalarDiffusivity(; ν=νh, κ=κh)
+    vertical_closure = VerticalScalarDiffusivity(; ν=νz, κ=κz_field)
 
-    biharmonic_closure = ScalarBiharmonicDiffusivity(HorizontalFormulation(), Oceananigans.defaults.FloatType;
-                                                     ν = 1e11)
+    biharmonic_closure = ScalarBiharmonicDiffusivity(
+        HorizontalFormulation(), Oceananigans.defaults.FloatType; ν=1e11
+    )
 
     @info "Building a model..."
 
-    @allowscalar model = HydrostaticFreeSurfaceModel(grid = grid,
-        free_surface = SplitExplicitFreeSurface(substeps=10),
-        momentum_advection = WENO(order=3),
-        tracer_advection = WENO(order=3),
-        buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(Oceananigans.defaults.FloatType)),
-        coriolis = coriolis,
-        closure = (horizontal_closure, vertical_closure, biharmonic_closure),
-        tracers = (:T, :S, :e),
-        boundary_conditions = (T = T_bcs, u = u_bcs, v = v_bcs),
-        forcing = (T = FT,)
+    @allowscalar model = HydrostaticFreeSurfaceModel(;
+        grid=grid,
+        free_surface=SplitExplicitFreeSurface(; substeps=10),
+        momentum_advection=WENO(; order=3),
+        tracer_advection=WENO(; order=3),
+        buoyancy=SeawaterBuoyancy(;
+            equation_of_state=LinearEquationOfState(Oceananigans.defaults.FloatType)
+        ),
+        coriolis=coriolis,
+        closure=(horizontal_closure, vertical_closure, biharmonic_closure),
+        tracers=(:T, :S, :e),
+        boundary_conditions=(T=T_bcs, u=u_bcs, v=v_bcs),
+        forcing=(T=FT,),
     )
 
     model.clock.last_Δt = Δt₀
@@ -211,8 +207,9 @@ end
 
 # Temperature flux:
 function T_flux_init(grid, p)
-    @inline temp_flux_function(x, y) = ifelse(y < p.y_shutoff, p.Qᵀ * cos(3π * y / p.Ly), 0.0)
-    temp_flux = Field{Center, Center, Nothing}(grid)
+    @inline temp_flux_function(x, y) =
+        ifelse(y < p.y_shutoff, p.Qᵀ * cos(3π * y / p.Ly), 0.0)
+    temp_flux = Field{Center,Center,Nothing}(grid)
     @allowscalar set!(temp_flux, temp_flux_function)
     return temp_flux
 end
@@ -220,13 +217,13 @@ end
 # wind stress:
 function u_wind_stress_init(grid, p)
     @inline u_stress(x, y) = -p.τ * sin(π * y / p.Ly)
-    wind_stress = Field{Face, Center, Nothing}(grid)
+    wind_stress = Field{Face,Center,Nothing}(grid)
     @allowscalar set!(wind_stress, u_stress)
     return wind_stress
 end
 
 function v_wind_stress_init(grid, p)
-    wind_stress = Field{Center, Face, Nothing}(grid)
+    wind_stress = Field{Center,Face,Nothing}(grid)
     @allowscalar set!(wind_stress, 0)
     return wind_stress
 end
@@ -235,9 +232,12 @@ end
 function temperature_salinity_init(grid, parameters)
     # Adding some noise to temperature field:
     ε(σ) = σ * randn()
-    Tᵢ_function(x, y, z) = parameters.ΔT * (exp(z / parameters.h) - exp(-Lz / parameters.h)) / (1 - exp(-Lz / parameters.h)) + ε(1e-8)
-    Tᵢ = Field{Center, Center, Center}(grid)
-    Sᵢ = Field{Center, Center, Center}(grid)
+    function Tᵢ_function(x, y, z)
+        return parameters.ΔT * (exp(z / parameters.h) - exp(-Lz / parameters.h)) /
+               (1 - exp(-Lz / parameters.h)) + ε(1e-8)
+    end
+    Tᵢ = Field{Center,Center,Center}(grid)
+    Sᵢ = Field{Center,Center,Center}(grid)
     @allowscalar set!(Tᵢ, Tᵢ_function)
     @allowscalar set!(Sᵢ, 35) # Initial Salinity
     return Tᵢ, Sᵢ
@@ -249,13 +249,15 @@ end
 
 function spinup_loop!(model)
     Δt = model.clock.last_Δt
-    @trace mincut = true track_numbers = false for i = 1:100
+    @trace mincut = true track_numbers = false for i in 1:100
         time_step!(model, Δt)
     end
     return nothing
 end
 
-function spinup_reentrant_channel_model!(model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, temp_flux)
+function spinup_reentrant_channel_model!(
+    model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, temp_flux
+)
     # setting IC's and BC's:
     set!(model.velocities.u.boundary_conditions.top.condition, u_wind_stress)
     set!(model.velocities.v.boundary_conditions.top.condition, v_wind_stress)
@@ -279,13 +281,15 @@ end
 
 function loop!(model)
     Δt = model.clock.last_Δt
-    @trace mincut = true checkpointing = true track_numbers = false for i = 1:100
+    @trace mincut = true checkpointing = true track_numbers = false for i in 1:100
         time_step!(model, Δt)
     end
     return nothing
 end
 
-function run_reentrant_channel_model!(model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, temp_flux)
+function run_reentrant_channel_model!(
+    model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, temp_flux
+)
     # setting IC's and BC's:
     set!(model.velocities.u.boundary_conditions.top.condition, u_wind_stress)
     set!(model.velocities.v.boundary_conditions.top.condition, v_wind_stress)
@@ -303,30 +307,64 @@ function run_reentrant_channel_model!(model, Tᵢ, Sᵢ, u_wind_stress, v_wind_s
     return nothing
 end
 
-function estimate_tracer_error(model, initial_temperature, initial_salinity, u_wind_stress, v_wind_stress, temp_flux, Δz, mld)
-    run_reentrant_channel_model!(model, initial_temperature, initial_salinity, u_wind_stress, v_wind_stress, temp_flux)
-    
+function estimate_tracer_error(
+    model,
+    initial_temperature,
+    initial_salinity,
+    u_wind_stress,
+    v_wind_stress,
+    temp_flux,
+    Δz,
+    mld,
+)
+    run_reentrant_channel_model!(
+        model,
+        initial_temperature,
+        initial_salinity,
+        u_wind_stress,
+        v_wind_stress,
+        temp_flux,
+    )
+
     Nx, Ny, Nz = size(model.grid)
 
     # Compute the zonal transport:
-    zonal_transport = (model.velocities.u[x_midpoint,1:Ny,1:Nz] .* model.grid.Δyᵃᶜᵃ) .* Δz
+    zonal_transport = (model.velocities.u[x_midpoint, 1:Ny, 1:Nz] .* model.grid.Δyᵃᶜᵃ) .* Δz
 
     return sum(zonal_transport) / 1e6 # Put it in Sverdrups
 end
 
-function differentiate_tracer_error(model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, temp_flux, Δz, mld,
-                                   dmodel, dTᵢ, dSᵢ, du_wind_stress, dv_wind_stress, dtemp_flux, dΔz, dmld)
-
-    dedν = autodiff(set_strong_zero(Enzyme.ReverseWithPrimal),
-                    estimate_tracer_error, Active,
-                    Duplicated(model, dmodel),
-                    Duplicated(Tᵢ, dTᵢ),
-                    Duplicated(Sᵢ, dSᵢ),
-                    Duplicated(u_wind_stress, du_wind_stress),
-                    Duplicated(v_wind_stress, dv_wind_stress),
-                    Duplicated(temp_flux, dtemp_flux),
-                    Duplicated(Δz, dΔz),
-                    Duplicated(mld, dmld))
+function differentiate_tracer_error(
+    model,
+    Tᵢ,
+    Sᵢ,
+    u_wind_stress,
+    v_wind_stress,
+    temp_flux,
+    Δz,
+    mld,
+    dmodel,
+    dTᵢ,
+    dSᵢ,
+    du_wind_stress,
+    dv_wind_stress,
+    dtemp_flux,
+    dΔz,
+    dmld,
+)
+    dedν = autodiff(
+        set_strong_zero(Enzyme.ReverseWithPrimal),
+        estimate_tracer_error,
+        Active,
+        Duplicated(model, dmodel),
+        Duplicated(Tᵢ, dTᵢ),
+        Duplicated(Sᵢ, dSᵢ),
+        Duplicated(u_wind_stress, du_wind_stress),
+        Duplicated(v_wind_stress, dv_wind_stress),
+        Duplicated(temp_flux, dtemp_flux),
+        Duplicated(Δz, dΔz),
+        Duplicated(mld, dmld),
+    )
 
     return dedν
 end
@@ -339,37 +377,55 @@ end
 architecture = ReactantState()
 
 # Timestep size:
-Δt₀ = 2.5minutes 
+Δt₀ = 2.5minutes
 
 # Make the grid:
-grid          = make_grid(architecture, Nx, Ny, Nz, z_faces)
-model         = build_model(grid, Δt₀, parameters)
-T_flux        = T_flux_init(model.grid, parameters)
+grid = make_grid(architecture, Nx, Ny, Nz, z_faces)
+model = build_model(grid, Δt₀, parameters)
+T_flux = T_flux_init(model.grid, parameters)
 u_wind_stress = u_wind_stress_init(model.grid, parameters)
 v_wind_stress = v_wind_stress_init(model.grid, parameters)
-Tᵢ, Sᵢ        = temperature_salinity_init(model.grid, parameters)
-mld           = Field{Center, Center, Nothing}(model.grid) # Not used for now
-Δz            = Reactant.ConcreteRArray(Δz)
+Tᵢ, Sᵢ = temperature_salinity_init(model.grid, parameters)
+mld = Field{Center,Center,Nothing}(model.grid) # Not used for now
+Δz = Reactant.ConcreteRArray(Δz)
 
 @info "Built $model."
 
-dmodel         = Enzyme.make_zero(model)
-dTᵢ            = Field{Center, Center, Center}(model.grid)
-dSᵢ            = Field{Center, Center, Center}(model.grid)
-du_wind_stress = Field{Face, Center, Nothing}(model.grid)
-dv_wind_stress = Field{Center, Face, Nothing}(model.grid)
-dT_flux        = Field{Center, Center, Nothing}(model.grid)
-dmld           = Field{Center, Center, Nothing}(model.grid)
-dΔz            = Enzyme.make_zero(Δz)
+dmodel = Enzyme.make_zero(model)
+dTᵢ = Field{Center,Center,Center}(model.grid)
+dSᵢ = Field{Center,Center,Center}(model.grid)
+du_wind_stress = Field{Face,Center,Nothing}(model.grid)
+dv_wind_stress = Field{Center,Face,Nothing}(model.grid)
+dT_flux = Field{Center,Center,Nothing}(model.grid)
+dmld = Field{Center,Center,Nothing}(model.grid)
+dΔz = Enzyme.make_zero(Δz)
 
 # Trying zonal transport:
 
 @info "Compiling the model run..."
 tic = time()
-rspinup_reentrant_channel_model! = @compile raise_first=true raise=true sync=true  spinup_reentrant_channel_model!(model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, T_flux)
+rspinup_reentrant_channel_model! = @compile raise_first = true raise = true sync = true spinup_reentrant_channel_model!(
+    model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, T_flux
+)
 #restimate_tracer_error = @compile raise_first=true raise=true sync=true estimate_tracer_error(model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, T_flux, Δz, mld)
-rdifferentiate_tracer_error = @compile raise_first=true raise=true sync=true  differentiate_tracer_error(model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, T_flux, Δz, mld,
-                                                                                                        dmodel, dTᵢ, dSᵢ, du_wind_stress, dv_wind_stress, dT_flux, dΔz, dmld)
+rdifferentiate_tracer_error = @compile raise_first = true raise = true sync = true differentiate_tracer_error(
+    model,
+    Tᵢ,
+    Sᵢ,
+    u_wind_stress,
+    v_wind_stress,
+    T_flux,
+    Δz,
+    mld,
+    dmodel,
+    dTᵢ,
+    dSᵢ,
+    du_wind_stress,
+    dv_wind_stress,
+    dT_flux,
+    dΔz,
+    dmld,
+)
 compile_toc = time() - tic
 
 @show compile_toc
@@ -415,7 +471,24 @@ jldsave(filename; Nx, Ny, Nz,
 
 tic = time()
 #output = restimate_tracer_error(model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, T_flux, Δz, mld)
-dedν = rdifferentiate_tracer_error(model, Tᵢ, Sᵢ, u_wind_stress, v_wind_stress, T_flux, Δz, mld, dmodel, dTᵢ, dSᵢ, du_wind_stress, dv_wind_stress, dT_flux, dΔz, dmld)
+dedν = rdifferentiate_tracer_error(
+    model,
+    Tᵢ,
+    Sᵢ,
+    u_wind_stress,
+    v_wind_stress,
+    T_flux,
+    Δz,
+    mld,
+    dmodel,
+    dTᵢ,
+    dSᵢ,
+    du_wind_stress,
+    dv_wind_stress,
+    dT_flux,
+    dΔz,
+    dmld,
+)
 run_toc = time() - tic
 
 @show run_toc
