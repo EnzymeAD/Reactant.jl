b46838eab934423184a5c92799c3f2eff3a7a21a
Sergio Sanchez Ramirez <15837247+mofeing@users.noreply.github.com>
2026-02-09 13:01:48 +0100

    fix `export_as_saved_model`

--- src/serialization/TFSavedModel.jl
+++ src/serialization/TFSavedModel.jl
@@ -44,56 +44,62 @@ function export_as_saved_model(
                PythonCall hasn't been installed and loaded.")
     end
 
-    mlir_mod = MLIR.IR.with_context() do ctx
-        parse(MLIR.IR.Module, thunk.module_string)
+    MLIR.IR.@dispose ctx = Reactant.ReactantContext() begin
+        MLIR.IR.activate(ctx)
+        try
+            mlir_mod = parse(MLIR.IR.Module, thunk.module_string)
+            ftype = MLIR.IR.FunctionType(first(MLIR.IR.body(mlir_mod)))
+
+            input_signature = [
+                VariableSignature(
+                    collect(Int64, size(MLIR.IR.input(ftype, i))),
+                    NUMPY_SIMPLE_TYPES[MLIR.IR.julia_type(eltype(MLIR.IR.input(ftype, i)))],
+                ) for i in 1:MLIR.IR.ninputs(ftype)
+            ]
+        
+            output_signature = [
+                VariableSignature(
+                    collect(Int64, size(MLIR.IR.result(ftype, i))),
+                    NUMPY_SIMPLE_TYPES[MLIR.IR.julia_type(eltype(MLIR.IR.result(ftype, i)))],
+                ) for i in 1:MLIR.IR.nresults(ftype)
+            ]
+        
+            if isempty(input_locations)
+                input_locations = [InputArgument(i) for i in 1:length(input_signature)]
+            end
+        
+            @assert length(input_locations) == length(input_signature) "The number of input \
+                                                                        locations must match the \
+                                                                        number of input signatures."
+        
+            c_print_callback = @cfunction(
+                MLIR.IR.print_callback, Cvoid, (MLIR.API.MlirStringRef, Any)
+            )
+            ref = Ref(IOBuffer())
+            result = MLIR.IR.LogicalResult(
+                MLIR.API.stablehloSerializePortableArtifactFromModule(
+                    mlir_mod, string(target_version), c_print_callback, ref, true
+                ),
+            )
+            MLIR.IR.isfailure(result) && throw("Couldn't serialize the module")
+            serialized_module = codeunits(String(take!(ref[])))
+        
+            return to_tf_saved_model(
+                ReactantFunctionSpec(
+                    input_signature,
+                    output_signature,
+                    input_locations,
+                    serialized_module,
+                    Dict(k => Array(v) for (k, v) in state_dict),
+                ),
+                saved_model_path,
+            )
+        finally
+            MLIR.IR.deactivate(ctx)
+        end
     end
 
-    ftype = MLIR.IR.FunctionType(first(MLIR.IR.body(mlir_mod)))
 
-    input_signature = [
-        VariableSignature(
-            collect(Int64, size(MLIR.IR.input(ftype, i))),
-            NUMPY_SIMPLE_TYPES[MLIR.IR.julia_type(eltype(MLIR.IR.input(ftype, i)))],
-        ) for i in 1:MLIR.IR.ninputs(ftype)
-    ]
-
-    output_signature = [
-        VariableSignature(
-            collect(Int64, size(MLIR.IR.result(ftype, i))),
-            NUMPY_SIMPLE_TYPES[MLIR.IR.julia_type(eltype(MLIR.IR.result(ftype, i)))],
-        ) for i in 1:MLIR.IR.nresults(ftype)
-    ]
-
-    if isempty(input_locations)
-        input_locations = [InputArgument(i) for i in 1:length(input_signature)]
-    end
-
-    @assert length(input_locations) == length(input_signature) "The number of input \
-                                                                locations must match the \
-                                                                number of input signatures."
-
-    c_print_callback = @cfunction(
-        MLIR.IR.print_callback, Cvoid, (MLIR.API.MlirStringRef, Any)
-    )
-    ref = Ref(IOBuffer())
-    result = MLIR.IR.LogicalResult(
-        MLIR.API.stablehloSerializePortableArtifactFromModule(
-            mlir_mod, string(target_version), c_print_callback, ref, true
-        ),
-    )
-    MLIR.IR.isfailure(result) && throw("Couldn't serialize the module")
-    serialized_module = codeunits(String(take!(ref[])))
-
-    return to_tf_saved_model(
-        ReactantFunctionSpec(
-            input_signature,
-            output_signature,
-            input_locations,
-            serialized_module,
-            Dict(k => Array(v) for (k, v) in state_dict),
-        ),
-        saved_model_path,
-    )
 end
 
 function to_tf_saved_model(fn_spec::ReactantFunctionSpec, path::String)
