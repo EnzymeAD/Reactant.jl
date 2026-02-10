c2bfc4db5ea5775d954665792de7051d968ec7d4
Avik Pal <avikpal@mit.edu>
2026-02-10 10:23:33 -0500

    fix: streamline

--- benchmark/misc/runbenchmarks.jl
+++ benchmark/misc/runbenchmarks.jl
@@ -1,33 +1,9 @@
 # Miscellaneous Benchmarks Runner
 # This script runs all misc benchmarks and stores results to a JSON file
-using InteractiveUtils: versioninfo
-using Reactant: Reactant
-using JSON3: JSON3
-using Printf: @sprintf
+
+include("../utils.jl")
 
 @info sprint(io -> versioninfo(io; verbose=true))
-
-# Get backend from environment or auto-detect
-function get_backend()
-    BENCHMARK_GROUP = get(ENV, "BENCHMARK_GROUP", nothing)
-
-    if BENCHMARK_GROUP == "CUDA"
-        Reactant.set_default_backend("gpu")
-        @info "Running CUDA benchmarks" maxlog = 1
-    elseif BENCHMARK_GROUP == "TPU"
-        Reactant.set_default_backend("tpu")
-        @info "Running TPU benchmarks" maxlog = 1
-    elseif BENCHMARK_GROUP == "CPU"
-        Reactant.set_default_backend("cpu")
-        @info "Running CPU benchmarks" maxlog = 1
-    else
-        BENCHMARK_GROUP = String(split(string(first(Reactant.devices())), ":")[1])
-        @info "Running $(BENCHMARK_GROUP) benchmarks" maxlog = 1
-    end
-
-    @assert BENCHMARK_GROUP in ("CPU", "CUDA", "TPU")
-    return BENCHMARK_GROUP
-end
 
 backend = get_backend()
 
@@ -35,6 +11,7 @@ backend = get_backend()
 using Reactant, LinearAlgebra, Enzyme
 using Chairmarks: @b
 using Random: Random
+using Printf: @sprintf
 
 # Include benchmark modules
 include("common.jl")
@@ -53,20 +30,4 @@ end
 
 results = run_all_benchmarks(backend)
 
-# Save results
-results_dir = joinpath(@__DIR__, "results")
-mkpath(results_dir)
-
-filename = string("misc_", backend, "_benchmarks.json")
-filepath = joinpath(results_dir, filename)
-
-standardized_results = Vector{Dict{String,Union{String,Float64}}}(undef, length(results))
-for (i, (k, v)) in enumerate(results)
-    standardized_results[i] = Dict("name" => k, "value" => v, "unit" => "s")
-end
-
-open(filepath, "w") do io
-    JSON3.pretty(io, JSON3.write(standardized_results))
-end
-
-@info "Saved $(length(results)) benchmark results to $(filepath)"
+save_results(results, joinpath(@__DIR__, "results"), "misc", backend)
