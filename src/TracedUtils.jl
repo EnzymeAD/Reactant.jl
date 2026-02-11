6b7a1da12263a5d5b47fb21fbb168679035bbc17
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2026-02-11 04:16:31 -0600

    try fix `__lookup_unique_name_in_module`

--- src/TracedUtils.jl
+++ src/TracedUtils.jl
@@ -1030,13 +1030,14 @@ end
 
 function __lookup_unique_name_in_module(mod, name)
     new_name = name
-    tab = MLIR.IR.SymbolTable(MLIR.IR.Operation(mod))
-    for i in 0:10000
-        new_name = i == 0 ? name : name * "_" * string(i)
-        MLIR.IR.mlirIsNull(MLIR.API.mlirSymbolTableLookup(tab, new_name)) && return new_name
+    MLIR.IR.@dispose tab = MLIR.IR.SymbolTable(mod) begin
+        for i in 0:10000
+            new_name = i == 0 ? name : name * "_" * string(i)
+            MLIR.IR.mlirIsNull(MLIR.API.mlirSymbolTableLookup(tab, new_name)) && return new_name
+        end
+        modstr = string(mod)
+        return error("Mod\n$modstr\nCould not find unique name for $name")
     end
-    modstr = string(mod)
-    return error("Mod\n$modstr\nCould not find unique name for $name")
 end
 
 function __take_region(compiled_fn)
