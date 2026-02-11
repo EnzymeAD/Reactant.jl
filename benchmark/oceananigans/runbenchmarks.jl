bd1f0abf4b4a5400921fc77b41f2061d94c44cd5
Avik Pal <avikpal@mit.edu>
2026-02-11 06:24:14 -0500

    feat: tflops

--- benchmark/oceananigans/runbenchmarks.jl
+++ benchmark/oceananigans/runbenchmarks.jl
@@ -1,17 +1,14 @@
 # Oceananigans Benchmarks Runner
 # This script runs all oceananigans benchmarks and stores results to a JSON file
+using Reactant, LinearAlgebra, Enzyme
+using Random: Random
+using Printf: @sprintf
 
 include("../utils.jl")
 
 @info sprint(io -> versioninfo(io; verbose=true))
 
 backend = get_backend()
-
-# Load dependencies used in benchmarks
-using Reactant, LinearAlgebra, Enzyme
-using Chairmarks: @b
-using Random: Random
-using Printf: @sprintf
 
 # Include benchmark modules
 module AbernatheyChannel
@@ -22,7 +19,7 @@ end
 
 # Run all benchmarks
 function run_all_benchmarks(backend::String)
-    results = Dict{String,Float64}()
+    results = Dict{String,Dict{String,Float64}}()
 
     AbernatheyChannel.run_abernathey_channel_benchmark!(results, backend)
 
