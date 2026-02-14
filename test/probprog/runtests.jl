b922e662adda91ee905d2923feef1d504ce03982
sbrantq <scharfrichterq@gmail.com>
2026-02-14 04:03:18 -0600

    fix

--- test/probprog/runtests.jl
+++ test/probprog/runtests.jl
@@ -1,7 +1,11 @@
-using Reactant, ParallelTestRunner, Test
+using Reactant, ParallelTestRunner, CondaPkg, Test
 
 const BACKEND = lowercase(get(ENV, "REACTANT_BACKEND_GROUP", "auto"))
 BACKEND != "auto" && Reactant.set_default_backend(BACKEND)
+
+CondaPkg.add_pip("jax"; version="==0.9.0")
+CondaPkg.add_pip("numpyro"; version="==0.19.0")
+CondaPkg.resolve()
 
 testsuite = find_tests(@__DIR__)
 delete!(testsuite, "common")
