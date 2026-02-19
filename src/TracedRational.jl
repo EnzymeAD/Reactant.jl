4816aec82596a4e2a01afcda0293ae192951bc35
Avik Pal <avikpal@mit.edu>
2026-02-18 21:01:32 -0500

    feat: more operation coverage

--- src/TracedRational.jl
+++ src/TracedRational.jl
@@ -78,90 +78,40 @@ Base.:+(x::TracedRational) = TracedRational(+x.num, x.den)
 Base.:-(x::TracedRational) = TracedRational(-x.num, x.den)
 # TODO: check for overflow/underflow for integer types
 
-# function Base.:+(x::TracedRational, y::TracedRational)
-#     num = @opcall +(x.num * y.den, y.num * x.den)
-#     den = @opcall *(x.den, y.den)
-#     return TracedRational(num, den)
-# end
+for op in (:+, :-, :rem, :mod)
+    @eval begin
+        function Base.$(op)(x::TracedRational, y::TracedRational)
+            xd, yd = Base.divgcd(promote(x.den, y.den)...)
+            return TracedRational($(op)(x.num * yd, y.num * xd), x.den * yd)
+        end
 
-# function Base.:-(x::TracedRational, y::TracedRational)
-#     num = @opcall -(x.num * y.den, y.num * x.den)
-#     den = @opcall *(x.den, y.den)
-#     return TracedRational(num, den)
-# end
+        function Base.$(op)(x::TracedRational, y::Union{TracedRNumber{<:Integer},Integer})
+            return TracedRational($(op)(x.num, x.den * y), x.den)
+        end
 
-# function Base.:*(x::TracedRational, y::TracedRational)
-#     num = @opcall *(x.num, y.num)
-#     den = @opcall *(x.den, y.den)
-#     return TracedRational(num, den)
-# end
+        function Base.$(op)(x::Union{TracedRNumber{<:Integer},Integer}, y::TracedRational)
+            return TracedRational($(op)(x * y.den, y.num), y.den)
+        end
+    end
+end
 
-# function Base.:/(x::TracedRational, y::TracedRational)
-#     num = @opcall *(x.num, y.den)
-#     den = @opcall *(x.den, y.num)
-#     return TracedRational(num, den)
-# end
+function Base.:*(x::TracedRational, y::TracedRational)
+    xn, yd = Base.divgcd(promote(x.num, y.den)...)
+    yn, xd = Base.divgcd(promote(y.num, x.den)...)
+    return TracedRational(xn * yn, xd * yd)
+end
 
-# function Base.:-(x::TracedRational)
-#     return TracedRational(@opcall -(x.num), x.den)
-# end
+function Base.:*(x::TracedRational, y::Union{TracedRNumber{<:Integer},Integer})
+    xd, yn = Base.divgcd(promote(x.den, y)...)
+    return TracedRational(x.num * yn, xd)
+end
 
-# function Base.:+(x::TracedRational, y::Integer)
-#     return TracedRational(@opcall +(x.num, x.den * y), x.den)
-# end
-# Base.:+(y::Integer, x::TracedRational) = x + y
-
-# function Base.:+(x::TracedRational, y::TracedRNumber)
-#     return TracedRational(@opcall +(x.num, x.den * y), x.den)
-# end
-# Base.:+(y::TracedRNumber, x::TracedRational) = x + y
-
-# function Base.:-(x::TracedRational, y::Integer)
-#     return TracedRational(@opcall -(x.num, x.den * y), x.den)
-# end
-# function Base.:-(y::Integer, x::TracedRational)
-#     return TracedRational(@opcall -(x.den * y, x.num), x.den)
-# end
-
-# function Base.:-(x::TracedRational, y::TracedRNumber)
-#     return TracedRational(@opcall -(x.num, x.den * y), x.den)
-# end
-# function Base.:-(y::TracedRNumber, x::TracedRational)
-#     return TracedRational(@opcall -(x.den * y, x.num), x.den)
-# end
-
-# function Base.:*(x::TracedRational, y::Integer)
-#     return TracedRational(@opcall *(x.num, y), x.den)
-# end
-# Base.:*(y::Integer, x::TracedRational) = x * y
-
-# function Base.:*(x::TracedRational, y::TracedRNumber)
-#     return TracedRational(@opcall *(x.num, y), x.den)
-# end
-# Base.:*(y::TracedRNumber, x::TracedRational) = x * y
-
-# function Base.:/(x::TracedRational, y::Integer)
-#     return TracedRational(x.num, @opcall *(x.den, y))
-# end
-# function Base.:/(x::Integer, y::TracedRational)
-#     return TracedRational(@opcall *(x, y.den), y.num)
-# end
-
-# function Base.:/(x::TracedRational, y::TracedRNumber)
-#     return TracedRational(x.num, @opcall *(x.den, y))
-# end
-# function Base.:/(x::TracedRNumber, y::TracedRational)
-#     return TracedRational(@opcall *(x, y.den), y.num)
-# end
+function Base.:*(x::Union{TracedRNumber{<:Integer},Integer}, y::TracedRational)
+    xn, yd = Base.divgcd(promote(x, y.den)...)
+    return TracedRational(xn * y.num, yd)
+end
 
 Base.inv(x::TracedRational) = TracedRational(x.den, x.num)
-
-# # Comparison operations
-# __compare(op::OP, x::TracedRational, y::TracedRational) = op(x.num * y.den, y.num * x.den)
-
-# Base.:(==)(x::TracedRational, y::TracedRational) = __compare(==, x, y)
-# Base.:<(x::TracedRational, y::TracedRational) = __compare(<, x, y)
-# Base.:<=(x::TracedRational, y::TracedRational) = __compare(<=, x, y)
 
 # Conversion operations
 Base.float(x::TracedRational) = float(x.num) / float(x.den)
