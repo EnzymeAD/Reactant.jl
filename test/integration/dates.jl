0404c11c35d1b94c7fe78a3071e2656a37108d27
Maximilian Gelbrecht <maximilian.gelbrecht@posteo.de>
2026-02-25 12:55:15 +0100

    Update test/integration/dates.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/integration/dates.jl
+++ test/integration/dates.jl
@@ -354,7 +354,8 @@ using Reactant
         @test @jit((a + Dates.Day(1)) + Dates.Month(1)) !=
             @jit((a + Dates.Month(1)) + Dates.Day(1))
         a = Dates.Date(2012, 1, 30)
-        @test @jit((a + Dates.Day(1)) + Dates.Month(1)) != @jit((a + Dates.Month(1)) + Dates.Day(1))
+        @test @jit((a + Dates.Day(1)) + Dates.Month(1)) !=
+            @jit((a + Dates.Month(1)) + Dates.Day(1))
         a = Dates.Date(2012, 2, 29)
         @test @jit((a + Dates.Day(1)) + Dates.Month(1)) != @jit((a + Dates.Month(1)) + Dates.Day(1))
     end
