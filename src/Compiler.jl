c0765f9c1c528123fe0ce0014f2fd81569672db9
snonk <scq@mit.edu>
2026-02-25 11:37:46 -0600

    add tests

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -960,8 +960,7 @@ function optimization_passes(
         if raise_shlo_to_blas_lapack
             if !compile_options.disable_structured_tensors_detection_passes
                 append!(transform_passes_list, [
-                    # TODO: enable once we have the lowering implemented
-                    # "dot_general_to_symm",
+                    "dot_general_to_symm",
                     "dot_general_to_syrk",
                 ])
             end
