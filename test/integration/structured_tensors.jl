12261b764902a37d9d88a5f6af19d588a034c3bb
snonk <scq@mit.edu>
2026-03-01 13:43:39 -0600

    update symm tests

--- test/integration/structured_tensors.jl
+++ test/integration/structured_tensors.jl
@@ -46,51 +46,31 @@ function raise_to_symm2(x, y)
     return 3 .* y * z
 end
 
-elty = Float32
+@testset "symm optimizations" begin
+    @testset for elty in (Float32, Float64, ComplexF32, ComplexF64)
+        RunningOnTPU && elty == ComplexF64 && continue
 
-x = Reactant.TestUtils.construct_test_array(elty, 4, 5)
-y = Reactant.TestUtils.construct_test_array(elty, 4, 7)
+        x = Reactant.TestUtils.construct_test_array(elty, 4, 5)
+        y1 = Reactant.TestUtils.construct_test_array(elty, 4, 7)
+        y2 = Reactant.TestUtils.construct_test_array(elty, 7, 4)
+        
+        x_ra = Reactant.to_rarray(x)
+        
+        @testset "fn: $(fn) | y: $(size(y))" for (fn, y) in
+                                                 ((raise_to_symm3, y1), (raise_to_symm4, y2))
+            y_ra = Reactant.to_rarray(y)
 
-x_ra = Reactant.to_rarray(x)
-y_ra = Reactant.to_rarray(y)
+            hlo = @code_hlo compile_options = CompileOptions(;
+                disable_structured_tensors_detection_passes=false,
+                optimization_passes=:before_jit,
+            ) fn(x_ra, y_ra)
+            @test occursin("enzymexla.blas.symm", repr(hlo))
 
-@code_hlo raise_to_symm3(x_ra, y_ra)
-@code_hlo compile_options = CompileOptions(;
-    disable_structured_tensors_detection_passes=false, optimization_passes=:before_jit
-) raise_to_symm3(x_ra, y_ra)
-@code_hlo compile_options = CompileOptions(;
-    disable_structured_tensors_detection_passes=false
-) raise_to_symm3(x_ra, y_ra)
+            fn_compile = @compile compile_options = CompileOptions(;
+                disable_structured_tensors_detection_passes=false
+            ) fn(x_ra, y_ra)
 
-@jit raise_to_symm3(x_ra, y_ra)
-@jit compile_options = CompileOptions(; disable_structured_tensors_detection_passes=false) raise_to_symm3(
-    x_ra, y_ra
-)
-
-# @testset "syrk optimizations" begin
-#     @testset for elty in (Float32, Float64, ComplexF32, ComplexF64)
-#         RunningOnTPU && elty == ComplexF64 && continue
-
-#         x = Reactant.TestUtils.construct_test_array(elty, 4, 5)
-#         y1 = Reactant.TestUtils.construct_test_array(elty, 4, 4)
-#         y2 = Reactant.TestUtils.construct_test_array(elty, 5, 5)
-#         x_ra = Reactant.to_rarray(x)
-
-#         @testset "fn: $(fn) | y: $(size(y))" for (fn, y) in
-#                                                  ((raise_to_syrk, y1), (raise_to_syrk2, y2))
-#             y_ra = Reactant.to_rarray(y)
-
-#             hlo = @code_hlo compile_options = CompileOptions(;
-#                 disable_structured_tensors_detection_passes=false,
-#                 optimization_passes=:before_jit,
-#             ) fn(x_ra, y_ra)
-#             @test occursin("enzymexla.blas.syrk", repr(hlo))
-
-#             fn_compile = @compile compile_options = CompileOptions(;
-#                 disable_structured_tensors_detection_passes=false
-#             ) fn(x_ra, y_ra)
-
-#             @test fn_compile(x_ra, y_ra) ≈ fn(x, y) atol = 1e-3 rtol = 1e-3
-#         end
-#     end
-# end
+            @test fn_compile(x_ra, y_ra) ≈ fn(x, y) atol = 1e-3 rtol = 1e-3
+        end
+    end
+end
