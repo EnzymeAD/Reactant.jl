8e29e2ed13116aea34c2db49b4b6d91dacba5c64
Avik Pal <avikpal@mit.edu>
2026-03-01 10:51:54 -0600

    test(probprog): force 1 job on TPU

--- test/probprog/runtests.jl
+++ test/probprog/runtests.jl
@@ -1,7 +1,6 @@
 using Reactant, ParallelTestRunner, CondaPkg, Test
 
 const BACKEND = lowercase(get(ENV, "REACTANT_BACKEND_GROUP", "auto"))
-BACKEND != "auto" && Reactant.set_default_backend(BACKEND)
 
 CondaPkg.add_pip("jax"; version="==0.9.0")
 CondaPkg.add_pip("numpyro"; version="==0.19.0")
@@ -10,14 +9,27 @@ CondaPkg.resolve()
 testsuite = find_tests(@__DIR__)
 delete!(testsuite, "common")
 
+jobs = min(
+    something(
+        Reactant.Accelerators.TPU.has_tpu() || BACKEND == "tpu" ? 1 : nothing,
+        ParallelTestRunner.default_njobs(),
+    ),
+    length(keys(testsuite)),
+)
+
 @testset "ProbProg" begin
-    runtests(
-        Reactant,
-        String[];
-        testsuite,
-        init_code=quote
-            using Reactant
-            $(BACKEND) != "auto" && Reactant.set_default_backend($(BACKEND))
-        end,
-    )
+    withenv(
+        "XLA_REACTANT_GPU_MEM_FRACTION" => 1 / (jobs + 0.1),
+        "XLA_REACTANT_GPU_PREALLOCATE" => false,
+    ) do
+        runtests(
+            Reactant,
+            String["--jobs=$(jobs)"];
+            testsuite,
+            init_code=quote
+                using Reactant
+                $(BACKEND) != "auto" && Reactant.set_default_backend($(BACKEND))
+            end,
+        )
+    end
 end
