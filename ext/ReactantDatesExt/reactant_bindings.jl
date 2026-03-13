abea2b4e8a2626156b9cc52e9175ef8f62aeb545
Maximilian Gelbrecht <maximilian.gelbrecht@posteo.de>
2026-03-13 14:09:15 +0100

    Update ext/ReactantDatesExt/reactant_bindings.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/ReactantDatesExt/reactant_bindings.jl
+++ ext/ReactantDatesExt/reactant_bindings.jl
@@ -50,7 +50,9 @@ for T in (
     )
         if mode == Reactant.ArrayToConcrete
             # all Dates types are hard coded to have Int64 fields
-            NF = Reactant.traced_type_inner(Int64, seen, mode, track_numbers, ndevices, runtime)
+            NF = Reactant.traced_type_inner(
+                Int64, seen, mode, track_numbers, ndevices, runtime
+            )
             return $(reactant_type){NF}
         else
             return T
