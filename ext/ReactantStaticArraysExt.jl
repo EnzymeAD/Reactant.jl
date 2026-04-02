a0a839b284243eac163d598d2b772f73e467fab1
Paul Tiede <ptiede91@gmail.com>
2026-04-02 00:16:21 -0400

    feat: better StructArray & StaticArray support (#2546)
    * Draft to figure out better StructArray support
    
    * Simplify and generalize structarray type conversion
    
    * Start adding StaticArray support
    
    * Add StaticArray support and tweak elem_apply_while_loop to select correct container type
    
    * Revert tracing.jl
    
    * Remove info debug
    
    * Remove get_ith
    
    * Add _copyto!
    
    * format
    
    * Fix broken test and add new tests
    
    * format
    
    * add StaticArrays
    
    * Add LinearAlgebra
    
    * Remove unused function
    
    * Reuse the known destination for while loop if possible
    
    * Update ext/ReactantStructArraysExt.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Proposed improved support for SArrays
    
    * fix dumb mistake
    
    * Add additional changes for StaticArrays
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Cleanup
    
    * Update
    
    * Update
    
    * Format
    
    * Fix for code review
    
    * Add comments
    
    * So dumb
    
    * Correct comment in overloaded_mul function
    
    Fix comment typo in overloaded_mul function.
    
    * Update to remove anonymous functions
    
    * Update
    
    * Add a complex test
    
    * Update
    
    ---------
    
    Co-authored-by: Billy Moses <wmoses@google.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Avik Pal <avikpal@mit.edu>

---
+++ ext/ReactantStaticArraysExt.jl
@@ -0,0 +1,41 @@
+module ReactantStaticArraysExt
+
+using Reactant
+import Reactant.TracedRArrayOverrides: overloaded_map, overloaded_mapreduce
+import Reactant.TracedLinearAlgebra: overloaded_mul
+
+using StaticArrays: SArray, StaticArray
+
+const SAReact{Sz,T} = StaticArray{Sz,T} where {Sz<:Tuple,T<:Reactant.TracedRNumber}
+
+Base.@nospecializeinfer function Reactant.traced_type_inner(
+    @nospecialize(FA::Type{SArray{S,T,N,L}}),
+    seen,
+    mode::Reactant.TraceMode,
+    @nospecialize(track_numbers::Type),
+    @nospecialize(ndevices),
+    @nospecialize(runtime)
+) where {S,T,N,L}
+    T_traced = Reactant.traced_type_inner(T, seen, mode, track_numbers, ndevices, runtime)
+    return SArray{S,T_traced,N,L}
+end
+
+function Reactant.materialize_traced_array(x::SAReact)
+    return x
+end
+
+# We don't want to overload map on StaticArrays since it is likely better to just unroll things
+overloaded_map(f, a::SAReact, rest::SAReact...) = f.(a, rest...)
+overloaded_mapreduce(f, op, a::SAReact; kwargs...) = mapreduce(f, op, a, kwargs...)
+
+function overloaded_mul(A::SAReact, B::SAReact, alpha::Number=true, beta::Number=false)
+    # beta is not supported since it is zero by default in Reactant 
+    # (similar is zero'd automatically for TracedRArrays)
+    C = A * B
+    if !(alpha isa Reactant.TracedRNumber) && isone(alpha)
+        return C
+    end
+    return C .* alpha
+end
+
+end
--- ext/ReactantStructArraysExt.jl
+++ ext/ReactantStructArraysExt.jl
@@ -40,8 +40,8 @@ function Base.copy(
 end
 
 function Reactant.broadcast_to_size(arg::StructArray{T}, rsize) where {T}
-    new = [broadcast_to_size(c, rsize) for c in components(arg)]
-    return StructArray{T}(NamedTuple(Base.propertynames(arg) .=> new))
+    new = Tuple((broadcast_to_size(c, rsize) for c in components(arg)))
+    return StructArray{T}(new)
 end
 
 function Base.copyto!(
@@ -53,10 +53,47 @@ function Base.copyto!(
     bc = Broadcast.preprocess(dest, bc)
 
     args = (Reactant.broadcast_to_size(Base.materialize(a), size(bc)) for a in bc.args)
-
     res = Reactant.TracedUtils.elem_apply_via_while_loop(bc.f, args...)
+    copyto!(dest, res)
 
+    return dest
+end
+
+function Reactant.TracedRArrayOverrides._copyto!(
+    dest::StructArray, bc::Base.Broadcast.Broadcasted{<:AbstractReactantArrayStyle}
+)
+    return copyto!(dest, bc)
+end
+
+function Base.copyto!(
+    dest::Reactant.TracedRArray, bc::Broadcasted{StructArrayStyle{S,N}}
+) where {S<:AbstractReactantArrayStyle,N}
+    axes(dest) == axes(bc) || Broadcast.throwdm(axes(dest), axes(bc))
+    isempty(dest) && return dest
+
+    bc = Broadcast.preprocess(dest, bc)
+    args = (Reactant.broadcast_to_size(Base.materialize(a), size(bc)) for a in bc.args)
+    res = Reactant.TracedUtils.elem_apply_via_while_loop(bc.f, args...)
     return copyto!(dest, res)
+end
+
+function alloc_sarr(bc, T)
+    # Short circuit for Complex since in Reactant they are just a regular number
+    T <: Complex && return similar(bc, T)
+    asa = Base.Fix1(alloc_sarr, bc)
+    if StructArrays.isnonemptystructtype(T)
+        return StructArrays.buildfromschema(asa, T)
+    else
+        return similar(bc, T)
+    end
+end
+
+function Base.similar(
+    bc::Broadcasted{StructArrayStyle{S,N}}, ::Type{ElType}
+) where {S<:AbstractReactantArrayStyle,N,ElType}
+    bc′ = convert(Broadcasted{S}, bc)
+    # It is possible that we have multiple broadcasted arguments
+    return alloc_sarr(bc′, ElType)
 end
 
 Base.@propagate_inbounds function StructArrays._getindex(
@@ -67,12 +104,40 @@ Base.@propagate_inbounds function StructArrays._getindex(
     return createinstance(T, get_ith(cols, I...)...)
 end
 
+setstruct(col, val, I) = @inbounds Reactant.@allowscalar col[I] = val
+struct SetStruct{T}
+    I::T
+end
+(s::SetStruct)(col, val) = setstruct(col, val, s.I)
+(s::SetStruct)(vals) = s(vals...)
+
 Base.@propagate_inbounds function Base.setindex!(
     s::StructArray{T,<:Any,<:Any,Int}, vals, I::TracedRNumber{TI}
 ) where {T,TI<:Integer}
     valsT = maybe_convert_elt(T, vals)
-    foreachfield((col, val) -> (@inbounds col[I] = val), s, valsT)
+    setter = SetStruct(I)
+    foreachfield(setter, s, valsT)
     return s
+end
+
+const MRarr = Union{Reactant.AnyTracedRArray,Reactant.RArray}
+getstruct(col, n, I) = @inbounds Reactant.@allowscalar col[n][I...]
+struct GetStruct{C,Idx}
+    cols::C
+    I::Idx
+end
+(g::GetStruct)(n) = getstruct(g.cols, n, g.I...)
+
+function StructArrays.get_ith(cols::NamedTuple{N,<:NTuple{K,<:MRarr}}, I...) where {N,K}
+    getter = GetStruct(cols, I)
+    ith = ntuple(getter, Val(K))
+    return ith
+end
+
+function StructArrays.get_ith(cols::NTuple{K,<:MRarr}, I...) where {K}
+    getter = GetStruct(cols, I)
+    ith = ntuple(getter, Val(K))
+    return ith
 end
 
 Base.@nospecializeinfer function Reactant.traced_type_inner(
@@ -90,41 +155,15 @@ Base.@nospecializeinfer function Reactant.traced_type_inner(
     return StructArray{ET_traced,N,C_traced,index_type(fieldtypes(C_traced))}
 end
 
-function Reactant.make_tracer(
-    seen,
-    @nospecialize(prev::StructArray{NT,N}),
-    @nospecialize(path),
-    mode;
-    track_numbers=false,
-    sharding=Reactant.Sharding.Sharding.NoSharding(),
-    runtime=nothing,
-    kwargs...,
-) where {NT<:NamedTuple,N}
-    track_numbers isa Bool && (track_numbers = track_numbers ? Number : Union{})
-    components = getfield(prev, :components)
-    if mode == TracedToTypes
-        push!(path, typeof(prev))
-        for c in components
-            make_tracer(seen, c, path, mode; track_numbers, sharding, runtime, kwargs...)
-        end
-        return nothing
-    end
-    traced_components = make_tracer(
-        seen,
-        components,
-        append_path(path, 1),
-        mode;
-        track_numbers,
-        sharding,
-        runtime,
-        kwargs...,
-    )
-    T_traced = traced_type(typeof(prev), Val(mode), track_numbers, sharding, runtime)
-    return StructArray{first(T_traced.parameters)}(traced_components)
-end
-
 @inline function Reactant.traced_getfield(@nospecialize(obj::StructArray), field)
     return Base.getfield(obj, field)
 end
 
+# This is to tell StructArrays to leave these array types alone.
+StructArrays.staticschema(::Type{<:Reactant.AnyTracedRArray}) = NamedTuple{()}
+StructArrays.staticschema(::Type{<:Reactant.RArray}) = NamedTuple{()}
+StructArrays.staticschema(::Type{<:Reactant.RNumber}) = NamedTuple{()}
+# # Even though RNumbers we have fields we want them to be threated as empty structs
+StructArrays.isnonemptystructtype(::Type{<:Reactant.RNumber}) = false
+StructArrays.isnonemptystructtype(::Type{<:Reactant.TracedRArray}) = false
 end
