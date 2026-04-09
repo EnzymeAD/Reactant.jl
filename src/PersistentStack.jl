16dc503f44c4a0b09e047e4ab36e664c2b669148
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-04-09 04:13:04 -0500

    implement iteration, indexing interfaces

--- src/PersistentStack.jl
+++ src/PersistentStack.jl
@@ -1,14 +1,45 @@
 struct PersistentStack{T}
     data::T
     prev::Union{Nothing, PersistentStack{T}}
+    length::Int
 end
 
-PersistentStack(x) = PersistentStack(x, nothing)
-PersistentStack{T}(x) where {T} = PersistentStack{T}(x, nothing)
+PersistentStack(x) = PersistentStack(x, nothing, 1)
+PersistentStack{T}(x) where {T} = PersistentStack{T}(x, nothing, 1)
 
-push(stack::PersistentStack, x) = PersistentStack(x, stack)
+push(stack::PersistentStack, x) = PersistentStack(x, stack, length(stack) + 1)
 pop(stack::PersistentStack) = stack.data
 Base.front(stack::PersistentStack) = stack.prev
+
+Base.IteratorEltype(::Type{<:PersistentStack{T}}) where {T} = Base.HasEltype()
+Base.eltype(::Type{<:PersistentStack{T}}) where {T} = T
+Base.IteratorSize(::Type{<:PersistentStack}) = Base.HasLength()
+Base.length(stack::PersistentStack) = stack.length
+
+function Base.iterate(stack::PersistentStack, state = 1)
+    state > length(stack) && return nothing
+    return stack[state], state + 1
+end
+
+# fast shortcut for Base.Iterators.reverse
+function Base.iterate(stack::Base.Iterators.Reverse{<:PersistentStack}, state = stack.itr)
+    isempty(stack.itr) && return nothing
+    return pop(state), front(state)
+end
+
+function Base.getindex(stack::PersistentStack, i)
+    if i < 1 || i > length(stack)
+        throw(BoundsError(stack, i))
+    end
+    count = length(stack)
+    while count > i
+        stack = front(stack)
+        count -= 1
+    end
+    return pop(stack)
+end
+Base.firstindex(::PersistentStack) = 1
+Base.lastindex(stack::PersistentStack) = length(stack)
 
 function Base.collect(stack::PersistentStack{T}) where {T}
     res = T[pop(stack)]
