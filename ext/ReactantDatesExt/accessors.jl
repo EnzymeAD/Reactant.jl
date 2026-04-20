b67ce6d49985d15653f4c48b3426970a3929b20f
Maximilian Gelbrecht <maximilian.gelbrecht@posteo.de>
2026-04-20 12:32:38 +0200

    Update ext/ReactantDatesExt/accessors.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/ReactantDatesExt/accessors.jl
+++ ext/ReactantDatesExt/accessors.jl
@@ -52,9 +52,9 @@ yearmonthday(dt::Union{ReactantDate,ReactantDateTime}) = Dates.yearmonthday(days
 # The algorithm is identical to Dates/src/accessors.jl (proleptic Gregorian calendar)
 # These versions replace ternary operators with `flds` and `divs`
 function Dates.yearmonthday(days::Reactant.TracedRNumber)
-    z = days + 306;
-    h = 100z - 25;
-    a = fld(h, 3652425);
+    z = days + 306
+    h = 100z - 25
+    a = fld(h, 3652425)
     b = a - fld(a, 4)
     y = fld(100b + h, 36525);
     c = b + z - 365y - fld(y, 4);
