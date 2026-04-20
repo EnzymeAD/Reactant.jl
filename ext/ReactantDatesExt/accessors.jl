cb078fc775f762e21e0180346b48c6faa2029192
Maximilian Gelbrecht <maximilian.gelbrecht@posteo.de>
2026-04-20 12:32:51 +0200

    Update ext/ReactantDatesExt/accessors.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/ReactantDatesExt/accessors.jl
+++ ext/ReactantDatesExt/accessors.jl
@@ -65,9 +65,9 @@ function Dates.yearmonthday(days::Reactant.TracedRNumber)
 end
 
 function Dates.yearmonth(days::Reactant.TracedRNumber)
-    z = days + 306;
-    h = 100z - 25;
-    a = fld(h, 3652425);
+    z = days + 306
+    h = 100z - 25
+    a = fld(h, 3652425)
     b = a - fld(a, 4)
     y = fld(100b + h, 36525);
     c = b + z - 365y - fld(y, 4);
