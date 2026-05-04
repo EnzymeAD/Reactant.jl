0e9542fa643f496d3ac08a91497359d24481a887
Billy Moses <wmoses@google.com>
2026-05-04 13:23:31 -0500

    Trainium support

---
+++ src/accelerators/Trainium.jl
@@ -0,0 +1,141 @@
+module Trainium
+
+using Reactant: Reactant
+using Scratch: @get_scratch!
+using Downloads: Downloads
+using p7zip_jll: p7zip
+using FileWatching: mkpidlock
+
+using ..Registration: register_backend
+
+const trainium_pjrt_plugin_dir = Ref{Union{Nothing,String}}(nothing)
+const trainium_pjrt_plugin_name = Ref{String}("libneuronpjrt.so")
+
+function setup_correct_env_vars!()
+    # From libneuronxla/__init__.py
+    if !haskey(ENV, "XLA_IR_SHAPE_CACHE_SIZE")
+        ENV["XLA_IR_SHAPE_CACHE_SIZE"] = "20480"
+    end
+    if haskey(ENV, "XLA_USE_BF16") || haskey(ENV, "XLA_DOWNCAST_BF16")
+        if !haskey(ENV, "NEURON_RT_STOCHASTIC_ROUNDING_EN")
+            ENV["NEURON_RT_STOCHASTIC_ROUNDING_EN"] = "1"
+        end
+    end
+end
+
+function make_pjrt_client(;
+    node_id::Integer=0,
+    num_nodes::Integer=1,
+    distributed_runtime_client=nothing,
+    allowed_devices::Union{Nothing,Vector{Int}}=nothing,
+)
+    @assert node_id == 0 "`make_pjrt_client` does not support node_id"
+    @assert num_nodes == 1 "`make_pjrt_client` does not support num_nodes > 1"
+    @assert distributed_runtime_client === nothing "`make_pjrt_client` does not support distributed_runtime_client"
+
+    if allowed_devices !== nothing
+        @debug "TrainiumClient doesn't support allowed_devices. Ignoring the kwarg."
+    end
+
+    return Reactant.XLA.PJRT.MakeClientUsingPluginAPI(get_trainium_pjrt_plugin_path(), "trainium", "Trainium")
+end
+
+function make_ifrt_client(;
+    node_id::Integer=0,
+    num_nodes::Integer=1,
+    distributed_runtime_client=nothing,
+    allowed_devices::Union{Nothing,Vector{Int}}=nothing,
+)
+    if allowed_devices !== nothing
+        @debug "TrainiumClient doesn't support allowed_devices. Ignoring the kwarg."
+    end
+
+    return Reactant.XLA.IFRT.MakeIFRTPJRTClientViaPluginAPI(
+        get_trainium_pjrt_plugin_path(),
+        "trainium",
+        "Trainium";
+        node_id,
+        num_nodes,
+        distributed_runtime_client,
+    )
+end
+
+function __init__()
+    if Sys.islinux() && has_trainium() && !Reactant.precompiling()
+        register_backend(
+            "trainium";
+            priority=1000,
+            pjrt_initialize_function=make_pjrt_client,
+            ifrt_initialize_function=make_ifrt_client,
+            preinitialize_setup_function=() -> begin
+                setup_trainium_pjrt_plugin!()
+                setup_correct_env_vars!()
+                nothing
+            end,
+        )
+    end
+end
+
+force_trainium_init() = haskey(ENV, "REACTANT_FORCE_TRAINIUM_INIT")
+
+function has_trainium()
+    if force_trainium_init()
+        return true
+    end
+    return ispath("/opt/aws/neuron/lib/libnrt.so.1")
+end
+
+function setup_trainium_pjrt_plugin!()
+    plugin_dir_from_env = get(ENV, "TRAINIUM_PJRT_PLUGIN_DIR", nothing)
+    if plugin_dir_from_env !== nothing && ispath(plugin_dir_from_env)
+        trainium_pjrt_plugin_dir[] = plugin_dir_from_env
+    else
+        # Check if we are in the development environment and have the file locally
+        dev_path = joinpath(@__DIR__, "../../libneuronxla/libneuronpjrt.so")
+        if isfile(dev_path)
+            trainium_pjrt_plugin_dir[] = dirname(dev_path)
+            return nothing
+        end
+        trainium_pjrt_plugin_dir[] = @get_scratch!("pjrt_plugin_trainium")
+    end
+    download_trainium_pjrt_plugin_if_needed(trainium_pjrt_plugin_dir[])
+    return nothing
+end
+
+get_trainium_pjrt_plugin_dir() = trainium_pjrt_plugin_dir[]
+
+function get_trainium_pjrt_plugin_path()
+    return joinpath(get_trainium_pjrt_plugin_dir(), trainium_pjrt_plugin_name[])
+end
+
+function download_trainium_pjrt_plugin_if_needed(dir=nothing)
+    dir === nothing && (dir = get_trainium_pjrt_plugin_dir())
+    @assert dir !== nothing "trainium_pjrt_plugin_dir is not set!"
+
+    trainium_pjrt_plugin_path = joinpath(dir, trainium_pjrt_plugin_name[])
+    if isfile(trainium_pjrt_plugin_path)
+        @debug "Trainium PJRT plugin already found in '$(trainium_pjrt_plugin_path)', nothing to do"
+    else
+        mkpidlock(joinpath(dir, "download_trainium_pjrt_plugin.lock")) do
+            if !isfile(trainium_pjrt_plugin_path)
+                @debug "Will install the Trainium PJRT plugin to '$(trainium_pjrt_plugin_path)'"
+                mktempdir() do tmp_dir
+                    zip_file_path = joinpath(tmp_dir, "libneuronxla.zip")
+                    wheel_url = "https://pip.repos.neuron.amazonaws.com/libneuronxla/libneuronxla-2.2.8201.0%2Bf46ac1ef-py3-none-linux_x86_64.whl"
+                    @debug "Downloading Trainium PJRT plugin from '$(wheel_url)'"
+                    Downloads.download(wheel_url, zip_file_path)
+                    run(
+                        pipeline(
+                            `$(p7zip()) x -tzip -o$(tmp_dir) -- $(zip_file_path)`, devnull
+                        ),
+                    )
+                    # Move libneuronpjrt.so to dir
+                    mv(joinpath(tmp_dir, "libneuronxla", "libneuronpjrt.so"), trainium_pjrt_plugin_path; force=true)
+                end
+            end
+        end
+        @assert isfile(trainium_pjrt_plugin_path)
+    end
+end
+
+end # module Trainium
