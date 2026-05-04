ff0531be35930c280b45cb3b188e5e8d6926050d
Billy Moses <wmoses@google.com>
2026-05-04 16:08:45 -0500

    move things

--- src/accelerators/Trainium.jl
+++ src/accelerators/Trainium.jl
@@ -28,60 +28,56 @@ function setup_correct_env_vars!()
     end
 end
 
-function make_pjrt_client(;
-    node_id::Integer=0,
-    num_nodes::Integer=1,
-    distributed_runtime_client=nothing,
-    allowed_devices::Union{Nothing,Vector{Int}}=nothing,
-)
-    @assert node_id == 0 "`make_pjrt_client` does not support node_id"
-    @assert num_nodes == 1 "`make_pjrt_client` does not support num_nodes > 1"
-    @assert distributed_runtime_client === nothing "`make_pjrt_client` does not support distributed_runtime_client"
-
-    if allowed_devices !== nothing
-        @debug "TrainiumClient doesn't support allowed_devices. Ignoring the kwarg."
-    end
-
-    # Load the Python library globally
-    Libdl.dlopen(PYTHON_LIB, Libdl.RTLD_GLOBAL)
-
-    # Initialize the Python interpreter
-    ccall(:Py_Initialize, Cvoid, ())
-
+    plugin_dir = get_trainium_pjrt_plugin_dir()
+    
     # Create a dummy libneuronxla module with expected attributes
     py_code = """
+import sys
+import os
+
+# Add path to the scratch directory where libneuronxla is extracted
+sys.path.append('$(escape_string(plugin_dir))')
+
+try:
+    import libneuronxla
+    print('Imported real libneuronxla successfully')
+    
+    # Call configure_environment and hook as required by the library
+    libneuronxla.configure_environment()
+    libneuronxla.hook()
+    print('Called configure_environment and hook')
+except ImportError as e:
+    print(f'Failed to import real libneuronxla: {e}. Falling back to dummy module.')
     import types
-    import sys
-    import os
     import socket
-
+    
     mod = types.ModuleType('libneuronxla')
-
-    # Add dummy callback
+    
     def dummy_callback(name, addressable_device_index, execution_count):
         return 'inputs'
     mod._dump_hlo_snapshot_callback = dummy_callback
-
-    # Add dummy configure function
+    
     def dummy_configure():
         pass
     mod.configure_environment = dummy_configure
-
-    # Add dummy hook function that sets NEURON_RT_ROOT_COMM_ID
+    
     def dummy_hook():
-        # Find a free port
         with socket.socket() as s:
             s.bind(('', 0))
             port = s.getsockname()[1]
-        
         os.environ['NEURON_RT_ROOT_COMM_ID'] = f'localhost:{port}'
         print(f'Dummy hook called, set NEURON_RT_ROOT_COMM_ID to localhost:{port}')
         pass
     mod.hook = dummy_hook
-
+    
+    def dummy_neuronx_cc(code, code_format, platform_version, file_prefix):
+        print('Dummy neuronx_cc called')
+        return 0, b''
+    mod.neuronx_cc = dummy_neuronx_cc
+    
     sys.modules['libneuronxla'] = mod
-    print('Dummy libneuronxla module with callbacks and functional hook created and registered')
-    """
+    print('Dummy libneuronxla module with callbacks, hook, and neuronx_cc created and registered')
+"""
 
     ccall(:PyRun_SimpleString, Cint, (Cstring,), py_code)
 
@@ -130,7 +126,7 @@ function has_trainium()
     if force_trainium_init()
         return true
     end
-    return ispath("/opt/aws/neuron/lib/libnrt.so.1")
+    return Sys.islinux() && any(startswith("neuron"), readdir("/dev"))
 end
 
 function setup_trainium_pjrt_plugin!()
@@ -153,6 +149,15 @@ end
 get_trainium_pjrt_plugin_dir() = trainium_pjrt_plugin_dir[]
 
 function get_trainium_pjrt_plugin_path()
+    dev_path = joinpath(@__DIR__, "../../libneuronxla/libneuronpjrt.so")
+    if isfile(dev_path)
+        return dev_path
+    end
+    # Check if it is in the scratch space as a directory
+    dir_path = joinpath(get_trainium_pjrt_plugin_dir(), "libneuronxla", trainium_pjrt_plugin_name[])
+    if isfile(dir_path)
+        return dir_path
+    end
     return joinpath(get_trainium_pjrt_plugin_dir(), trainium_pjrt_plugin_name[])
 end
 
@@ -177,8 +182,8 @@ function download_trainium_pjrt_plugin_if_needed(dir=nothing)
                             `$(p7zip()) x -tzip -o$(tmp_dir) -- $(zip_file_path)`, devnull
                         ),
                     )
-                    # Move libneuronpjrt.so to dir
-                    mv(joinpath(tmp_dir, "libneuronxla", "libneuronpjrt.so"), trainium_pjrt_plugin_path; force=true)
+                    # Move the whole libneuronxla directory to dir
+                    mv(joinpath(tmp_dir, "libneuronxla"), joinpath(dir, "libneuronxla"); force=true)
                 end
             end
         end
