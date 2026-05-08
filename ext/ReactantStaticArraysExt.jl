84c0dc5dde1ee897a42f1ae17d81828a6b01359e
Paul Tiede <ptiede91@gmail.com>
2026-05-07 21:43:23 -0500

    Add additional static array fixes and tests

--- ext/ReactantStaticArraysExt.jl
+++ ext/ReactantStaticArraysExt.jl
@@ -2,11 +2,13 @@ module ReactantStaticArraysExt
 
 using Reactant
 import Reactant.TracedRArrayOverrides: overloaded_map, overloaded_mapreduce
-import Reactant.TracedLinearAlgebra: overloaded_mul
-
+import Reactant.TracedLinearAlgebra: overloaded_mul, overloaded_dot
+using LinearAlgebra: dot
 using StaticArrays: SArray, StaticArray
 
-const SAReact{Sz,T} = StaticArray{Sz,T} where {Sz<:Tuple,T<:Reactant.TracedRNumber}
+const SARArray{Sz,T,N} = StaticArray{Sz,T,N} where {Sz<:Tuple,T<:Reactant.TracedRNumber}
+const SARVector{N, T, N} = SARArray{Tuple{N},T,N}
+const SARMatrix{M,N,T} = SARArray{Tuple{M,N},T,N}
 
 Base.@nospecializeinfer function Reactant.traced_type_inner(
     @nospecialize(FA::Type{SArray{S,T,N,L}}),
@@ -20,15 +22,15 @@ Base.@nospecializeinfer function Reactant.traced_type_inner(
     return SArray{S,T_traced,N,L}
 end
 
-function Reactant.materialize_traced_array(x::SAReact)
+function Reactant.materialize_traced_array(x::SARArray)
     return x
 end
 
 # We don't want to overload map on StaticArrays since it is likely better to just unroll things
-overloaded_map(f, a::SAReact, rest::SAReact...) = f.(a, rest...)
-overloaded_mapreduce(f, op, a::SAReact; kwargs...) = mapreduce(f, op, a, kwargs...)
+overloaded_map(f, a::SARArray, rest::SARArray...) = f.(a, rest...)
+overloaded_mapreduce(f, op, a::SARArray; kwargs...) = mapreduce(f, op, a, kwargs...)
 
-function overloaded_mul(A::SAReact, B::SAReact, alpha::Number=true, beta::Number=false)
+function overloaded_mul(A::SARArray, B::SARArray, alpha::Number=true, beta::Number=false)
     # beta is not supported since it is zero by default in Reactant 
     # (similar is zero'd automatically for TracedRArrays)
     C = A * B
@@ -36,6 +38,14 @@ function overloaded_mul(A::SAReact, B::SAReact, alpha::Number=true, beta::Number
         return C
     end
     return C .* alpha
+end
+
+function overloaded_dot(x::SARArray, y::SARArray)
+    return dot(x, y)
+end
+
+function overloaded_dot(x::SARVector, y::SARMatrix, z::SARVector)
+    return dot(x, y, z)
 end
 
 end
