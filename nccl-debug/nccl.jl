b6839954c064e3cb0477b92662544018bf5373d0
Roman Lee <rplee7@gmail.com>
2026-05-08 15:54:51 -0500

    update nccl-debug dir

--- nccl-debug/nccl.jl
+++ nccl-debug/nccl.jl
@@ -1,18 +1,45 @@
 using Test
 using MPI
 using NCCL
+using CUDA
 using Reactant
+
+using InteractiveUtils
+
+# Reactant.set_default_backend("cpu")
+# Reactant.set_default_backend("gpu")
 
 const ReactantNCCLExt = Base.get_extension(Reactant, :ReactantNCCLExt)
 ReactantNCCLExt === nothing && error("ReactantNCCLExt is not loaded")
 
-const COMM = MPI.COMM_WORLD
+const MPICOMM = MPI.COMM_WORLD
 
 MPI.Init()
-ReactantNCCLExt.init_default_comm()
+ncclComm = ReactantNCCLExt.init_default_comm()
 
-println("\ncode_hlo optimize=false:\n",
-        @code_hlo optimize=false MPI.Comm_rank(COMM))
+
+# MPI.Allreduce
+if true
+    send_buf = ones(Float64, 5)
+    recv_buf = ones(Float64, 5)
+
+    println("\ncode_hlo optimize=false:\n",
+            @code_hlo optimize=false MPI.Allreduce!(ConcreteRArray(send_buf), ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
+    println("\ncode_hlo optimize=\"lower-enzymexla-mpi{backend=cuda ncclCommPtr=1}\":\n",
+            @code_hlo optimize="lower-enzymexla-mpi{backend=cuda ncclCommPtr=1}" MPI.Allreduce!(ConcreteRArray(send_buf), ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
+    println("\ncode_hlo:\n",
+            @code_hlo MPI.Allreduce!(ConcreteRArray(send_buf), ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
+
+    # println(MPI.Allreduce!(send_buf, ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
+    println(@jit MPI.Allreduce!(ConcreteRArray(send_buf), ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
+    TODO might need a Reactant CUDA Synchronize here before we inspect the output?
+
+
+    # recv_buf = NCCL.Allreduce!(CuArray(send_buf), CuArray(recv_buf), +, ncclComm)
+    # CUDA.synchronize()
+    # println(Array(recv_buf))
+
+end
 
 ReactantNCCLExt.destroy_default_comm()
 MPI.Finalize()
--- nccl-debug/run.sh
+++ nccl-debug/run.sh
@@ -1,1 +1,3 @@
 mpiexecjl -n 2 julia --color=yes --project nccl.jl
+gdb -batch -ex run -ex bt --args bash -lc 'XLA_REACTANT_GPU_MEM_FRACTION=0.5 XLA_REACTANT_GPU_PREALLOCATE=false julia --project nccl.jl'
+gdb --batch -ex 'set env XLA_REACTANT_GPU_MEM_FRACTION 0.5' -ex 'set env XLA_REACTANT_GPU_PREALLOCATE false' -ex run -ex bt --args julia --project nccl.jl
