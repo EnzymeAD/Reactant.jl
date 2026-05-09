fa0cac54b306d094c37d832b2224ae9e62ce7b37
Roman Lee <rplee7@gmail.com>
2026-05-08 19:59:52 -0500

    update nccl.jl, almost working maybe

--- nccl-debug/nccl.jl
+++ nccl-debug/nccl.jl
@@ -16,23 +16,29 @@ const MPICOMM = MPI.COMM_WORLD
 
 MPI.Init()
 ncclComm = ReactantNCCLExt.init_default_comm()
+println("MPI rank $(MPI.Comm_rank(MPICOMM)) NCCL comm = ", ncclComm)
+println("MPI rank $(MPI.Comm_rank(MPICOMM)) XLA device = ",
+        Reactant.XLA.default_device(Reactant.XLA.default_backend()))
 
 
 # MPI.Allreduce
 if true
     send_buf = ones(Float64, 5)
     recv_buf = ones(Float64, 5)
+    rsend_buf = ConcreteRArray(send_buf)
+    rrecv_buf = ConcreteRArray(recv_buf)
 
-    println("\ncode_hlo optimize=false:\n",
-            @code_hlo optimize=false MPI.Allreduce!(ConcreteRArray(send_buf), ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
-    println("\ncode_hlo optimize=\"lower-enzymexla-mpi{backend=cuda ncclCommPtr=1}\":\n",
-            @code_hlo optimize="lower-enzymexla-mpi{backend=cuda ncclCommPtr=1}" MPI.Allreduce!(ConcreteRArray(send_buf), ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
+    # println("\ncode_hlo optimize=false:\n",
+    #         @code_hlo optimize=false MPI.Allreduce!(rsend_buf, rrecv_buf, MPI.SUM, MPICOMM))
+    # println("\ncode_hlo optimize=\"lower-enzymexla-mpi{backend=cuda ncclCommPtr=1}\":\n",
+    #         @code_hlo optimize="lower-enzymexla-mpi{backend=cuda ncclCommPtr=1}" MPI.Allreduce!(rsend_buf, rrecv_buf, MPI.SUM, MPICOMM))
     println("\ncode_hlo:\n",
-            @code_hlo MPI.Allreduce!(ConcreteRArray(send_buf), ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
+            @code_hlo MPI.Allreduce!(rsend_buf, rrecv_buf, MPI.SUM, MPICOMM))
 
     # println(MPI.Allreduce!(send_buf, ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
-    println(@jit MPI.Allreduce!(ConcreteRArray(send_buf), ConcreteRArray(recv_buf), MPI.SUM, MPICOMM))
-    TODO might need a Reactant CUDA Synchronize here before we inspect the output?
+    result = @jit MPI.Allreduce!(rsend_buf, rrecv_buf, MPI.SUM, MPICOMM)
+    Reactant.synchronize(result)
+    println("MPI rank $(MPI.Comm_rank(MPICOMM)) result = ", Array(result))
 
 
     # recv_buf = NCCL.Allreduce!(CuArray(send_buf), CuArray(recv_buf), +, ncclComm)
