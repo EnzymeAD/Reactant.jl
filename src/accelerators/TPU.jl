741d4883104792df67d3fb7e0cb7ce4fb6ee8ddd
William Moses <gh@wsmoses.com>
2026-05-21 17:56:54 -0500

    Update LIBTPU_VERSION to 0.0.42.dev20260520

--- src/accelerators/TPU.jl
+++ src/accelerators/TPU.jl
@@ -13,7 +13,7 @@ using ..Registration: register_backend
 const libtpu_dir = Ref{Union{Nothing,String}}(nothing)
 const RUNNING_IN_CLOUD_TPU_VM = Ref{Union{Nothing,Bool}}(nothing)
 
-const LIBTPU_VERSION = "0.0.41.dev20260509"
+const LIBTPU_VERSION = "0.0.42.dev20260520"
 const LIBTPU_SO = "libtpu-$(replace(string(LIBTPU_VERSION), '.' => '_')).so"
 
 function setup_correct_env_vars!()
