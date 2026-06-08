f4b3ba9b66cc7069af7bd7c73f616526b4dfff7b
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-06-08 06:55:58 -0500

    format

--- ext/ReactantVectorInterfaceExt.jl
+++ ext/ReactantVectorInterfaceExt.jl
@@ -3,10 +3,18 @@ module ReactantVectorInterfaceExt
 using Rectant
 using VectorInterface
 
-Reactant.promote_to(TT::Type{TracedRNumber{T}}, ::VectorInterface.Zero) where {T} = promote_to(TT, zero(T))
-Reactant.promote_to(TT::Type{TracedRNumber{T}}, ::VectorInterface.One) where {T} = promote_to(TT, one(T))
+function Reactant.promote_to(TT::Type{TracedRNumber{T}}, ::VectorInterface.Zero) where {T}
+    return promote_to(TT, zero(T))
+end
+function Reactant.promote_to(TT::Type{TracedRNumber{T}}, ::VectorInterface.One) where {T}
+    return promote_to(TT, one(T))
+end
 
-Reactant.promote_to(::Type{TracedRNumber}, v::VectorInterface.Zero) = promote_to(TracedRNumber{Float64}, v)
-Reactant.promote_to(::Type{TracedRNumber}, v::VectorInterface.One) = promote_to(TracedRNumber{Float64}, v)
+function Reactant.promote_to(::Type{TracedRNumber}, v::VectorInterface.Zero)
+    return promote_to(TracedRNumber{Float64}, v)
+end
+function Reactant.promote_to(::Type{TracedRNumber}, v::VectorInterface.One)
+    return promote_to(TracedRNumber{Float64}, v)
+end
 
 end
