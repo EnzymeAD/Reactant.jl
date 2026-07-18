9141067d7143ef255001e607467d1021859d3318
William Moses <gh@wsmoses.com>
2026-07-18 17:55:26 -0500

    Update TPU.jl

--- src/accelerators/TPU.jl
+++ src/accelerators/TPU.jl
@@ -12,7 +12,7 @@ using ..Registration: register_backend
 const libtpu_dir = Ref{Union{Nothing,String}}(nothing)
 const RUNNING_IN_CLOUD_TPU_VM = Ref{Union{Nothing,Bool}}(nothing)
 
-const LIBTPU_VERSION = "0.0.44.dev20260717"
+const LIBTPU_VERSION = "0.0.45.dev20260717"
 const LIBTPU_SO = "libtpu-$(replace(string(LIBTPU_VERSION), '.' => '_')).so"
 
 function setup_correct_env_vars!()
