53e3966ffef2432083fb7d7670dcc81deac1822b
William Moses <gh@wsmoses.com>
2026-07-26 18:26:40 -0500

    Fix cudnn static linking (#3110)
    * Fix cudnn static linking
    
    * fmt
    
    * rm fuse-ld by default
    
    * fixup
    
    * fix
    
    * fix

--- deps/build_local.jl
+++ deps/build_local.jl
@@ -273,8 +273,6 @@ push!(build_cmd_list, "--jobs=$(parsed_args["jobs"])")
 push!(build_cmd_list, "--experimental_ui_max_stdouterr_bytes=-1")
 push!(build_cmd_list, "--spawn_strategy=local")
 
-push!(build_cmd_list, "--linkopt=-fuse-ld=lld")
-
 for opt in parsed_args["copt"]
     push!(build_cmd_list, "--copt=$(opt)")
 end
@@ -300,11 +298,14 @@ if cc_is_gcc
         end
     end
 else
-    # Assume the compiler is clang if not GCC. `using_clang` is an option
-    # introduced by Enzyme-JAX.
-    push!(build_cmd_list, "--define=using_clang=true")
     push!(build_cmd_list, "--copt=-Wno-unused-command-line-argument")
 end
+
+if !isnothing(Sys.which("lld"))
+  push!(build_cmd_list, "--linkopt=-fuse-ld=lld")
+end
+
+
 push!(build_cmd_list, "--copt=-Wno-private-header")
 push!(build_cmd_list, "--color=$(parsed_args["color"])")
 push!(build_cmd_list, ":libReactantExtra.so")
