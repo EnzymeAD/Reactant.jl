3eb17377377d737cbe5a837337b8d8839707cbcc
Penelope Yong <penelopeysm@gmail.com>
2026-08-29 01:13:53 +0100

    Fix Tables ext (#3230)

--- ext/ReactantTablesExt.jl
+++ ext/ReactantTablesExt.jl
@@ -7,13 +7,15 @@ using Tables: Tables
 # which is ambiguous with Reactant's catch-all `TracedRNumber` comparisons. A `Col` is a
 # `ScanExpr`, not something that can be promoted to a traced number, so the mixed cases
 # should always run Tables' method.
-for jlop in (:(<), :(<=))
-    @eval begin
-        function Base.$(jlop)(lhs::TracedRNumber, rhs::Tables.Col)
-            return invoke(Base.$(jlop), Tuple{Number,Tables.Col}, lhs, rhs)
-        end
-        function Base.$(jlop)(lhs::Tables.Col, rhs::TracedRNumber)
-            return invoke(Base.$(jlop), Tuple{Tables.Col,Number}, lhs, rhs)
+@static if isdefined(Tables, :Col)
+    for jlop in (:(<), :(<=))
+        @eval begin
+            function Base.$(jlop)(lhs::TracedRNumber, rhs::Tables.Col)
+                return invoke(Base.$(jlop), Tuple{Number,Tables.Col}, lhs, rhs)
+            end
+            function Base.$(jlop)(lhs::Tables.Col, rhs::TracedRNumber)
+                return invoke(Base.$(jlop), Tuple{Tables.Col,Number}, lhs, rhs)
+            end
         end
     end
 end
