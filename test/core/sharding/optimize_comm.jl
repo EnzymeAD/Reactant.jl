d35843d2bfffefe77939248d1dd2e35856a824bf
Ludovic Raess <ludovic.rass@gmail.com>
2026-08-31 17:37:05 +0200

    Test any GPU, not CUDA specific

--- test/core/sharding/optimize_comm.jl
+++ test/core/sharding/optimize_comm.jl
@@ -3,7 +3,7 @@ using Reactant, Test, FileCheck
 const addressable_devices = Reactant.addressable_devices()
 
 const RunningOnTPU = contains(string(Reactant.devices()[1]), "TPU")
-const RunningOnGPU = contains(string(Reactant.devices()[1]), "CUDA")
+const RunningOnGPU = Reactant.XLA.platform_name(Reactant.XLA.default_backend()) != "cpu"
 
 function rotate(x)
     y = x[1:100, :]
