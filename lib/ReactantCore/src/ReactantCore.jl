2e90a5a5fdc6fe087ede643d68e1ffefcd89e7e6
Paul Berg <naydex.mc+github@gmail.com>
2026-09-08 16:24:56 +0200

    ReactantCore: Improve raising of if with results

--- lib/ReactantCore/src/ReactantCore.jl
+++ lib/ReactantCore/src/ReactantCore.jl
@@ -723,6 +723,8 @@ function trace_if(expr; store_last_line=nothing, depth=0, track_numbers)
 
     all_vars = unique(all_input_vars ∪ all_output_vars)
 
+    result_sym = gensym(:if_result)
+
     true_branch_fn = :(
         (args,) -> begin
             $([:($v = if hasproperty(args, $(QuoteNode(v)))
@@ -730,8 +732,8 @@ function trace_if(expr; store_last_line=nothing, depth=0, track_numbers)
                     else
                         $(MissingTracedValue)()
                     end) for v in all_vars]...)
-            $(true_block)
-            return ($(all_output_vars...),)
+            $(result_sym) = $(true_block)
+            return ($(result_sym), $(all_output_vars...),)
         end
     )
     true_branch_fn = cleanup_expr_to_avoid_boxing(
@@ -746,8 +748,8 @@ function trace_if(expr; store_last_line=nothing, depth=0, track_numbers)
                     else
                         $(MissingTracedValue)()
                     end) for v in all_vars]...)
-            $(false_block)
-            return ($(all_output_vars...),)
+            $(result_sym) = $(false_block)
+            return ($(result_sym), $(all_output_vars...),)
         end
     )
     false_branch_fn = cleanup_expr_to_avoid_boxing(
@@ -767,13 +769,14 @@ function trace_if(expr; store_last_line=nothing, depth=0, track_numbers)
     reactant_code_block = quote
         $(true_branch_fn)
         $(false_branch_fn)
-        ($(all_output_vars...),) = $(traced_if)(
+        ($(result_sym), $(all_output_vars...),) = $(traced_if)(
             $(cond_name),
             $(true_branch_fn_name),
             $(false_branch_fn_name),
             $(args_expr);
             track_numbers=($(track_numbers)),
         )
+        $(result_sym)
     end
 
     non_reactant_code_block = Expr(:if, cond_name, original_expr.args[2])
@@ -789,7 +792,7 @@ function trace_if(expr; store_last_line=nothing, depth=0, track_numbers)
             $(cond_name) = $(cond_expr)
             $(reactant_code_block)
         end,
-        (true_branch_fn_name, false_branch_fn_name),
+        (true_branch_fn_name, false_branch_fn_name, result_sym),
         all_check_vars,
     )
 
